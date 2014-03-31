a41f37943feb759bd5e27f3e6a4cfa8a16f22ed2
Ben Best <bbest@nceas.ucsb.edu>
2014-03-31 15:46:50 -0700

    fixed pressures rasters and output differences, most notably in cc_slr, cc_sst, fp_com_hb and fp_com_lb. (ohi-science/ohicore#48)

--- Global/GL-NCEAS-Pressures_v2013-fixed/digest_rasters.R
+++ Global/GL-NCEAS-Pressures_v2013-fixed/digest_rasters.R
@@ -3,14 +3,10 @@ library(tools)
 library(plyr)
 library(dplyr)
 
-#wd = 'N:/model/GL-NCEAS-Pressures_v2013a/tmp'
-wd = '/Volumes/data_edit/model/GL-NCEAS-Pressures_v2013a/tmp'
-setwd(wd)
+source('src/R/common.R') # get dir_neptune_data based on OS
+dir_wd = file.path(dir_neptune_data, 'model/GL-NCEAS-Pressures_v2013a')
 
-p = read.csv('../model_rasters_output.csv', stringsAsFactors=F)
-
-# DEBUG: redo ones with 3nm since got overwritten
-#p = subset(p, pressure %in% c('po_chemicals','po_chemicals_3nm','po_nutrients','po_nutrients_3nm'))
+p = read.csv(file.path(dir_wd, 'model_rasters_output.csv'), stringsAsFactors=F)
 
 rescaling_fxns = list(
   'no_rescaling'      = expression(v),
@@ -19,20 +15,20 @@ rescaling_fxns = list(
   'sst_abs313_110pct' = expression(abs(v) / (313 * 1.10))
 ) 
 
-for (i in 1:nrow(p)){    # i=1
-# DEBUG redo
-#for (i in which(p$pressure %in% c('cc_slr','cc_sst','cc_uv'))){    # i=1    
+for (i in 1:nrow(p)){ # i=1  
+  
+  # set pressure (non-year-specific) variables
+  pressure  = p[i,'pressure']
+  rescaling = ifelse(p[i,'rescaling']=='', 'no_rescaling', p[i,'rescaling'])
+  rgn       = file_path_sans_ext(basename(p$rgn[i]))
   
   for (yr in 2012:2013){ # yr=2012
     
     # start with details per region for readability
-    d = read.csv('../../GL-NCEAS-OceanRegions_v2013a/data/rgn_details.csv')[,c('rgn_id','rgn_typ','rgn_key','rgn_nam')]    
+    d = read.csv(file.path(dir_neptune_data, 'model/GL-NCEAS-OceanRegions_v2013a/data/rgn_details.csv'))[,c('rgn_id','rgn_typ','rgn_key','rgn_nam')]    
     
-    # set tif variables
-    pressure  = p[i,'pressure']
-    rescaling = p[i,'rescaling']
+    # set year-specific variables    
     tifs      = file_path_sans_ext(basename(unlist(strsplit(p[i, sprintf('tifs_%da', yr)], ','))))
-    rgn       = file_path_sans_ext(basename(p$rgn[i]))
     tif.dbfs  = sprintf('%s_%s.dbf', rgn, tifs)
     tif.flds  = make.names(tifs)    
     cat(sprintf('\n\n--------\n%s for %da. rescaling: %s. tifs: %s\n', pressure, yr, rescaling, paste(tifs, collapse=', ')))    
@@ -42,78 +38,27 @@ for (i in 1:nrow(p)){    # i=1
       
       flds.dbf = setNames(c('rgn_id', tif.flds[j]), c('VALUE','MEAN'))
       d = merge(x=d, by='rgn_id',
-                y=rename(read.dbf(tif.dbfs[j]), flds.dbf)[,flds.dbf], all.x=T)  ; head(d)
+                y=rename(read.dbf(file.path(dir_wd, 'tmp', tif.dbfs[j])), flds.dbf)[,flds.dbf], all.x=T)
       v = d[[tif.flds[j]]]
-      
-      if (rescaling!=''){
-        cat(sprintf('    %s before rescaling:\n', tif.dbfs[j]))
-        print(summary(v))
-      }      
-      
-      # apply rescaling, if needed
-      if (rescaling=='score-max-plus10'){
-        d[[tif.flds[j]]] = v / (max(v, na.rm=T) * 1.10)
-        p[i,'rescaling_digest'] = "rescaling=='score-max-plus10': v / (max(v, na.rm=T) * 1.10"
-        
-      } else if (rescaling!='slr_clamp0_110pct'){        
-        d[[tif.flds[j]]] = ifelse(v<0,0,v) / (max(v, na.rm=T) * 1.10)
-        p[i,'rescaling_digest'] = "rescaling!='slr_clamp0_110pct': ifelse(v<0,0,v) / (max(v, na.rm=T) * 1.10)"
-            
-      } else if (rescaling!='sst_abs313_110pct'){        
-        d[[tif.flds[j]]] = abs(v) / (313 * 1.10)           
-        p[i,'rescaling_digest'] = "rescaling!='sst_abs313_110pct': abs(v) / (313 * 1.10)"
-            
-      } else if (rescaling!=''){        
-        stop(sprintf("rescaling '%s' not handled!", rescaling))
-      }
-      
-      #if (rescaling!=''){
-      cat(sprintf('    %s after %s:\n', tif.dbfs[j], p[i,'rescaling_function']))
-      print(summary(d[[tif.flds[j]]]))
-      #}
-      
-      # DEBUG ----
-      # try all rescaling, and see which matches
-      for (fxn in names(rescaling_fxns)){
-        d[[sprintf('%s_%s', basename(tif.flds[j]), fxn)]] = eval(rescaling_fxns[[fxn]])        
-      }
-    } # end: for (j in 1:length(tifs)){
-    
-    # for file in *-no-rescaling.csv ; do mv "$file" "${file/-no-rescaling.csv/-no_rescaling.csv}" ; done
+      d[[tif.flds[j]]] = eval(rescaling_fxns[[rescaling]])
+    }      
     
     # get average of tifs (NOTE: removing NAs for mean)
     d[['pressure_score']] = apply(d[, tif.flds, drop=F], 1, mean, na.rm=T)
     
     # write to csv per year-pressure
-    csv = sprintf('../data/%s_%d_check.csv', pressure, yr) 
-#    write.csv(d[, c('rgn_id', 'pressure_score')], csv, na='', row.names=F)    
+    csv = sprintf('%s/data/%s_%d_NEW.csv', dir_wd, pressure, yr)
+    write.csv(d[, c('rgn_id', 'pressure_score')], csv, na='', row.names=F)    
     
-    # DEBUG ----
-    # try all rescaling, and see which matches
-    d_0 = read.csv(sprintf('../data/%s_%d.csv', pressure, yr))
-    for (fxn in names(rescaling_fxns)){
-      fld = sprintf('%s_%s', pressure, fxn)
-      d[[fld]] = apply(d[,sprintf('%s_%s', basename(tif.flds), fxn),drop=F], 1, mean, na.rm=T)
-      write.csv(x=rename(d[,c('rgn_id',fld)], setNames('pressure_score', fld)), 
-                file=sprintf('../data/%s_%d_DEBUG-%s.csv', pressure, yr, fxn), na='', row.names=F)    
-      d_ck = merge(d_0, d[,c('rgn_id',fld)], by='rgn_id')
-      if (isTRUE(all.equal(d_ck[[fld]], d_ck[['pressure_score']]))){
-        p[i,sprintf('rescaling_used_%d',yr)] = paste(na.omit(c(p[i,'rescaling_used'], fxn)), collapse=' | ')  
-        cat(sprintf('\n  rescaling_USED: %s\n', p[i,'rescaling_used']))
-      }      
-    }
-    
-    # NEW ----
-    # get average of tifs (NOTE: removing NAs for mean)
-    fld = sprintf('%s_%s', pressure, fxn)
-    if(rescaling=='') rescaling='no_rescaling'
-    d[['pressure_score']] = apply(d[, tif.flds, drop=F], 1, mean, na.rm=T)
-    
-    # write to csv per year-pressure
-    csv = sprintf('../data/%s_%d_check.csv', pressure, yr)
-    write.csv(d[, c('rgn_id', 'pressure_score')], csv, na='', row.names=F)
-
+    # DEBUG: get difference with OLD ----
+    d_0 = read.csv(sprintf('%s/data/%s_%d.csv', dir_wd, pressure, yr))
+    d_ck = merge(d_0, rename(d[, c('rgn_id', 'pressure_score')], c('pressure_score'='pressure_score_NEW')), by='rgn_id', all=T)
+    d_ck[['dif']] = d_ck[['pressure_score_NEW']] - d_ck[['pressure_score']]
+    write.csv(d_ck, sprintf('%s/data/%s_%d_NEW-DIF.csv', dir_wd, pressure, yr))
+    p[i,sprintf('dif_mean_%d',yr)] = mean(d_ck[['dif']], na.rm=T)
+    p[i,sprintf('dif_min_%d',yr)]  =  min(d_ck[['dif']], na.rm=T)
+    p[i,sprintf('dif_max_%d',yr)]  =  max(d_ck[['dif']], na.rm=T)            
   } 
 }
 
-write.csv(p, '_model_rasters_output_check.csv', row.names=F, na='')
+write.csv(p, file.path(dir_wd, 'tmp/_model_rasters_output_NEW-DIF.csv'), row.names=F, na='')
