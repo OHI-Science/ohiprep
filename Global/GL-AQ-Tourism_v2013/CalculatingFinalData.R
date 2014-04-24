cfb44b8b7ba4345693ff66efd347d683a7045782
Melsteroni <frazier@nceas.ucsb.edu>
2014-04-24 08:49:04 -0700

    Tourism data analysis

--- Global/GL-AQ-Tourism_v2013/CalculatingFinalData.R
+++ Global/GL-AQ-Tourism_v2013/CalculatingFinalData.R
@@ -9,24 +9,33 @@ library(dplyr)
 
 rm(list = ls())
 
-setwd("C:\\Users\\Melanie\\Desktop\\AQ-TourismData_v2013")
+setwd("/var/data/ohi/model/GL-AQ-Tourism_v2013")
 
 
 # Load in data ----
 sites <- read.csv("tmp/Sites_CCAMLR.csv")
 tourists <- read.csv("raw/TR_data.csv")
+area <- read.csv("/var/data/ohi/git-annex/Global/NCEAS-Regions_v2014/data/sp_inland1km_ccamlr_gcs_data.csv")
+labels <- read.csv("/var/data/ohi/git-annex/Global/NCEAS-Antarctica-Other_v2014/rgn_labels_ccamlr.csv")
+
 
 # Check for consistency among names:
 setdiff(tourists$Site_Name, sites$Site_name) # Ok:  these three sites were cut because they were outside the CCAMLR regions
 setdiff(sites$Site_name, tourists$Site_Name)
 
 sites <- sites %.%
-  select("Site_Name"=Site_name, rgn_id, F_CODE)
+  select("Site_Name"=Site_name, sp_id)
 
 
 tourists_summary <- tourists %.%
-  inner_join(sites) %.%
-  group_by(F_CODE, rgn_id, year)%.%
-  summarise(Total=sum(Total))
+  inner_join(sites, by="Site_Name") %.%
+  group_by(sp_id, year)%.%
+  summarise(TotalTourists=sum(Total))
 
-write.csv(tourists_summary, "tmp/rgn_year_TR.csv", row.names=FALSE)
+# merge with areas to get density of tourists for each region
+area <- subset(area, select=c(sp_id, area_km2))
+
+tourists_summary <- merge(tourists_summary, area, by="sp_id")
+tourists_summary$Tourists_per_km2 <- tourists_summary$TotalTourists/tourists_summary$area_km2 
+
+write.csv(tourists_summary, "rgn_year_TR.csv", row.names=FALSE)
--- Global/GL-AQ-Tourism_v2013/dataPrep/OverlayCoordinateData.R
+++ Global/GL-AQ-Tourism_v2013/dataPrep/OverlayCoordinateData.R
@@ -13,13 +13,14 @@ library(colorspace)
 
 rm(list = ls())
 
-setwd("C:\\Users\\Melanie\\Desktop\\GL-AQ-Tourism_v2013")
+setwd("/var/data/ohi/model/GL-AQ-Tourism_v2013")
 
 
 # Load in data ----
-sites <- read.csv("tmp\\sites_dd.csv")
-map <- readOGR(dsn="N:\\git-annex\\Global\\NCEAS-Regions_v2014\\data", layer="sp_gcs")
-
+sites <- read.csv("tmp/sites_dd.csv")
+map <- readOGR(dsn="/var/data/ohi/git-annex/Global/NCEAS-Regions_v2014/data", layer="sp_gcs")
+map <- map[map@data$sp_type %in% c("eez-ccamlr", "land-ccamlr"), ]
+plot(map)
 
 # convert site data to spatial dataframe
 coordinates(sites) <- ~longitude_dd+latitude_dd
@@ -32,21 +33,14 @@ plot(map)
 plot(sites, add=TRUE, cex=.5, col="red", pch=16)
 dev.off()
 
-map_AQ <- map[map$rgn_name %in% "Antarctica",]
-map_AQ@data$rgn_id <- map_AQ@data$rgn_id[drop=TRUE]
-png("AntarcticaMap.png", units="cm", width=15, height=15, res=300)
-spplot(map_AQ, c("rgn_id"), col.regions=diverge_hsv(19))
-dev.off()
 
 # overlay points ----
 sites_CCAMLR <- over(sites, map)
-
-sites_CCAMLR <- data.frame(data.frame(sites), sites_CCAMLR) 
-sites_CCAMLR <- subset(sites_CCAMLR, select=c(Site_name, rgn_id, latitude_dd, longitude_dd, F_CODE, rgn_type))
+sites_CCAMLR <- data.frame(data.frame(sites), sites_CCAMLR) # add sites data
 
 
 # check on NA sites:
-sites_NA <- sites[is.na(sites_CCAMLR$F_CODE), ]
+sites_NA <- sites[is.na(sites_CCAMLR$sp_id), ]
 
 png("AntarcticaMap_NApoints.png", units="cm", width=15, height=15, res=300)
 plot(map)
@@ -54,5 +48,8 @@ plot(sites_NA, add=TRUE, cex=1.3, col="red", pch=16)
 dev.off()
 
 # cut sites outside of CCAMLR:
-sites_CCAMLR <- sites_CCAMLR[!is.na(sites_CCAMLR$F_CODE), ]
-write.csv(sites_CCAMLR, "Sites_CCAMLR.csv", row.names=FALSE)
+sites_CCAMLR <- sites_CCAMLR[!is.na(sites_CCAMLR$sp_id), ]
+
+sites_CCAMLR <- subset(sites_CCAMLR, select=c(Site_name, sp_id, latitude_dd, longitude_dd, rgn_type))
+
+write.csv(sites_CCAMLR, "tmp/Sites_CCAMLR.csv", row.names=FALSE)
