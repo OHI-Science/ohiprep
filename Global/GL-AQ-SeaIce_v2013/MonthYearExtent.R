1a32962d556cc28f523cd5f0c64dba3c926d2ef2
Melsteroni <melanie.frazier@gmail.com>
2014-04-27 07:33:06 -0700

    Cleaning up sea ice analysis

--- Global/GL-AQ-SeaIce_v2013/MonthYearExtent.R
+++ Global/GL-AQ-SeaIce_v2013/MonthYearExtent.R
@@ -67,17 +67,6 @@ z.coverage$km2 <-  z.coverage$value*(pixel/1000)^2
 # select relevant variables
 z.coverage <- subset(z.coverage, select=c("zone", "year", "monthCode", "month", "km2"))
 
-# month average calculation
-monthlyAvg <- z.coverage %.%
-  filter(year<=final.year) %.%
-  group_by(zone, month) %.%
-  summarise(all_years_avg = mean(km2))
-
-# join with z.coverage
-z.coverage2 <- z.coverage %.%
-  left_join(monthlyAvg) %.%
-  mutate(anomoly=km2-all_years_avg)
-
 write.csv(z.coverage2, sprintf("tmp/%s_AQ_YearlyMonthlyIceCover.csv", p), row.names=FALSE)
   
 }
