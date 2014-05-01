3c402c14e3fbaf329fcfc3fb54911f84bd602493
Melsteroni <frazier@nceas.ucsb.edu>
2014-04-30 17:55:45 -0700

    General clean up

--- Global/HS_AQ_Pressures_2013/DataSummary.R
+++ Global/HS_AQ_Pressures_2013/DataSummary.R
@@ -39,14 +39,6 @@ regions_gcs@data$sp_id <- as.numeric(1:dim(regions_gcs@data)[1])
 # take a look at SST raster that I need to extract data from:
 SST <- raster("/var/data/ohi/model/GL-NCEAS-Pressures_v2013a/tmp/sst_05_10-82_86i_mol.tif")
 
-v <- extract(SST, regions_mol, weights=TRUE, progress="text")
-ldply(v, fun=)
-
-sapply(v, function(x) if (!is.null(x)) {sum(apply(x, 1, prod)) / sum(x[,2])} else NA  )
-
-
-SST_oldData <- read.dbf("/var/data/ohi/model/GL-NCEAS-Pressures_v2013a/tmp/rgn_fao_mol_sst_05_10-82_86i_mol.dbf")
-
 plot(SST)
 plot(regions_mol, add=TRUE)
 
@@ -65,16 +57,14 @@ data <- merge(regions_mol@data, regions_stats, all.y=TRUE, by.x="sp_id", by.y="z
 
 write.csv(data,"SST_ZonalMean.csv", 
           row.names=FALSE)  
-#NOTE: Need to standardize - but need to ask ben about a discrepancy between
-# the model_rasters_output.csv and the data results in the final table (cc_sst_2013.csv)
 
+#SST_oldData <- read.dbf("/var/data/ohi/model/GL-NCEAS-Pressures_v2013a/tmp/rgn_fao_mol_sst_05_10-82_86i_mol.dbf")
 
 
 ############################
 ## ACID ----
 ############################
 acid <- raster("/var/data/ohi/model/GL-NCEAS-Pressures_v2013a/tmp/masked_impacts_acid.tif")
-#acid_oldData <- read.dbf("/var/data/ohi/model/GL-NCEAS-Pressures_v2013a/tmp/rgn_fao_mol_sst_05_10-82_86i_mol.dbf")
 
 plot(acid)
 plot(regions_mol, add=TRUE)
@@ -95,41 +85,26 @@ data <- merge(regions_mol@data, regions_stats, all.y=TRUE, by.x="sp_id", by.y="z
 write.csv(data,"acid_ZonalMean.csv", 
           row.names=FALSE)  
 
+#acid_oldData <- read.dbf("/var/data/ohi/model/GL-NCEAS-Pressures_v2013a/tmp/rgn_fao_mol_sst_05_10-82_86i_mol.dbf")
 
 ############################
 ## UV ----
 ############################
 uv <- raster("/var/data/ohi/model/GL-NCEAS-Pressures_v2013a/tmp/impact_layers_2013_redo/impact_layers/work/uv/uv_baseline_anomaly/omi_aura_uv_anomaly_2008m01-2012m12_trans.tif")
-#uv_oldData <- read.dbf("/var/data/ohi/model/GL-NCEAS-Pressures_v2013a/tmp/rgn_fao_mol_sst_05_10-82_86i_mol.dbf")
 
 plot(uv)
 plot(regions_gcs, add=TRUE)
-
-#convert to raster
-rasterize(regions_gcs, uv, 
-          field="sp_id", 
-          filename="sp_gcs_raster_uv", overwrite=TRUE, 
-          progress="text")
-
-
-regions_raster <- raster("sp_gcs_raster_uv")
-
-regions_stats <- zonal(uv,  regions_raster, progress="text")
-
-data <- merge(regions_gcs@data, regions_stats, all.y=TRUE, by.x="sp_id", by.y="zone") 
-
-write.csv(data,"uv_ZonalMean.csv", 
-          row.names=FALSE)  
 
 ## extracting with polygons might be better due to low resolution of data.
 v <- extract(uv, regions_gcs, weights=TRUE, progress="text", df=TRUE)
 v2 <- v %.%
   group_by(ID) %.%
-  summarize(uv = sum(omi_aura_uv_anomaly_2008m01.2012m12_trans*weight))
+  summarize(mean = weighted.mean(omi_aura_uv_anomaly_2008m01.2012m12_trans, weight, na.rm=TRUE))
 v2 <- merge(regions_gcs@data, v2, all.y=TRUE, by.x="sp_id", by.y="ID") 
 write.csv(v2,"tmp//uv_ZonalMean.csv", 
           row.names=FALSE)  
 
+#uv_oldData <- read.dbf("/var/data/ohi/model/GL-NCEAS-Pressures_v2013a/tmp/rgn_fao_mol_sst_05_10-82_86i_mol.dbf")
 
 ############################
 ## Commercial High Bycatch
@@ -208,9 +183,6 @@ data <- merge(regions@data, regions_stats, all.y=TRUE, by.x="sp_id", by.y="zone"
 write.csv(data,"slr_ZonalMean.csv", 
           row.names=FALSE)                               
 
-test <- extract(slr, regions_gcs[25, ], fun=mean, na.rm=TRUE, weights=TRUE)
-test <- extract(slr, regions_gcs[25, ], fun=mean, na.rm=TRUE, weights=TRUE)
-
 
 
 ############################
@@ -235,43 +207,8 @@ regions_stats <- zonal(shipping,  regions_raster, progress="text")
 data <- merge(regions_mol@data, regions_stats, all.y=TRUE, by.x="sp_id", by.y="zone") 
 
 
-write.csv(data,"shipping.csv", 
+write.csv(data,"shipping_ZonalMean.csv", 
           row.names=FALSE)  
-
-
-
-
-#################################
-## Checking against old regions
-#################################
-
-regions_mol_old <- readOGR(dsn="/var/data/ohi/model/GL-NCEAS-OceanRegions_v2013a/data", layer="rgn_ocean_cntry_mol")
-regions_mol_old <- regions_mol_old[regions_mol_old$rgn_typ %in% "eez", ]
-
-# take a look at SST raster that I need to extract data from:
-SST <- raster("/var/data/ohi/model/GL-NCEAS-Pressures_v2013a/tmp/sst_05_10-82_86i_mol.tif")
-SST_oldData <- read.dbf("/var/data/ohi/model/GL-NCEAS-Pressures_v2013a/tmp/rgn_fao_mol_sst_05_10-82_86i_mol.dbf")
-
-plot(SST)
-plot(regions, add=TRUE)
-
-#convert to raster
-rasterize(regions_mol_old, SST, 
-          field="rgn_id", 
-          filename="sp_mol_raster_SST_old", overwrite=TRUE, 
-          progress="text")
-
-
-regions_raster <- raster("sp_mol_raster_SST_old")
-
-data <- zonal(SST,  regions_raster, progress="text")
-
-write.csv(data,"SST_ZonalMean_old.csv", 
-          row.names=FALSE)  
-
-#test <- extract(SST, regions_mol_old, fun=mean, na.rm=TRUE, weights=TRUE, progress="text")
-# results: na.rm did not seem to generally work.  However, the results did not 
-# seem overall better for the few regions that worked. 
 
 
 
