21df64290b1627805146f95917a3a8033e7512f4
Melsteroni <frazier@nceas.ucsb.edu>
2014-05-08 20:49:47 -0700

    Data extraction and calculations for Antartica Tourism and Recreation goal

--- Global/GL-AQ-Tourism_v2013/dataPrep/PreparingCoordinateData.R
+++ Global/GL-AQ-Tourism_v2013/dataPrep/PreparingCoordinateData.R
@@ -2,18 +2,18 @@
 ## Antarctica Tourism & Recreation Goal
 ## STEP 1: Preparing the data
 #################################################
-#Peter Oy Island  68°48?14?S, 90°35?35?W
-#Peter 1 Island 68°47S, 90°35W
+#Peter Oy Island  68?48?14?S, 90?35?35?W
+#Peter 1 Island 68?47S, 90?35W
 
 # McMurdo Ice Channel
-# 77°30'S, 162WE 
-# Changed to 166°06'E based on word document (ATCM35_att068_e.doc)
+# 77?30'S, 162WE 
+# Changed to 166?06'E based on word document (ATCM35_att068_e.doc)
 
 rm(list = ls())
 
-setwd("C:\\Users\\Melanie\\Desktop\\AQ-TourismData_v2013")
+setwd("/var/data/ohi/model/GL-AQ-Tourism_v2013")
 
-sites <- read.csv("Tourist site coordinates.csv")
+sites <- read.csv("raw/Tourist site coordinates.csv", stringsAsFactors=FALSE, fileEncoding="latin1", strip.white=TRUE)
 sites[is.na(sites$Coordinates), ]
 sites <- sites[!(sites$Site_name %in% "Unknown Name"), ]
 
@@ -22,8 +22,8 @@ sites$latitude <- sapply(strsplit(sites$Coordinates2, ","), function(x) x[1])
 sites$longitude <- sapply(strsplit(sites$Coordinates2, ","), function(x) x[2])
 
 # latitude
-sites$deg_latitude <- sapply(strsplit(sites$latitude, "°"), function(x) x[1])
-sites$deg_latitude_extra <- sapply(strsplit(sites$latitude, "°"), function(x) x[2])
+sites$deg_latitude <- sapply(strsplit(sites$latitude, "Â°"), function(x) x[1])
+sites$deg_latitude_extra <- sapply(strsplit(sites$latitude, "Â°"), function(x) x[2])
 sites$min_latitude <- sapply(strsplit(sites$deg_latitude_extra, "'"), function(x) x[1])
 sites$region_latitude <- sapply(strsplit(sites$deg_latitude_extra, "'"), function(x) x[2])
 
@@ -35,8 +35,8 @@ table(as.factor(sites$region_latitude))
 sites$latitude_dd <- (sites$deg_latitude + sites$min_latitude/60) * ifelse(sites$region_latitude=="S", -1, 1)
 
 # longtitude
-sites$deg_longitude <- sapply(strsplit(sites$longitude, "°"), function(x) x[1])
-sites$deg_longitude_extra <- sapply(strsplit(sites$longitude, "°"), function(x) x[2])
+sites$deg_longitude <- sapply(strsplit(sites$longitude, "Â°"), function(x) x[1])
+sites$deg_longitude_extra <- sapply(strsplit(sites$longitude, "Â°"), function(x) x[2])
 sites$min_longitude <- sapply(strsplit(sites$deg_longitude_extra, "'"), function(x) x[1])
 sites$region_longitude <- sapply(strsplit(sites$deg_longitude_extra, "'"), function(x) x[2])
 
@@ -50,5 +50,5 @@ sites$longitude_dd <- (sites$deg_longitude + sites$min_longitude/60) * ifelse(si
 
 ## save data
 sites <- subset(sites, select=c(Site_name, latitude_dd, longitude_dd))
-write.csv(sites, "sites_dd.csv", row.names=FALSE)
+#write.csv(sites, "tmp/sites_dd.csv", row.names=FALSE)
 
