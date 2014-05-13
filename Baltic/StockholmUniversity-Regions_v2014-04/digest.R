c3bbce30b94fb800abeb809661c4bd64b6fe63e6
Ben Best <bbest@nceas.ucsb.edu>
2014-05-13 04:06:07 -0700

    fixed Baltic regions
    ohi-science/ohicore#75

--- Baltic/StockholmUniversity-Regions_v2014-04/digest.R
+++ Baltic/StockholmUniversity-Regions_v2014-04/digest.R
@@ -6,22 +6,17 @@ library(dplyr)
 library(tools)
 
 # paths
-shp_in   = 'N:/git-annex/Baltic/StockholmUniversity-Regions_v2014-04/data/rgn_gcs.shp'
-json_out = 'N:/git-annex/Baltic/StockholmUniversity-Regions_v2014-04/data/rgn_gcs.geojson'
+shp_in      = 'N:/git-annex/Baltic/StockholmUniversity-Regions_v2014-04/data/rgn_gcs.shp'
+geojson_out = 'N:/git-annex/Baltic/StockholmUniversity-Regions_v2014-04/data/rgn_gcs.geojson'
+js_out      = 'N:/git-annex/Baltic/StockholmUniversity-Regions_v2014-04/data/rgn_gcs.js'
 
 # read and write shapefile
-#sp::set_ll_warn(TRUE) # convert error to warning about exceeding longlat bounds
 x = readShapeSpatial(shp_in, proj4string=sp::CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'))
 x = subset(x, rgn_type=='eez')
 flds = c('rgn_id'='rgn_id', 'rgn_nam'='rgn_name') #flds = c('rgn_id'='region_id', 'rgn_nam'='region_nam') # too long for shapefile columns
 x@data = rename(x@data[,names(flds)], flds) # drop other fields  
-writeOGR(x, dsn=json_out, layer=basename(file_path_sans_ext(json_out)), driver='GeoJSON')
-rgdal::writeOGR(x, dsn=shp.to, layer=basename(file_path_sans_ext(shp.to)), driver='ESRI Shapefile')  
-fw = file(js.to, 'wb')
+writeOGR(x, dsn=json_out, layer=basename(file_path_sans_ext(geojson_out)), driver='GeoJSON')
+fw = file(js_out, 'wb')
 cat('var regions = ', file=fw)
-cat(readLines(geojson.to, n = -1), file=fw)
-close(fw)
-
-# copy other reference files
-#rgn_simple_gcs.shp
-  
+cat(readLines(geojson_out, n = -1), file=fw)
+close(fw)  
