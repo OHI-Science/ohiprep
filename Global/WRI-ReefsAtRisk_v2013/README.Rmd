ee890a39f3216dfda9431fc61e5c42d1aa86f17d
bbest <bbest@nceas.ucsb.edu>
2014-06-10 12:35:01 -0700

    finished blast and cyanide fishing extraction to 2013 regions. ohi-science/ohicore#34

---
+++ Global/WRI-ReefsAtRisk_v2013/README.Rmd
@@ -0,0 +1,10 @@
+Global WRI Reefs at Risk
+========================================================
+
+Average value of 3 nm cells per region from 1km raster of raw impact scores in Halpern (2008).
+
+## 2013
+Extracted from 2013 regions.
+
+## 2014
+TODO...
--- Global/WRI-ReefsAtRisk_v2013/digest.R
+++ Global/WRI-ReefsAtRisk_v2013/digest.R
@@ -1,31 +1,22 @@
-source('src/R/common.R')
+library(raster) # overwrites dplyr select
 
-library(raster)
+source('src/R/common.R') # dir_neptune_data
+dir_d = 'Global/WRI-ReefsAtRisk_v2013'
 
-r.tif = file.path(dir_neptune_data, 'model/GL-NCEAS-OceanRegions_v2013a/data/rgn_mol.tif')
-b.tif = file.path(dir_neptune_data, 'model/GL-WRI-ReefsAtRisk_Distance/data/gl_thr_blast_3nm.tif')
-p.tif = file.path(dir_neptune_data, 'model/GL-WRI-ReefsAtRisk_Distance/data/gl_thr_poison_3nm.tif')
+# read in rasters
+r13 = raster(file.path(dir_neptune_data, 'model/GL-NCEAS-OceanRegions_v2013a/data/rgn_mol.tif'))
+b = raster(file.path(dir_neptune_data, 'model/GL-WRI-ReefsAtRisk_Distance/data/gl_thr_blast_3nm.tif'))
+p = raster(file.path(dir_neptune_data, 'model/GL-WRI-ReefsAtRisk_Distance/data/gl_thr_poison_3nm.tif'))
 
-# raster functions: raster, rasterize, extract, rasterToPoints, zonal
-r = raster(r.tif)
-b = raster(b.tif)
-p = raster(b.tif)
+# calculate means per region (SLOW: takes 9.5 min each)
+b_z = zonal(b, r13, 'mean')
+p_z = zonal(p, r13, 'mean')
 
-#r_p = rasterToPoints(r) # SLOW!
-system.time({ b_z = zonal(b, r, 'mean') })
-system.time({ p_z = zonal(p, r, 'mean') })
-
-zonal_dt = function (x, z, stat = "mean", digits = 0, na.rm = TRUE, ...) {
-  # zonal function using 10x faster data.table package
-  #   source: http://r-sig-geo.2731867.n2.nabble.com/Alternative-to-zonal-for-large-images-tt7582580.html#a7582593
-  library(data.table)
-  fun   = match.fun(stat) 
-  vals  = getValues(x) 
-  zones = round(getValues(z), digits = digits) 
-  rDT   = data.table(vals, z=zones) 
-  setkey(rDT, z) 
-  rDT[, lapply(.SD, fun), by=z] 
-} 
-
-system.time({ b_z_dt = zonal_dt(b, r, 'mean') })
-system.time({ p_z_dt = zonal_dt(p, r, 'mean') })
+# output
+dir.create(file.path(dir_d, 'data'))
+write.csv(dplyr::select(data.frame(b_z), rgn_id=zone, p_rank=mean), 
+          file.path(dir_d, 'data/gl_thr_blast_3nm_rgn2013.csv'), 
+          row.names=F, na='')
+write.csv(dplyr::select(data.frame(p_z), rgn_id=zone, p_rank=mean), 
+          file.path(dir_d, 'data/gl_thr_poison_3nm_rgn2013.csv'),
+          row.names=F, na='')
