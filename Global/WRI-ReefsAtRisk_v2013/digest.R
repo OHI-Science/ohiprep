ba23a9e80cfa7d8b20436ed03a87d90f0fac73ef
bbest <bbest@nceas.ucsb.edu>
2014-06-10 14:00:05 -0700

    updated blast and poison to true pressures score

--- Global/WRI-ReefsAtRisk_v2013/digest.R
+++ Global/WRI-ReefsAtRisk_v2013/digest.R
@@ -1,22 +1,37 @@
-library(raster) # overwrites dplyr select
-
 source('src/R/common.R') # dir_neptune_data
+library(raster)
+select = dplyr::select
+
+# vars
 dir_d = 'Global/WRI-ReefsAtRisk_v2013'
-
-# read in rasters
 r13 = raster(file.path(dir_neptune_data, 'model/GL-NCEAS-OceanRegions_v2013a/data/rgn_mol.tif'))
-b = raster(file.path(dir_neptune_data, 'model/GL-WRI-ReefsAtRisk_Distance/data/gl_thr_blast_3nm.tif'))
-p = raster(file.path(dir_neptune_data, 'model/GL-WRI-ReefsAtRisk_Distance/data/gl_thr_poison_3nm.tif'))
+lyrs = list(blast   =list(tif='gl_thr_blast_3nm.tif'),
+            poision =list(tif='gl_thr_blast_3nm.tif'))
 
-# calculate means per region (SLOW: takes 9.5 min each)
-b_z = zonal(b, r13, 'mean')
-p_z = zonal(p, r13, 'mean')
+dir.create(file.path(dir_d, 'data'), showWarnings=F)
 
-# output
-dir.create(file.path(dir_d, 'data'))
-write.csv(dplyr::select(data.frame(b_z), rgn_id=zone, p_rank=mean), 
-          file.path(dir_d, 'data/gl_thr_blast_3nm_rgn2013.csv'), 
-          row.names=F, na='')
-write.csv(dplyr::select(data.frame(p_z), rgn_id=zone, p_rank=mean), 
-          file.path(dir_d, 'data/gl_thr_poison_3nm_rgn2013.csv'),
-          row.names=F, na='')
+for (i in 1:length(lyrs)){ # i=1
+  
+  # vars
+  lyr = names(lyrs)[i]  
+  tif = file.path(dir_neptune_data, 'model/GL-WRI-ReefsAtRisk_Distance/data', lyrs[[i]][['tif']])
+  csv = sprintf('%s/data/%s_rgn2013.csv', dir_d,  tools::file_path_sans_ext(basename(tif)))
+  
+  # zonal
+  r = raster(tif)
+  z = zonal(r, r13, 'mean')
+
+  if (lyr=='blast'){
+    z = b_z
+  } else {
+    z = p_z
+  }
+  
+  # get score
+  d = data.frame(z) %.%    
+    filter(mean > 1) %.%
+    mutate(score = mean - 1) %.%
+    select(rgn_id=zone, score)
+  
+  write.csv(d, csv, row.names=F, na='')  
+}
