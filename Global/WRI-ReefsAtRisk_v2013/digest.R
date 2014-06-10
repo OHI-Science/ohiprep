9c5f6da6c50465adb0fe18c562c48ec1e3359310
bbest <bbest@nceas.ucsb.edu>
2014-06-10 14:26:51 -0700

    updated poison (was just blast)

--- Global/WRI-ReefsAtRisk_v2013/digest.R
+++ Global/WRI-ReefsAtRisk_v2013/digest.R
@@ -3,35 +3,27 @@ library(raster)
 select = dplyr::select
 
 # vars
-dir_d = 'Global/WRI-ReefsAtRisk_v2013'
+tifs = sprintf('%s/model/GL-WRI-ReefsAtRisk_Distance/data/%s', 
+               dir_neptune_data,  
+               c('gl_thr_blast_3nm.tif', 'gl_thr_poison_3nm.tif'))
+dir_out = 'Global/WRI-ReefsAtRisk_v2013'
+
 r13 = raster(file.path(dir_neptune_data, 'model/GL-NCEAS-OceanRegions_v2013a/data/rgn_mol.tif'))
-lyrs = list(blast   =list(tif='gl_thr_blast_3nm.tif'),
-            poision =list(tif='gl_thr_blast_3nm.tif'))
+dir.create(file.path(dir_o, 'data'), showWarnings=F)
 
-dir.create(file.path(dir_d, 'data'), showWarnings=F)
-
-for (i in 1:length(lyrs)){ # i=1
-  
-  # vars
-  lyr = names(lyrs)[i]  
-  tif = file.path(dir_neptune_data, 'model/GL-WRI-ReefsAtRisk_Distance/data', lyrs[[i]][['tif']])
-  csv = sprintf('%s/data/%s_rgn2013.csv', dir_d,  tools::file_path_sans_ext(basename(tif)))
+for (tif in tifs){ # tif = tifs[2]
   
   # zonal
   r = raster(tif)
-  z = zonal(r, r13, 'mean')
+  z = zonal(r, r13, 'mean') # SLOW: ~9 min ea 
 
-  if (lyr=='blast'){
-    z = b_z
-  } else {
-    z = p_z
-  }
-  
-  # get score
+  # score
   d = data.frame(z) %.%    
     filter(mean > 1) %.%
     mutate(score = mean - 1) %.%
     select(rgn_id=zone, score)
   
+  # write
+  csv = sprintf('%s/data/%s_rgn2013.csv', dir_out,  tools::file_path_sans_ext(basename(tif)))
   write.csv(d, csv, row.names=F, na='')  
 }
