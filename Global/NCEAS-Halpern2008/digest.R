ffc3d3cafcadef6f43d04c9211b2ab4a1389340a
bbest <bbest@nceas.ucsb.edu>
2014-06-23 08:24:06 -0700

    extracting Halpern2008 pressure and habitat rasters on 2014 OHI regions OHI-Science/ohicore#34

--- Global/NCEAS-Halpern2008/digest.R
+++ Global/NCEAS-Halpern2008/digest.R
@@ -1,25 +1,55 @@
 # get zonal stats on rasters
 
+# much faster on https://neptune.nceas.ucsb.edu/rstudio/, ~ 15 min ea
+
+# started ~ 3am
+
+# TODO: git mv NCEAS-Halpern2008 NCEAS-Halpern2008_v2014
+#       check whether zone really is rgn_id
+
 library(raster)
 library(rgdal) # on Mac with R 3.1: http://cran.r-project.org/bin/macosx/contrib/3.1/rgdal_0.8-16.tgz
+library(stringr)
+library(reshape2)
+library(plyr)
+library(dplyr)
 
 source('src/R/common.R')
 
-r = raster('C:/tmp/Global/NCEAS-Regions_v2014/data/rgn_offshore_mol.tif')
-p = raster('C:/tmp/Global/NCEAS-Halpern2008/tmp/masked_ecosystems_coral_reef.tif') # file.path(dir_neptune_data, 'model/GL-NCEAS-Halpern2008/data
-m = raster('C:/tmp/Global/NCEAS-Halpern2008/tmp/masked_model.tif') # copied from N:/model/GL-NCEAS-Halpern2008/data/
+dir_out = 'Global/NCEAS-Halpern2008/data'
+
+r = raster(file.path(dir_neptune_data, 'git-annex/Global/NCEAS-Regions_v2014/data/rgn_offshore_mol.tif'))
+p = raster(file.path(dir_neptune_data, 'model/GL-NCEAS-Halpern2008/data/masked_ecosystems_coral_reef.tif'))
+
+if (!file.exists(dir_out)) dir.create(dir_out, showWarnings=F)
 
 tifs = list.files(file.path(dir_neptune_data, 'model/GL-NCEAS-Halpern2008/data'), pattern=glob2rx('*.tif'))
-for (i in 1:length(tifs)){
-  p = tifs[i]
-  cat(sprintf('%02d of %d: %s (%s)', i, basename(p), )))
-  print(system.time({
-    z = zonal(r, p, progress="text") }))
+
+for (i in 3:length(tifs)){ # i=1
   
-  data <- merge(regions_mol@data, regions_stats, all.y=TRUE, by.x="sp_id", by.y="zone") 
+  f = tifs[i]
+  csv = sprintf('%s/%s.csv', dir_out, str_replace(tools::file_path_sans_ext(f), 'masked_', '') )
+  cat(sprintf('%02d of %d: %s (%s)\n', i, length(tifs), f, Sys.time()))
   
-  write.csv(data,"SST_ZonalMean.csv", 
-            row.names=FALSE)  
+  p = raster(sprintf('%s/model/GL-NCEAS-Halpern2008/data/%s', dir_neptune_data, tifs[i]))
+  
+  if (str_detect(f, '^masked_ecosystems_')){
+    
+    z = zonal(p, r, fun='sum', progress='text') %>%
+      as.data.frame() %>%
+      mutate(area_km2 = sum * res(p)[1]^2 / 1000^2 ) %>%  # convert ncells to km^2, cellsize of 934.48 m 
+      select(rgn_id=zone, area_km2) %>%
+      arrange(rgn_id)
+    
+  } else {
+    
+    z = zonal(p, r, fun='mean', progress='text') %>%
+      as.data.frame() %>%
+      select(rgn_id=zone, mean) %>%
+      arrange(rgn_id)
+    
+  }
+  
+  write.csv(z, csv, na='', row.names=F)
   
 }
-
