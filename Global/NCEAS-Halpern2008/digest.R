74cd26cd3f5bc4bd6fd677ced5e287c250cc372d
Ben Best <bbest@nceas.ucsb.edu>
2014-06-23 01:04:28 -0700

    fixing rasters of NCEAS-Regions
    to be registered to same cellsize, extent, and snapraster as Halpern2008

--- Global/NCEAS-Halpern2008/digest.R
+++ Global/NCEAS-Halpern2008/digest.R
@@ -1,26 +1,25 @@
-# need to align rasters in model/GL-NCEAS-Halpern2008 to new regions:
-# 
-# Switching to ArcGIS on Win, b/c in R on Mac:
-# ```
-# z = zonal(r,  rgn_offshore_mol, progress="text")
-# Error in compareRaster(c(x, z)) : different extent
-# ```
+# get zonal stats on rasters
 
 library(raster)
 library(rgdal) # on Mac with R 3.1: http://cran.r-project.org/bin/macosx/contrib/3.1/rgdal_0.8-16.tgz
 
 source('src/R/common.R')
 
-rgn_offshore_mol = raster(file.path(dir_neptune_data,'git-annex/Global/NCEAS-Regions_v2014/data/rgn_offshore_mol.tif'))
+r = raster('C:/tmp/Global/NCEAS-Regions_v2014/data/rgn_offshore_mol.tif')
+p = raster('C:/tmp/Global/NCEAS-Halpern2008/tmp/masked_ecosystems_coral_reef.tif') # file.path(dir_neptune_data, 'model/GL-NCEAS-Halpern2008/data
+m = raster('C:/tmp/Global/NCEAS-Halpern2008/tmp/masked_model.tif') # copied from N:/model/GL-NCEAS-Halpern2008/data/
 
-list.files(file.path(dir_neptune_data, 'model/GL-NCEAS-Halpern2008/data'), pattern=glob2rx('*.tif'))
+tifs = list.files(file.path(dir_neptune_data, 'model/GL-NCEAS-Halpern2008/data'), pattern=glob2rx('*.tif'))
+for (i in 1:length(tifs)){
+  p = tifs[i]
+  cat(sprintf('%02d of %d: %s (%s)', i, basename(p), )))
+  print(system.time({
+    z = zonal(r, p, progress="text") }))
+  
+  data <- merge(regions_mol@data, regions_stats, all.y=TRUE, by.x="sp_id", by.y="zone") 
+  
+  write.csv(data,"SST_ZonalMean.csv", 
+            row.names=FALSE)  
+  
+}
 
-r = raster(file.path(dir_neptune_data, 'model/GL-NCEAS-Halpern2008/data/masked_ecosystems_coral_reef.tif'))
-
-extent(rgn_offshore_mol)
-extent(r)
-
-#a = alignExtent(r, extent(rgn_offshore_mol))
-r_r = resample(r, rgn_offshore_mol, method='bilinear') # 10:24
-
-z = zonal(r,  rgn_offshore_mol, progress="text")
