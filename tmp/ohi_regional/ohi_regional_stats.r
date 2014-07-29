531baa6c360cdd213fd2342748a8b419d5e14bd7
jules32 <jstewart@nceas.ucsb.edu>
2014-07-29 17:20:04 -0500

    redid coastal pop and jobs using 2014 data

--- tmp/ohi_regional/ohi_regional_stats.r
+++ tmp/ohi_regional/ohi_regional_stats.r
@@ -5,7 +5,7 @@
 
 ## setup ----
 source('../ohiprep/src/R/common.R') # set dir_neptune_data
-# library(stringr)
+library(stringr)
 library(tools)
 # library(reshape2)
 
@@ -14,7 +14,7 @@ c = read.csv(file.path(dir_neptune_data, 'model/GL-NCEAS-CoastalPopulation_v2013
                        'cntry_popsum2013_inland25mi_complete.csv')); head(c)
 
 
-## read in regional assessments ----
+## read in regional assessments regions ----
 nonCountry = c('U.S. West Coast', 'Baltic Sea', 'Hawaii')
 
 RAs = read.csv('../ohiprep/tmp/ohi_regional/rgns_regional.csv') %>%
@@ -28,48 +28,22 @@ RAs = read.csv('../ohiprep/tmp/ohi_regional/rgns_regional.csv') %>%
   select(rgn_nam, rgn_id, status) %>%
   arrange(status, rgn_nam); head(RAs)
 
-# read in layers of interest from layers.csv
-loi = list(
-  'area_eez'     = 'rgn_area',
-  'coastal_jobs' = 'le_jobs_cur_adj_value',
-  'coastal_rev'  = 'le_rev_cur_adj_value')
+# read in layers from layers.csv ----
+loi = list('area_eez' = 'rgn_area') # layers of interest
 
 layers = read.csv('../ohi-global/eez2013/layers.csv') %>%
   filter(layer %in% loi)
 
-
-# loop through, read in and pull out only the regional assessments ---- 
+# loop through from layers.csv
 if (exists('d')) rm(d)
 for (i in 1:length(layers$filename)) { # i=1
   
   l = read.csv(file.path('../ohi-global/eez2013/layers', layers$filename[i])); head(l)
   nam = names(loi)[loi == file_path_sans_ext(layers$filename[i])]
-  
-  # rename 'value' field
   names(l)[!names(l) %in% c('rgn_id', 'cntry_key', 'sector', 'category')] = 'value'
   
-  # save a coyp
-  l2 = l 
-  
-  # if cntry_key, translate to rgn_id
-  if ('cntry_key' %in% names(l)){
-    
-    lf = l %>%
-      left_join(c,
-                by = 'cntry_key') %>% 
-      filter(!cntry_key %in% c('ATA', 'ANT'))    
-    lf$rgn_id[lf$cntry_key == 'ABW'] = 250 # Aruba
-    lf$rgn_id[lf$cntry_key == 'BRN'] = 247 # Brunei
-    lf$rgn_id[lf$cntry_key == 'HKG'] = 209 # Hong Kong 
-    lf$rgn_id[lf$cntry_key == 'MNE'] = 186 # Montenegro
-    lf$rgn_id[lf$cntry_key == 'MYS'] = 206 # Malaysia
-    
-    l2 = lf %>%
-      select(rgn_id, sector, value)        
-  }
-  
   # sum by rgn_id, add layer indicator
-  h = l2 %>% 
+  h = l %>% 
     group_by(rgn_id) %>%
     summarize(value = sum(value, na.rm=T)) %>% # just need to sum by rgn_id
     mutate(layer = nam) %>%
@@ -84,6 +58,48 @@ for (i in 1:length(layers$filename)) { # i=1
 }
 head(d)
 
+
+## read in LE 2014 data ----
+
+dirLE = '../ohiprep/Global/NCEAS-Livelihoods_v2014/data'
+
+# loop through from LE 2014
+if (exists('w')) rm(w)
+for (j in list.files(dirLE, pattern=glob2rx('*.csv'), full.names=T)){ # j="../ohiprep/Global/NCEAS-Livelihoods_v2014/data/cntry_jobs_2014a.csv"
+  
+  l = read.csv(j); head(l)
+  
+  # rename 'value' field
+  nam = str_split_fixed(basename(file_path_sans_ext(j)), '_', 2)[2]
+#   nam = names(l)[!names(l) %in% c('rgn_id', 'cntry_key', 'sector', 'category', 'year')] 
+  names(l)[!names(l) %in% c('rgn_id', 'cntry_key', 'sector', 'category', 'year')] = 'value'
+ 
+  # if cntry_key, translate to rgn_id
+  if ('cntry_key' %in% names(l)){
+    
+    lf = l %>%
+      left_join(c,
+                by = 'cntry_key') %>% 
+      filter(!cntry_key %in% c('ATA')) %>% # Antarctica
+      select(rgn_id, sector, value)        
+  }
+  
+  # sum by rgn_id, add layer indicator
+  h = lf %>% 
+    group_by(rgn_id) %>%
+    summarize(value = sum(value, na.rm=T)) %>% # just need to sum by rgn_id
+    mutate(layer = nam) %>%
+    arrange(rgn_id); head(h); summary(h) 
+  
+  # rbind
+  if (!exists('w')){
+    w = h
+  } else {
+    w = rbind(w, h)
+  }
+}
+head(w)
+
 ## coastal pop and coastal inland area from Neptune ----
 cp = c %>%
   select(rgn_id, 
@@ -95,22 +111,22 @@ ca = c %>%
          value = area_km2) %>%
   mutate(layer = 'area_inland25km')
 
+# coastline length from Neptune
 cl = read.csv(file.path(dir_neptune_data, 'model/GL-NCEAS-OceanRegions_v2013a/data', 
                         'rgn_area_inland1km.csv')) %>%
   select(rgn_id, 
          value = area_km2) %>%
   mutate(layer = 'area_inland1km')
 
-# coastline length from Neptune
-#...must id this file
-
 
 ## combine all data layers and 
-h_g = rbind(d, ca, cp, cl) %>%
+h_g = rbind(d, w, ca, cp, cl) %>%
   group_by(rgn_id, layer) %>%
   summarize(value = sum(value, na.rm = T)) %>% # stopifnot(anyDuplicated(h_g[,c('rgn_id', 'layer')]) == 0)
   dcast(rgn_id ~ layer, value.var = 'value') %>%
-  select(rgn_id, area_eez, area_inland1km, area_inland25km, coastal_pop, coastal_jobs, coastal_rev); head(h_g)
+  select(rgn_id, area_eez, area_inland1km, coastal_pop, 
+         coastal_jobs = jobs_2014a, 
+         coastal_rev  = rev_2014a); head(h_g)
 
 
 
@@ -126,15 +142,16 @@ write.csv(hb_globalsum, paste(f_out, '_globalsum.csv', sep = ''), row.names = F,
 h_ra = h_g %>%
   filter(rgn_id %in% RAs$rgn_id) %>%
   left_join(RAs, by = 'rgn_id') %>%
-  select(rgn_nam, status, area_eez, area_inland1km, area_inland25km, coastal_pop, coastal_jobs, coastal_rev); head(h_ra)
+  select(rgn_nam, status, area_eez, area_inland1km, coastal_pop, coastal_jobs, coastal_rev); head(h_ra)
 
 
-# add 2 eezs by hand from Wikipedia: http://en.wikipedia.org/wiki/Exclusive_economic_zone#United_States
+# add 2 eezs by hand: 
+# area_eez from Wikipedia: http://en.wikipedia.org/wiki/Exclusive_economic_zone#United_States
 h_ra2 = rbind(h_ra, 
             data.frame(rgn_nam = 'US West Coast', status='current', area_eez=895346+1579538, 
-                       area_inland1km=NA, area_inland25km=NA, coastal_jobs=NA, coastal_pop=NA, coastal_rev=NA),
+                       area_inland1km=NA, coastal_jobs=NA, coastal_pop=NA, coastal_rev=NA),
             data.frame(rgn_nam = 'Hawaii', status='current', area_eez=825549, 
-                       area_inland1km=NA, area_inland25km=NA, coastal_jobs=NA, coastal_pop=NA, coastal_rev=NA)) %>%
+                       area_inland1km=NA, coastal_jobs=NA, coastal_pop=NA, coastal_rev=NA)) %>%
   arrange(status, rgn_nam); h_ra2
 
 write.csv(h_ra2, paste(f_out, '.csv', sep = ''), row.names = F, na = '')
