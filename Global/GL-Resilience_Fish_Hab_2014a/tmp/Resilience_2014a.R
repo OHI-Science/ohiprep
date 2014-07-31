17c5fd51a14bca7aa63e4965d016e16ed6ee6802
katlongo <longo@nceas.ucsb.edu>
2014-07-31 00:12:37 -0700

    separated resilience output into individual files

--- Global/GL-Resilience_Fish_Hab_2014a/tmp/Resilience_2014a.R
+++ Global/GL-Resilience_Fish_Hab_2014a/tmp/Resilience_2014a.R
@@ -10,18 +10,21 @@ library(ohicore)
 source('../ohiprep/src/R/common.R') # set dir_neptune_data
 source('../ohiprep/src/R/ohi_clean_fxns.R') # name-to-region functions (but need to add new saup names files)
 
-dir_d = '../ohiprep/Global/GL-Resilience_Fish_Hab_2014a/tmp' # set folder where files are saved
+dir_d = '../ohiprep/Global/GL-Resilience_Fish_Hab_2014a' # set folder where files are saved
 MPA_data = '../ohiprep/Global/WDPA-MPA_v2014/data'
 rgn_data = '../ohiprep/tmp/install_local/ohi-global-master/eez2014/layers'
 
 # following files 'as is' from Nature 2012 (disaggregated for 'children' new ohi2013 regions) - no year field bc no updates
-r_hab<-read.csv(file.path(dir_d, 'r_habitat_2013a.csv')) ; head(r_hab)
-r_mora<-read.csv(file.path(dir_d, 'r_mora_2013a.csv'))  ; head(r_mora)
-r_mora_s4<-read.csv(file.path(dir_d, 'r_mora_s4_2013a.csv')) ; head(r_mora_s4) # a table with the list of layers that apply to each resilience layer
+r_hab<-read.csv(file.path(dir_d, 'tmp/r_habitat_2013a.csv')) ; head(r_hab)
+r_mora<-read.csv(file.path(dir_d, 'tmp/r_mora_2013a.csv'))  ; head(r_mora)
+r_mora_s4<-read.csv(file.path(dir_d, 'tmp/r_mora_s4_2013a.csv')) ; head(r_mora_s4) # a table with the list of layers that apply to each resilience layer
 
-r_combos<-read.csv(file.path(dir_d, 'r_combos.csv')) ; head(r_combos)
+r_combos<-read.csv(file.path(dir_d, 'tmp/r_combos.csv')) ; head(r_combos)
 r_combos[r_combos==0]<-NA # change 0s to NAs so they can be excluded when calculating the mean (see below)
-r_join<-read.csv(file.path(dir_d, 'r_join.csv')) ; head(r_join) #  a table with the complete list of v2013a region ids and a "join" column
+# remove year identifiers from the layer names
+r_combos$Layer<-str_split_fixed(r_combos$Layer,'2013a',2)[,1]
+
+r_join<-read.csv(file.path(dir_d, 'tmp/r_join.csv')) ; head(r_join) #  a table with the complete list of v2013a region ids and a "join" column
 r_combo<-join(r_join,r_combos) # create the "weights" matrix, where a 1 means that resilience indicator is included in that resilience layer combo, and 0 means it's omitted
 names(r_combo)[5:9]<-paste("w_",names(r_combo)[5:9],sep="")
 r_combo<-rename(r_combo,c(rgn_id_2013="rgn_id"))
@@ -58,9 +61,9 @@ MPA_z<-join(MPA_z,area_eez) ; length(unique(MPA_z$rgn_id))# join eez area
 MPA_z<-arrange(MPA_z,rgn_id,year)
 
 #### Step 2 ## process MPA data to get a score for each OHI scenario
-r_MPAc12<-r_MPA %>% filter(year<2012) %>% group_by(rgn_id,tot_area_km2) %>%  summarise(mpa_area=sum(area_km2)) %>% mutate(scen='v2012')
-r_MPAc13<- r_MPA %>% filter(year<2013) %>% group_by(rgn_id,tot_area_km2) %>%  summarise(mpa_area=sum(area_km2)) %>% mutate(scen='v2013')
-r_MPAc14<-r_MPA %>% group_by(rgn_id,tot_area_km2) %>%  summarise(mpa_area=sum(area_km2)) %>% mutate(scen='v2014')
+r_MPAc12<-r_MPA %>% filter(year<2012) %>% group_by(rgn_id,tot_area_km2) %>%  summarise(mpa_area=sum(area_km2)) %>% mutate(scen='2012a')
+r_MPAc13<- r_MPA %>% filter(year<2013) %>% group_by(rgn_id,tot_area_km2) %>%  summarise(mpa_area=sum(area_km2)) %>% mutate(scen='2013a')
+r_MPAc14<-r_MPA %>% group_by(rgn_id,tot_area_km2) %>%  summarise(mpa_area=sum(area_km2)) %>% mutate(scen='2014a')
 r_MPAc<-rbind(r_MPAc12,r_MPAc13,r_MPAc14)
 
 # calculate MPA score (where MPA areas occupying 30% of the coastal area within 3nm gets a score of 1)
@@ -68,9 +71,9 @@ r_MPAc<-r_MPAc %>% mutate(score=ifelse(mpa_area/tot_area_km2*1/0.3>1,1,mpa_area/
 r_MPA3nm<-r_MPAc %>% select(rgn_id, scen, 'MPA_coast'=score)
 
 # same for MPAs within EEZ area
-r_MPAe12<-r_MPA %>% filter(year<2012) %>% group_by(rgn_id,tot_area_km2) %>%  summarise(mpa_area=sum(area_km2)) %>% mutate(scen='v2012')
-r_MPAe13<- r_MPA %>% filter(year<2013) %>% group_by(rgn_id,tot_area_km2) %>%  summarise(mpa_area=sum(area_km2)) %>% mutate(scen='v2013')
-r_MPAe14<-r_MPA %>% group_by(rgn_id,tot_area_km2) %>%  summarise(mpa_area=sum(area_km2)) %>% mutate(scen='v2014')
+r_MPAe12<-r_MPA %>% filter(year<2012) %>% group_by(rgn_id,tot_area_km2) %>%  summarise(mpa_area=sum(area_km2)) %>% mutate(scen='2012a')
+r_MPAe13<- r_MPA %>% filter(year<2013) %>% group_by(rgn_id,tot_area_km2) %>%  summarise(mpa_area=sum(area_km2)) %>% mutate(scen='2013a')
+r_MPAe14<-r_MPA %>% group_by(rgn_id,tot_area_km2) %>%  summarise(mpa_area=sum(area_km2)) %>% mutate(scen='2014a')
 r_MPAe<-rbind(r_MPAe12,r_MPAe13,r_MPAe14)
 
 # calculate MPA score (where MPA areas occupying 30% of the coastal area within 3nm gets a score of 1)
@@ -96,8 +99,22 @@ r_scores <-r_combo %>% group_by(rgn_id, scen, Layer) %>% mutate(score=sum(w_Mora
                                                                         
 r_scores<-r_scores %>% select(rgn_id,  scen, score) # Layer is also included because they're all the variables in the grouping factor
 
-###############################################
-##### to do: save as layers
-#  save as separate scores by scenario and layer (what format is needed for toolbox??)
-write.csv()
+# save as separate files for each layer-scenario combination
+
+d<-r_scores
+d<-rename(d,c('score'='resilience.score'))
+d<-d[!is.na(d$Layer),]
+d$Layer<-paste(d$Layer,d$scen,sep='')
+d<-d[,-3]
+d<-ungroup(d)
+labels<-unique(d$Layer)
+
+            for (i in labels) {
+                              csv = sprintf('%s.csv', i)   
+                              d2 = d %>% filter(Layer==i) %>% select (rgn_id, resilience.score)
+                              write.csv(d2, file.path(dir_d, 'data', csv), row.names=F)
+                             
+                              stopifnot(anyDuplicated(d2[,c('rgn_id', 'year')]) == 0)
+                              
+                              }
 
