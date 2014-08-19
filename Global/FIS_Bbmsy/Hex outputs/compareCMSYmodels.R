45ff074372305348eb1a2518126360569f26edef
Melsteroni <frazier@nceas.ucsb.edu>
2014-08-18 22:22:19 -0700

    Additions to model comparison

--- Global/FIS_Bbmsy/Hex outputs/compareCMSYmodels.R
+++ Global/FIS_Bbmsy/Hex outputs/compareCMSYmodels.R
@@ -10,7 +10,11 @@
 ##       3. save files to FIS_B_bmsy/Hex outputs
 ##       4. compare data files
 
+
+library(plyr)
 library(dplyr)
+library(tidyr)
+library(ggplot2)
 dir_d = '../ohiprep/Global/NCEAS-Fisheries_2014a' # set folder where files are saved
 
 ###############################################
@@ -33,3 +37,94 @@ b_bmsy_lyr <- b_bmsy %>%
   select(fao_id, taxon_name, year=yr, b_bmsy)
 
 write.csv(b_bmsy_lyr, file.path(dir_d, 'data/fnk_fis_b_bmsy_lyr.csv'), row.names=F, na='')
+
+
+###############################################
+## Visualizing differences----
+###############################################
+list <- grep("EEZ", list.files("Global/FIS_Bbmsy/Hex outputs"), value=TRUE)
+all.data <- lapply(list, function(i){
+  #x <- list[1]  
+  tmp <- read.csv(file.path("Global/FIS_Bbmsy/Hex outputs", i))
+  tmp$model <- sapply(strsplit(i, "_"), function(x)x[4])
+  tmp$zeros <- sapply(strsplit(i, "_"), function(x)x[5])
+  tmp$zeros <- gsub(".csv", "", tmp$zeros)
+  tmp$scenario <- sapply(strsplit(i, "_"), function(x)x[1])
+  tmp
+})
+
+data <- ldply(all.data)
+
+data  <- data %>%
+  filter(goal=="FIS",
+         dimension=="score",
+         region_id !=0)
+
+mod <- lm(score ~ model*scenario, data=data)
+
+compareScenario <- data %>%
+  mutate(model=as.factor(model),
+         zeros=as.factor(zeros)) %>%
+  group_by(region_id, model, zeros) %>%
+  summarize(diff_2013minus2012 = score[scenario=='EEZ2013'] - score[scenario=='EEZ2012'])
+
+
+### compare distributions of models
+
+compareModel <- spread(data, model, score)
+
+ggplot(subset(compareModel, scenario=="EEZ2013"), aes(x=constrained, y=uniform, color=zeros, group=zeros)) +
+  geom_point(size=3) +
+  geom_abline(intercept=0, slope=1) +
+  theme_bw()
+
+
+
+ggplot(subset(compareScenario, model=="uniform" & zeros=="w0s"), aes(x=diff_2013minus2012) ) +
+  geom_histogram(fill="grey", color="darkgray")+
+  labs(title="uniform, with 0s")
+
+ggplot(subset(compareScenario, model=="uniform" & zeros=="no0s"), aes(x=diff_2013minus2012) ) +
+  geom_histogram(fill="grey", color="darkgray")+
+  labs(title="uniform, no added 0s")
+
+ggplot(subset(compareScenario, model=="constrained" & zeros=="w0s"), aes(x=diff_2013minus2012) ) +
+  geom_histogram(fill="grey", color="darkgray")+
+  labs(title="constrained, with 0s")
+
+ggplot(subset(compareScenario, model=="constrained" & zeros=="no0s"), aes(x=diff_2013minus2012) ) +
+  geom_histogram(fill="grey", color="darkgray")+
+  labs(title="constrained, no added 0s")
+
+
+compareZeros <- spread(data, zeros, score)
+
+ggplot(subset(compareZeros, scenario=="EEZ2013"), aes(x=no0s, y=w0s, color=model, group=model)) +
+  geom_point(size=3) +
+  geom_abline(intercept=0, slope=1) +
+  theme_bw()
+
+
+data <- ldply(all.data)
+
+data  <- data %>%
+  filter(goal=="FIS",
+         dimension=="status",
+         region_id !=0,
+         zeros=="w0s",
+         scenario=="EEZ2013") %>%
+  mutate(model=as.factor(model),
+         zeros=as.factor(zeros),
+         scenario=as.factor(scenario))
+
+ggplot(subset(data, model=="constrained"), aes(x=score)) +
+  geom_histogram(fill="gray", color="darkgray") +
+  theme_bw() +
+  labs(x="FIS status", title="Constrained, EEZ2013, with 0s")
+
+
+ggplot(subset(data, model=="uniform"), aes(x=score)) +
+  geom_histogram(fill="gray", color="darkgray") +
+  theme_bw() +
+  labs(x="FIS status", title="Constrained, EEZ2013, with 0s")
+
