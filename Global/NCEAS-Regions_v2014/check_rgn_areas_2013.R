5dce75e524629fafd2131fc106198223c8a6a845
bbest <bbest@nceas.ucsb.edu>
2014-09-15 15:28:47 -0700

    checking areas in data files OHI-Science/issues#161

--- Global/NCEAS-Regions_v2014/check_rgn_areas_2013.R
+++ Global/NCEAS-Regions_v2014/check_rgn_areas_2013.R
@@ -1,5 +1,5 @@
 source('src/R/common.R')
-library
+library(dplyr)
 
 dir_rgn13 = file.path(dir_neptune_data, 'model/GL-NCEAS-OceanRegions_v2013a/data')
 
@@ -20,3 +20,67 @@ read.csv('Global/NCEAS-Regions_v2014/data/sp_data_gcs.csv', na.strings='') %.%
 # sp_type sp_id sp_name sp_key area_km2 rgn_type rgn_id rgn_name rgn_key
 #     eez   209   China    CHN        0      eez    209    China     CHN
 #    land   209   China    CHN        0     land    209    China     CHN
+
+
+# bbest trying again: land ok, offshore
+for (csv in list.files('Global/NCEAS-Regions_v2014/data', '.*\\.csv$', full.names=T)){
+  cat(sprintf('\n%s\n', basename(csv)))
+  
+  d = read.csv(csv, na.strings='')
+
+  if ('sp_name' %in% names(d)){
+    print(d %>% filter(sp_name=='China'))
+  } else if ('rgn_name' %in% names(d)){
+    print(d %>% filter(rgn_name=='China'))
+  } else {
+    print('  WARNING: sp_name or rgn_name not found!')
+  }
+}
+
+# rgn_gcs_data.csv
+#   rgn_type rgn_id rgn_name rgn_key area_km2
+# 1     land    209    China     CHN 355085.1
+# 
+# rgn_inland1km_gcs_data.csv
+# rgn_type rgn_id rgn_name rgn_key area_km2
+# 1     land    209    China     CHN 20882.33
+# 
+# rgn_inland25km_gcs_data.csv
+# rgn_type rgn_id rgn_name rgn_key area_km2
+# 1     land    209    China     CHN 211937.4
+# 
+# rgn_inland50km_gcs_data.csv
+# rgn_type rgn_id rgn_name rgn_key area_km2
+# 1     land    209    China     CHN 355084.8
+# 
+# rgn_offshore1km_gcs_data.csv
+# rgn_type rgn_id rgn_name rgn_key area_km2
+# 1      eez    209    China     CHN 25451.72
+# 
+# rgn_offshore3nm_gcs_data.csv
+# rgn_type rgn_id rgn_name rgn_key area_km2
+# 1      eez    209    China     CHN 92113.56
+# 
+# sp_gcs_data.csv
+# sp_type sp_id sp_name sp_key area_km2 rgn_type rgn_id rgn_name rgn_key cntry_id12 rgn_id12 rgn_name12
+# 1    land   209   China    CHN 355085.1     land    209    China     CHN        CHN      159      China
+# 
+# sp_inland1km_gcs_data.csv
+# sp_type sp_id sp_name sp_key area_km2
+# 1    land   209   China    CHN 20882.33
+# 
+# sp_inland25km_gcs_data.csv
+# sp_type sp_id sp_name sp_key area_km2
+# 1    land   209   China    CHN 211937.4
+# 
+# sp_inland50km_gcs_data.csv
+# sp_type sp_id sp_name sp_key area_km2
+# 1    land   209   China    CHN 355084.8
+# 
+# sp_offshore1km_gcs_data.csv
+# sp_type sp_id sp_name sp_key area_km2
+# 1     eez   209   China    CHN 25451.72
+# 
+# sp_offshore3nm_gcs_data.csv
+# sp_type sp_id sp_name sp_key area_km2
+# 1     eez   209   China    CHN 92113.56
