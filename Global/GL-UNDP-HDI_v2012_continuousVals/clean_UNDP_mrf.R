66f80f035a88eb32893dbfb7c8cbc741bdbe39bd
Melsteroni <frazier@nceas.ucsb.edu>
2014-12-13 10:16:25 -0800

    Creating gap-filled HDI data for 2013 OHI paper.

--- Global/GL-UNDP-HDI_v2012_continuousVals/clean_UNDP_mrf.R
+++ Global/GL-UNDP-HDI_v2012_continuousVals/clean_UNDP_mrf.R
@@ -1,24 +1,27 @@
-# clean_UNDP.r: (by JStewart Jul2013)
+# Modified from data-edit/model/clean_UNDP (by JStewart Jul2013)
 
+source("../ohiprep/src/R/common.R")
 
 ## sovereignty (parent-children) gapfilling with add_gapfill_sov.r
 
 #setwd('N:/git-annex/Global/FigurePrep_2013OHI/GL-UNDP-HDI_v2012_continuousVals_mrf/raw')
 
-library(reshape2)
-library(gdata)
-library(plyr)
-require(sqldf)
-options(max.print=5E6)
-options(gsubfn.engine = "R") # otherwise, get X11 launching for sqldf pa
+#library(reshape2)
+#library(gdata)
+library(stringr)
+#require(sqldf)
+#options(max.print=5E6)
+#options(gsubfn.engine = "R") # otherwise, get X11 launching for sqldf pa
 
 #This is the old version:
 #source('C:/Users/Melanie/Desktop/NCEAS/Projects/OHI 2013/GL-UNDP-HDI_v2012/ohi_clean_fxns.r') 
-source('')
+source('../ohicore/R/gapfill_georegions.R')
+source('../ohicore/R/name_to_rgn.R')
+
 d.all =  matrix(nrow=0, ncol=0)
-for (f in list.files(pattern=glob2rx('*Table1.csv'))){
+for (f in list.files(path="Global/GL-UNDP-HDI_v2012_continuousVals/raw", pattern=glob2rx('*Table1.csv'))){
   #f <- list.files(pattern=glob2rx('*Table1.csv'))[[1]]
-  d = read.csv(f, header=F) 
+  d = read.csv(file.path("Global/GL-UNDP-HDI_v2012_continuousVals/raw", f), header=F) 
     names(d)[1] = 'Country'
   
   # add identifier column
@@ -32,13 +35,68 @@ d.all[, "Developed_Developing"] = gsub('.*ed', '1', d.all[, "Developed_Developin
 d.all[, "Developed_Developing"] = gsub('.*ing', '0', d.all[, "Developed_Developing"]) 
 d.all[, "Developed_Developing"] = as.numeric(d.all[, "Developed_Developing"])
   
-d.all$year = rep.int(2013, dim(d.all)[1])
+d.all$year = 2013
 
-d.all <- rename(d.all, c(V2="HDI", V3="LifeExp", V4="Schooling", 
+d.all <- plyr::rename(d.all, c(V2="HDI", V3="LifeExp", V4="Schooling", 
                          V5="ExpectedSchooling", V6="GNI", V7="GNIrankHDIrank",
                          V8= "NonIncomeHDI"))
 
-d.all$GNIrankHDIrank <- as.numeric(gsub("â???", "-", d.all$GNIrankHDIrank))
+d.all <- d.all %>%
+  select(Country, year, HDI)
+
+# Weighted average of China data based on population
+(0.906*7.859 + 0.699*1367)/(1367+7.859) # 0.700
+d.all <- subset(d.all, Country!="Hong Kong China (SAR)")
+d.all[d.all$Country ==  "China", ]
+d.all$HDI[d.all$Country=="China"] <- 0.700
+
+
+test <- name_to_rgn(d.all, fld_name="Country", fld_value="HDI")
+
+
+## sovereign gapfilling with gapfill_georegions.r ----
+# use gapfill_georegions: lookup table that has sov_ids and weight the 'parent' country with 1, others with 0
+
+# read in lookups
+sovregions = read.csv('../ohiprep/src/LookupTables/eez_rgn_2013master.csv', na.strings='') %>% 
+  select(rgn_id = rgn_id_2013,
+         r2 = sov_id) %>%     # r2 is actually rgn_ids of sovereign regions
+  group_by(rgn_id) %>%                       # remove duplicated countrys from this rgn_id list                    
+  summarize(r2 = mean(r2, na.rm=T)) %>% # duplicates always have the same sov_id (r2 value)
+  mutate(r1 = r2, 
+         r0 = r2,
+         fld_wt = as.integer(rgn_id == r2)) %>%  # flag the 'parent' rgn_id with 1, others with 0 
+  filter(rgn_id < 255, rgn_id != 213); head(sovregions)
+
+# join fld_wt weighting to m_d
+test = test %>% 
+  left_join(sovregions %>%
+              select(rgn_id, fld_wt),
+            by = 'rgn_id'); head(test)
+
+test = test %>%
+  filter(!rgn_id %in% c(213,255)) %>%
+  select(rgn_id, HDI, fld_wt)
+
+
+# gapfill_georegions
+attrsave  = "Global/GL-UNDP-HDI_v2012_continuousVals/data/rgn_hdi_2013_attr.csv"
+
+# library(devtools); load_all('../ohicore')
+# source('../ohicore/R/gapfill_georegions.R')
+test_gap_filled = gapfill_georegions(
+  data = test,
+  fld_id = c('rgn_id'),
+  fld_weight = 'fld_wt',
+  georegions = sovregions %>%
+    select(-fld_wt),
+  r0_to_NA = TRUE, 
+  attributes_csv = (attrsave)) 
+
+# investigate attribute tables
+head(attr(d_g_a, 'gapfill_georegions'))  # or to open in excel: system(sprintf('open %s', attrsave))
+
+test2 <- gapfill_georegions(test, fld_id='region_id', fld_value="HDI")
 
 ###############################################################
 # this assigns the region name and id
