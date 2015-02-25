aed1b2d3e60c2995df345388d5b58e270e719111
Melsteroni <frazier@nceas.ucsb.edu>
2015-02-25 12:46:29 -0800

    Doing some checks of extraction data.

--- globalprep/WDPA_MPA/v2015/digest.R
+++ globalprep/WDPA_MPA/v2015/digest.R
@@ -3,8 +3,6 @@ library(stringr)
 library(dplyr)
 library(tidyr)
 
-library(raster)
-raster(file.path(dir_neptune_data, "git-annex/Global/NCEAS-Regions_v2014/data/sp_inland_mol.tif"))
 source('src/R/common.R')
 dir_prod = 'globalprep/WDPA_MPA/v2015'
 
@@ -20,12 +18,12 @@ for (i in 1:length(lyrs)){
   dbf = names(lyrs)[i]
   csv = lyrs[[i]]
 
-  d = read.dbf(file.path(dir_prod, 'tmp/new_rasters_500mcell', dbf))
+  d = read.dbf(file.path(dir_prod, 'tmp/orig_rasters', dbf))
 
 m <- gather(d, year, area_m2, VALUE_0:VALUE_2014)
 m <- m %>%
   mutate(year=as.integer(gsub('VALUE_', '', year))) %>%
-  mutate(area_km2 = area_m2/(500*500)) %>%      ###change to match cell size
+  mutate(area_km2 = area_m2/(1000*1000)) %>%      ###change to match cell size
   select(rgn_id=VALUE, year, area_km2) %>%
   arrange(rgn_id, year)
 
@@ -55,7 +53,7 @@ length(table(m$rgn_id)) #now the regions are there...
 #     m$year[m$rgn_id ==78] = 1972
 #     m$area_km2[m$rgn_id == 220] = 0.873250772 # special fix for Sint Maarten[220]
 #     m$year[m$rgn_id == 220] = 2006
-} 
+#} 
    
   # save layer
   write.csv(m, file.path(dir_prod, 'tmp/orig_rasters', csv), row.names=F, na='')
@@ -150,4 +148,55 @@ compare <- compare %>%
   select(-total)
 
 
+compare$new500vs1km <- compare$area_km2_new500 - compare$area_km2_new1km
+hist(compare$new500vs1km)
+mean(compare$new500vs1km, na.rm=TRUE)
+median(compare$new500vs1km, na.rm=TRUE)
+range(compare$new500vs1km, na.rm=TRUE)
 write.csv(compare, file.path(dir_prod, "compare_data.csv"), row.names=FALSE)
+
+TotalArea <- compare %>%
+  select(rgn_id, year, area_km2_new500) %>%
+  group_by(rgn_id) %>%
+  summarize(totalArea_new500=sum(area_km2_new500, na.rm=TRUE)) %>%
+  arrange(totalArea_new500)
+write.csv(TotalArea, file.path(dir_prod, "TotalArea.csv"), row.names=FALSE)
+
+## offshore calculations:
+new1  <-  read.csv(file.path(dir_prod, 'tmp/new_rasters_1kmcell/lsp_protarea_offshore3nm.csv'))
+old1 <-  read.csv(file.path(dir_prod, 'tmp/orig_rasters/lsp_protarea_offshore3nm.csv'))
+new500 <- read.csv(file.path(dir_prod, 'tmp/new_rasters_500mcell/lsp_protarea_offshore3nm.csv'))
+old_old <- read.csv('Global/WDPA-MPA_v2014/data/lsp_protarea_offshore3nm.csv')
+
+new1  <- new1 %>%
+  select(rgn_id, year, area_km2_new1km=area_km2)
+old1  <- old1 %>%
+  select(rgn_id, year, area_km2_oldnear1km=area_km2)
+new500  <- new500 %>%
+  select(rgn_id, year, area_km2_new500=area_km2)
+old_old <- old_old %>%
+  select(rgn_id, year, area_km2_oldData=area_km2)
+
+compare <- merge(new1, old1, all=TRUE)
+compare <- merge(compare, new500, all=TRUE)
+compare <- merge(compare, old_old, all=TRUE)
+
+compare <- compare %>%
+  mutate(total = rowSums(compare[, c('area_km2_new1km', 'area_km2_oldnear1km', 'area_km2_new500', 'area_km2_oldData')], na.rm=TRUE)) %>%
+  filter(total > 0) %>%
+  select(-total)
+
+
+compare$new500vs1km <- compare$area_km2_new500 - compare$area_km2_new1km
+hist(compare$new500vs1km)
+mean(compare$new500vs1km, na.rm=TRUE)
+median(compare$new500vs1km, na.rm=TRUE)
+range(compare$new500vs1km, na.rm=TRUE)
+write.csv(compare, file.path(dir_prod, "compare_data_offshore.csv"), row.names=FALSE)
+
+TotalArea <- compare %>%
+  select(rgn_id, year, area_km2_new500) %>%
+  group_by(rgn_id) %>%
+  summarize(totalArea_new500=sum(area_km2_new500, na.rm=TRUE)) %>%
+  arrange(totalArea_new500)
+write.csv(TotalArea, file.path(dir_prod, "TotalArea_offshore.csv"), row.names=FALSE)
