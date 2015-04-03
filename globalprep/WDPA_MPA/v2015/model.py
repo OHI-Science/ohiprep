5dd5ca90b9ffc37860523884051d5fb263f0554d
Casey O'Hara <ohara@nceas.ucsb.edu>
2015-04-03 13:52:12 -0700

    Updated readme.md to include notes on filtering methodology for 2015 assessment (including notes on IUCN_CAT and MANG_PLAN filter schemes).  model_mang_plan.py renamed to model.py, as it is most likely to be approved as the "official" 2015 filtering method.

--- globalprep/WDPA_MPA/v2015/model.py
+++ globalprep/WDPA_MPA/v2015/model.py
@@ -1,12 +1,21 @@
-#cmd: C:\Python27\ArcGISx6410.2\python.exe G:\ohiprep\Global\WDPA-MPA_v2014\model.py
+# File: ohiprep/globalprep/WDPA_MPA/v2015/model.py
+#
+# Modified March 2015 by Casey O'Hara
+#
+# Filters by 
+# * "STATUS" == 'Designated':  includes only officially designated areas, not
+#   areas still in planning or proposal phases.
+# * "MANG_PLAN" NOT LIKE 'Non-MPA%': eliminates all areas specifically designated
+#   as Non-MPA Programmatic Management Plans (code only used for U.S.).  Eliminates
+#   groundfish management areas, '25 mph manatee' zones, and the like.
 
 import arcpy, os, subprocess, csv, sys, socket
 from arcpy.sa import *
 
 # configuration based on machine name
-dirs =  {'git'    :'C:/Users/visitor/Documents/github/ohiprep', 
+dirs =  {'git'    :'C:/Users/ohara/Documents/github/ohiprep', 
          'neptune':'N:',
-         'tmp'    :'C:/Users/visitor/tmp',
+         'tmp'    :'C:/Users/ohara/tmp',
          }
 print 'Setting pathnames...'
 print dirs
@@ -25,22 +34,19 @@ print 'dir_anx = ' + dir_anx
 poly_wdpa  = '%s/raw/WDPA_Jan2015_Public/WDPA_Jan2015_Public.gdb/WDPA_poly_Jan2015' % dir_anx
 dir_rgn    = '%s/git-annex/Global/NCEAS-Regions_v2014/data' % dirs['neptune']
 
-##rgn_offshore_3nm   = '%s/rgn_offshore3nm_mol.tif' % dir_rgn
-##rgn_inland_1km     = '%s/new_rgn_ohara/rgn_inland1km_mol_1kmcell.tif'   % dir_rgn
-rgn_offshore_3nm   = '%s/new_rgn_ohara/rgn_offshore3nm_mol_500mcell.tif' % dir_rgn
-rgn_inland_1km     = '%s/new_rgn_ohara/rgn_inland1km_mol_500mcell.tif'   % dir_rgn
+rgn_off_3nm   = '%s/rgn_raster_500m/rgn_offshore3nm_mol_500mcell.tif' % dir_rgn
+rgn_inl_1km     = '%s/rgn_raster_500m/rgn_inland1km_mol_500mcell.tif'   % dir_rgn
 
 print 'input poly_wdpa = ' + poly_wdpa
 print 'input dir_rgn = ' + dir_rgn
-print 'input rgn_offshore_3nm = ' + rgn_offshore_3nm
-print 'input rgn_inland_1km = ' + rgn_inland_1km
+print 'input rgn_off_3nm = ' + rgn_off_3nm
+print 'input rgn_inl_1km = ' + rgn_inl_1km
 
 
 # outputs
-ply = '%s/poly_wdpa_d' % gdb
+ply_d = '%s/poly_wdpa_d' % gdb    # designated and protected
 tif = '%s/wdpa_designated_mol.tif' % dir_tmp
 msk = '%s/rgn_offshore3nm_inland1km_mol.tif' % dir_tmp
-print 'output ply = ' + ply
 print 'output tif = ' + tif
 print 'output msk = ' + msk
 
@@ -49,9 +55,9 @@ print 'output msk = ' + msk
 arcpy.CheckOutExtension('Spatial')
 arcpy.env.overwriteOutput=1
 
-# projections
-sr_mol = arcpy.SpatialReference('Mollweide (world)') # projected Mollweide (54009)
-sr_gcs = arcpy.SpatialReference('WGS 1984')          # geographic coordinate system WGS84 (4326)
+# projections: is this used anywhere else?
+##sr_mol = arcpy.SpatialReference('Mollweide (world)') # projected Mollweide (54009)
+##sr_gcs = arcpy.SpatialReference('WGS 1984')          # geographic coordinate system WGS84 (4326)
 
 # shapefiles don't have nulls, so use geodatabase
 if not os.path.exists(dir_tmp):
@@ -64,38 +70,33 @@ arcpy.env.workspace = gdb; os.chdir(dir_tmp)
 arcpy.env.scratchWorkspace = dir_tmp
 
 # develop coastal raster to use as a mask
-print 'Developing coastal raster to use as a mask'
-r = Con(IsNull(Raster(rgn_offshore_3nm)), Raster(rgn_inland_1km), Raster(rgn_offshore_3nm))
-r.save(msk)
+##print 'Developing coastal raster to use as a mask'
+##r = Con(IsNull(Raster(rgn_off_3nm)), Raster(rgn_inl_1km), Raster(rgn_off_3nm))
+##r.save(msk)
 
-print 'Setting environment based on msk'
+print 'Setting environment based on msk.'
 arcpy.env.snapRaster = arcpy.env.cellSize = arcpy.env.outputCoordinateSystem = arcpy.env.extent = msk
 
 # select only designated
-print 'Selecting only designated features and creating new feature class based on STATUS = Designated'
-arcpy.Select_analysis(poly_wdpa, ply, '"STATUS"=\'Designated\'')
+print 'Selecting only designated features and creating new feature class based on STATUS = Designated.'
+arcpy.Select_analysis(poly_wdpa, ply_d, '("STATUS" = \'Designated\') AND ("MANG_PLAN" NOT LIKE \'Non-MPA%\')')
 
-# create priority field, prioritizing DESIG_TYPE: Regional(2) > National(1) > International (0) and then the earliest STATUS_YR (inverse). Highest number gets priority.
+# create priority field, prioritizing by earliest STATUS_YR (take inverse of STATUS_YR)
 print 'Creating and calculating priority field'
-arcpy.AddField_management(ply, 'poly_priority', 'FLOAT')
-code_block = """
-def get_priority(typ,yr):
-    return({'International':0.0,'National':1.0,'Regional':2.0}[typ] + 1/(float(yr)+1))
-"""
-arcpy.CalculateField_management(ply, 'poly_priority', 'get_priority(typ=!DESIG_TYPE!, yr=!STATUS_YR!)', 'PYTHON_9.3', code_block)
+arcpy.AddField_management(ply_d, 'poly_priority', 'FLOAT')
+arcpy.CalculateField_management(ply_d, 'poly_priority', '1/(!STATUS_YR! + 1)', 'PYTHON_9.3')
 
 # polygon to raster
-print 'From Designated polygons, create raster .tif'
-arcpy.env.snapRaster = arcpy.env.cellSize = arcpy.env.outputCoordinateSystem = arcpy.env.extent = msk
-arcpy.PolygonToRaster_conversion(ply, 'STATUS_YR', tif, 'MAXIMUM_COMBINED_AREA', 'poly_priority', msk)
+print 'From Protected polygons, create raster .tif'
+arcpy.PolygonToRaster_conversion(ply_d, 'STATUS_YR', tif, 'MAXIMUM_COMBINED_AREA', 'poly_priority', msk)
 
 # tabulate
 # note: zone raster must have an attribute table, automatically created for integer rasters
-print 'Tabulating cell counts in Designated raster that overlap with offshore regions'
-TabulateArea(rgn_offshore_3nm, 'VALUE', tif, 'VALUE', '%s/tmp/rgn_offshore3nm_wdpa.dbf' % dir_git, msk)
-print 'Tabulating cell counts in Designated raster that overlap with inland regions'
-TabulateArea(rgn_inland_1km, 'VALUE', tif, 'VALUE', '%s/tmp/rgn_inland1km_wdpa.dbf'   % dir_git, msk)
-	# final .dbfs saved into local github/tmp location
+print 'Tabulating cell counts in Protected raster that overlap with offshore regions'
+TabulateArea(rgn_off_3nm, 'VALUE', tif, 'VALUE', '%s/rgn_offshore3nm_wdpa.dbf' % dir_tmp, msk)
+print 'Tabulating cell counts in Protected raster that overlap with inland regions'
+TabulateArea(rgn_inl_1km, 'VALUE', tif, 'VALUE', '%s/rgn_inland1km_wdpa.dbf'   % dir_tmp, msk)
+	# final .dbfs saved into local tmp location
 
 # copy tmp to neptune
 # paths = [gdb, tif, msk]
@@ -104,3 +105,4 @@ TabulateArea(rgn_inland_1km, 'VALUE', tif, 'VALUE', '%s/tmp/rgn_inland1km_wdpa.d
 # for fro in paths:
 #     arcpy.Copy_management(fro, '%s/tmp/%s' % (dir_anx, os.path.basename(fro)))
 #     #arcpy.Delete_management(tmp+'/'+d)
+
--- globalprep/WDPA_MPA/v2015/readme.md
+++ globalprep/WDPA_MPA/v2015/readme.md
@@ -1,18 +1,32 @@
-# LSP notes
+# LSP notes and notes on the WDPA database
 
 The original data and additional tmp files are located on Neptune:
-N:\git-annex\globalprep\WDPA_MPA\v2015
+`N:\git-annex\globalprep\WDPA_MPA\v2015`
 
-model.py: 
-* Uses pre-prepared rasters of 3nm offshore and 1 km inland regions to convert WDPA-MPA polygons to a raster. Note: cell size for WDPA-MPA raster is driven by region rasters.
-* Extracts data for protected areas within the two coastal regions, outputs as tables (.dbf).
-* More information about the .py script is found in the README_dataDescription.html (in the v2014_archive folder)
+##Files and Folders
+* `model.py:` Imports polygons and filters to count 'Designated' protected areas, avoiding 'non-MPA Programmatic Management Plans' (see **Issues** below).  Creates raster based upon earliest protection date, and tallies protected areas within each 3nm offshore and 1km inland coastal region.
+    * Uses pre-prepared rasters of 3nm offshore and 1 km inland regions to convert WDPA-MPA polygons to a raster. Note: cell size for WDPA-MPA raster is driven by region rasters, currently pointing to 500 m rasters
+    * Extracts data for protected areas within the two coastal regions, outputs as tables (.dbf).
+    * More information about the .py script is found in the `README_dataDescription.html` (in the `v2014_archive` folder)
+* `digest.R:` Prepares the final data (using processed data within .\tmp) for the Toolbox.
+* `.\tmp:` Contains processed data (.dbf outputs) from model.py.  
+    * `.\tmp\new_rasters_500mcell` contains the outputs from a preliminary analysis based upon 500 m raster cells, with old (outdated) filtering method. Going forward, LSP analyses are now based on 500m cells, so this folder should probably be deleted.
+    * `.\tmp\iucn_dbfs` contains .dbf outputs from `model_iucn.py`.
+    * `.\tmp\mang_plan_dbfs` contains .dbf outputs based upon current `model.py` which cuts out `MANG_PLAN == 'non-MPA Programmatic Management Plan'` areas.  This is the 'latest and greatest' model for v2015, so these data should be moved to `.\tmp\`
+* `.\data:` Contains final data (.csv outputs) from `digest.R` for use in the Toolbox.
 
-digest.R: prepares the final data (using processed data within .\tmp) for the Toolbox.
+##Exploratory Files and Folders 
+* `model_iucn.py:` An early attempt to resolve an issue with the 2015 WDPA database.  See **Issues** below.
+* `digest_iucn.R:` reworked to process IUCN-filtered data.
+* `scenario_compare.R:` Script to compare outputs of various filtering schemes.
+* `.\DataExplore:` Additional analysis to compare and verify results of basing analysis on finer-resolution rasters (500 m vs 1000 m).  
+    * `explore.R` performs analyses, and reports outputs of these analyses in the other files within this directory.
 
-.\tmp: contains processed data (.dbf outputs) from model.py.  .\tmp\new_rasters_500mcell contains the outputs from the most recent analysis based upon 500 m raster cells.
+##Issues on WDPA_MPA database for LSP
+The original `model.py` script imports a polygon layer denoting global protected areas according to the WDPA MPA database, published in January 2015.  This version of the database includes a number of very large protected areas, previously designated, that indicated fishery management areas and the like.  After discussion, we decided that these "problem areas" do not fit the philosophy of a "lasting special place," and should not be included.
 
-.\data: contains final data (.csv outputs) from digest.R for use in the Toolbox.
+Based on a paper by Ban et al, 2014(?) we looked at using IUCN protected area categories as a filtering mechanism.  Rather than weighting different categories, we elected to exclude any Category V and VI areas that are not specifically denoted as "no take" areas (`NO_TAKE == 'All'`).  While this method eliminated the "problem areas," it also eliminated other areas due to the broad definition of what is included in IUCN Category V, including Marine National Monuments and National Marine Sanctuaries, which we decided was not acceptable.  This model is coded in `model_iucn.py`
 
-.\DataExplore: additional analysis to compare and verify results of basing analysis on finer-resolution rasters (500 m vs 1000 m).  explore.R performs analyses, and reports outputs of these analyses in the other files within this directory.
+Examining the `MANG_PLAN` field, it was clear that the "problem areas" were tagged as "non-MPA Programmatic Species Management Plan" (or Habitat or Fishery Management Plan).  Filtering to exclude polygons with this designation eliminated all the problem areas for this year.
 
+However, this use of the `MANG_PLAN` field does not comply with the WDPA_MPA standards -- it is meant to be used to provide a hyperlink or a bibliographic reference to a published plan.  The US seems to be the only country that uses the field to code for 'MPA' and 'non-MPA' management plans; if the use or definition of this field changes in future data sets, this filtering may need to be re-examined.
