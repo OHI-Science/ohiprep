21866cad18cdcdb720ea3e4e741e5ddae38e8805
Melsteroni <frazier@nceas.ucsb.edu>
2015-04-22 11:49:29 -0700

    NP: exploring exposure values

--- globalprep/FAO_commodities/ExposureExplore/analysis.R
+++ globalprep/FAO_commodities/ExposureExplore/analysis.R
@@ -72,3 +72,35 @@ ggplot(data, aes(exposure)) +
   labs(title = "Seaweeds: Distribution of exposure (all years)")
 quantile(data$exposure)
 mean(data$exposure)
+
+
+##### Explorign different quantiles for setting the "max" harvest/area values in Exposure
+#####  This is from the 2013 assessment
+
+data <- read.csv(file.path(dir_np, "ExposureExplore/exp_explore.csv"))
+library(ggplot2)
+
+ggplot(data, aes(x=x_99)) +
+ geom_histogram(color="black", fill="gray") +
+  theme_bw() +
+  labs(title="NP: Exposure 99th quantile", x="Exposure Score", y="Number of observations")
+
+ggplot(data, aes(x=x_95)) +
+  geom_histogram(color="black", fill="gray") +
+  theme_bw() +
+  labs(title="NP: Exposure 95th quantile", x="Exposure Score", y="Number of observations")
+
+
+ggplot(data, aes(x=exposure, fill=product)) +
+  geom_histogram() +
+  theme_bw() 
+
+ggplot(data, aes(x=exposure, y=x_95, color=product, shape=product)) +
+  geom_point(size=3) +
+  geom_abline(slope=1, intercept=0) +
+  labs(title="Comparing exposure calculated from max vs 95th quantile") +
+  theme_bw()
+
+
+ggsave(file.path(dir_np, "ExposureExplore/oldExpvs99Exp.png"))
+  
