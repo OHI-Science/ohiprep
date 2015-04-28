61e831fca178ea37371053ba5eaec093f3a82b2c
Melsteroni <frazier@nceas.ucsb.edu>
2015-04-28 14:22:13 -0700

    Functions to visualize OHI-global data

--- src/R/VisGlobal.R
+++ src/R/VisGlobal.R
@@ -36,12 +36,12 @@ changePlot <- function(repo="~/ohi-global", scenario="eez2013", commit="previous
     scale_colour_brewer(palette="Dark2") +
     geom_jitter(position = position_jitter(width=0.2, height=0))
   
-  ggsave(file.path(repo, 'figures/DataCheck', paste0(fileSave, "_changePlot_", Sys.Date(), '.png')))
+  ggsave(file.path(repo, 'figures/DataCheck', paste0(fileSave, "_changePlot_", Sys.Date(), '.png')), width=8, height=5)
   write.csv(data_new, file.path(repo, 'figures/DataCheck', paste0(fileSave, "_diff_data_", Sys.Date(), '.csv')), row.names=FALSE)
 }
 
 
-scatterPlot <- function(repo="~/ohi-global", scenario="eez2013", commit="previous", goal, fileSave){
+scatterPlot <- function(repo="~/ohi-global", scenario="eez2013", commit="previous", goal, dim="score", fileSave){
   #   scenario <- "eez2013"  ## options: 'eez2012', 'eez2013', 'eez2014', 'eez2015'
   #   commit <- "final_2014"   ## 'final_2014', 'previous', a commit code (ie., 'e30e7a4')
   #   fileSave <- 'LSP_trend_data'
@@ -70,10 +70,12 @@ scatterPlot <- function(repo="~/ohi-global", scenario="eez2013", commit="previou
   data_old <- read_git_csv(repo, commit2, path) %>%
     select(goal, dimension, region_id, old_score=score)
   
+  criteria <- ~dimension == dim
+  
   data_new <- read.csv(file.path(repo, path)) %>%
     left_join(data_old, by=c('goal', 'dimension', 'region_id')) %>%
     mutate(change = score-old_score) %>%
-    filter(dimension=="score") %>%
+    filter_(criteria) %>%
     group_by(goal) %>% 
     mutate(mean = mean(change, na.rm=TRUE),
            sd =  sd(change, na.rm=TRUE)) %>%
@@ -89,15 +91,15 @@ scatterPlot <- function(repo="~/ohi-global", scenario="eez2013", commit="previou
   ggplot(data_new, aes(x=old_score, y=score)) +
     geom_point(shape=19) +
     theme_bw() + 
-    labs(title=paste(goal, scenario, commit, sep=": "), y="New scores", x="Scores from 2014 analysis") +
+    labs(title=paste(scenario, goal, dim, commit, sep=": "), y="New scores", x="Scores from 2014 analysis") +
     geom_abline(slope=1, intercept=0, color="red") +
     geom_text(aes(label=plotLabel), vjust=1.5, size=3)
     
-    ggsave(file.path(repo, 'figures/DataCheck', paste0(fileSave, "_scatterPlot_", Sys.Date(), '.png')))
+    ggsave(file.path(repo, 'figures/DataCheck', paste0(fileSave, "_scatterPlot_", Sys.Date(), '.png')), width=10, height=8)
 }
 
 
-goalHistogram <- function(repo="~/ohi-global", scenario="eez2013", goal, fileSave){
+goalHistogram <- function(repo="~/ohi-global", scenario="eez2013", goal, dim="score", fileSave){
   #   scenario <- "eez2013"  ## options: 'eez2012', 'eez2013', 'eez2014', 'eez2015'
   #   fileSave <- 'LSP_trend_data'
   #   goal <- 'LSP'
@@ -110,9 +112,10 @@ goalHistogram <- function(repo="~/ohi-global", scenario="eez2013", goal, fileSav
   require(RColorBrewer)
   path = paste0(scenario, '/scores.csv')
   
+  criteria  <- ~dimension == dim
   
   data_new <- read.csv(file.path(repo, path)) %>%
-    filter(dimension == "score")
+    filter_(dim)
   
   data_new <- data_new[data_new$goal == goal, ]
            
@@ -120,7 +123,7 @@ goalHistogram <- function(repo="~/ohi-global", scenario="eez2013", goal, fileSav
   ggplot(data_new, aes(x=score)) +
     geom_histogram(color='black', fill="gray") +
     theme_bw() + 
-    labs(title=paste(goal, scenario, "2015 analysis", sep=": "), y="Regions", x="Scores") 
+    labs(title=paste(scenario, goal, "2015 analysis", sep=": "), y="Regions", x="Scores") 
   
-  ggsave(file.path(repo, 'figures/DataCheck', paste0(fileSave, "_histPlot_", Sys.Date(), '.png')))
+  ggsave(file.path(repo, 'figures/DataCheck', paste0(fileSave, "_histPlot_", Sys.Date(), '.png', width=8, height=5)))
 }
