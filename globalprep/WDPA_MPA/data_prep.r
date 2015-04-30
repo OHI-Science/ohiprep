59d9fbaa15d8a4058e1b37dc128f2f4934524ce5
Casey O'Hara <ohara@nceas.ucsb.edu>
2015-04-30 11:54:14 -0700

    Cleaning up WDPA_MPA directory, updating readme.md, and creating a placeholder data_prep.R

--- globalprep/WDPA_MPA/data_prep.r
+++ globalprep/WDPA_MPA/data_prep.r
@@ -1,3 +1,6 @@
-# this will look in all WDPA_MPA subfolders and operate
-
-dir_data = '~/github/ohiprep/globalprep/WDPA_MPA/v2015' # make a for loop for the future...
+# sequence:
+#   use model.py to rasterize WDPA polygons and output .dbfs
+#     use python just to generate rasters, and move raster analysis over to R script?
+#     call python script from top-level R script using rPython?
+#       if so, can we pass variables to it, e.g. file locations for a new analysis?
+#   use digest.R to crunch .dbfs into total areas per region per year
