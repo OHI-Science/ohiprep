426160256846e9697ff66b61c9d864807d3fe07d
Melsteroni <frazier@nceas.ucsb.edu>
2015-05-13 10:40:07 -0700

    Updates to global OHI2015 data visualization functions

--- src/R/VisGlobal.R
+++ src/R/VisGlobal.R
@@ -12,15 +12,16 @@ changePlot <- function(repo="~/ohi-global", scenario="eez2013", commit="previous
   require(ggplot2)
   require(RColorBrewer)
   
+
   if(commit=="previous"){
-    commit2 = substring(commits(repo)[[1]]@sha, 1, 7)
+    commit2 = substring(commits(repository(repo))[[1]]@sha, 1, 7)
   } else{
      if (commit == "final_2014"){
     commit2 = '4da6b4a'
   } else {commit2 = commit}
   }
   path = paste0(scenario, '/scores.csv')
-  
+
   data_old <- read_git_csv(repo, commit2, path) %>%
     select(goal, dimension, region_id, old_score=score)
   data_new <- read.csv(file.path(repo, path)) %>%
@@ -55,7 +56,7 @@ scatterPlot <- function(repo="~/ohi-global", scenario="eez2013", commit="previou
   require(RColorBrewer)
   
   if(commit=="previous"){
-    commit2 = substring(commits(repo)[[1]]@sha, 1, 7)
+    commit2 = substring(commits(repository(repo))[[1]]@sha, 1, 7)
   } else{
     if (commit == "final_2014"){
       commit2 = '4da6b4a'
@@ -91,7 +92,7 @@ scatterPlot <- function(repo="~/ohi-global", scenario="eez2013", commit="previou
   ggplot(data_new, aes(x=old_score, y=score)) +
     geom_point(shape=19) +
     theme_bw() + 
-    labs(title=paste(scenario, goal, dim, commit, sep=": "), y="New scores", x="Scores from 2014 analysis") +
+    labs(title=paste(scenario, goal, dim, commit, sep=": "), y="New scores", x="Scores from 2015 analysis") +
     geom_abline(slope=1, intercept=0, color="red") +
     geom_text(aes(label=plotLabel), vjust=1.5, size=3)
     
@@ -99,23 +100,18 @@ scatterPlot <- function(repo="~/ohi-global", scenario="eez2013", commit="previou
 }
 
 
-goalHistogram <- function(repo="~/ohi-global", scenario="eez2013", goal, dim="score", fileSave){
+goalHistogram <- function(scenario="eez2013", goal, dim="score", fileSave){
   #   scenario <- "eez2013"  ## options: 'eez2012', 'eez2013', 'eez2014', 'eez2015'
-  #   fileSave <- 'LSP_trend_data'
-  #   goal <- 'LSP'
-  ## Useful code: repository(repo)
-  ## Useful code: commits(repo)
-  
-  # devtools::install_github('ropensci/git2r') # to get latest version
-  require(git2r)
+  #   fileSave <- 'NP_function'
+  #   goal <- 'NP'  
   require(ggplot2)
   require(RColorBrewer)
   path = paste0(scenario, '/scores.csv')
   
   criteria  <- ~dimension == dim
   
-  data_new <- read.csv(file.path(repo, path)) %>%
-    filter_(dim)
+  data_new <- read.csv(path) %>%
+    filter_(criteria)
   
   data_new <- data_new[data_new$goal == goal, ]
            
@@ -125,5 +121,5 @@ goalHistogram <- function(repo="~/ohi-global", scenario="eez2013", goal, dim="sc
     theme_bw() + 
     labs(title=paste(scenario, goal, "2015 analysis", sep=": "), y="Regions", x="Scores") 
   
-  ggsave(file.path(repo, 'figures/DataCheck', paste0(fileSave, "_histPlot_", Sys.Date(), '.png', width=8, height=5)))
+  ggsave(file.path('figures/DataCheck', paste0(fileSave, "_histPlot_", Sys.Date(), '.png')), width=8, height=5)
 }
