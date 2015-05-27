420ad16df977e7db558145c026e593b100cfe900
Casey O'Hara <ohara@neptune.nceas.ucsb.edu>
2015-05-27 14:38:16 -0700

    fixed issue with the groups_override functionality...

--- globalprep/SpeciesDiversity/ingest_new.R
+++ globalprep/SpeciesDiversity/ingest_new.R
@@ -139,7 +139,7 @@ spp_all <- create_spp_master_lookup(dir_anx, scenario = 'v2015', reload = FALSE)
 #       We're looking at ~ 4000 polygons, on the order of five hours? probably much more?
 # * when finished with each spp_group, save a file of iucn_sid | LOICZID | proportionArea for that group.
 
-extract_loiczid_per_spp(dir_anx, scenario = 'v2015', reload = TRUE)
+extract_loiczid_per_spp(dir_anx, scenario = 'v2015', reload = FALSE)
 # use groups_override argument to run function on partial list of species groups.
 
   
