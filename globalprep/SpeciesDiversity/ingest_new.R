6aa131dbac2514e512186a00b605334cfa409ecc
Casey O'Hara <ohara@nceas.ucsb.edu>
2015-05-28 18:46:52 -0700

    reworking the ingest files.  Trying to get Aquamaps and IUCN both to list species presence per loiczid cell

--- globalprep/SpeciesDiversity/ingest_new.R
+++ globalprep/SpeciesDiversity/ingest_new.R
@@ -17,20 +17,6 @@
 #     - SPECIESID   SpecCode   sciname   am_status
 #   * IUCN species modified to include:
 #     - sid(Red.List.Species.ID)  sciname  iucn_status  popn_trend
-# The process:
-# * full_join based on sciname; rename iucn_sid = sid, am_sid = SPECIESID
-# * status, and info_source:
-#   - for species with iucn_status information, use that; otherwise am_status; otherwise NA
-#   - also tag info_source with am, iucn, or na
-# * popn_trend: 
-#   - for species with IUCN popn_trend info, use that
-#   - otherwise NA
-# * spatial_source: 
-#   - figure out which species have IUCN spatial data, and flag spatial_source with iucn;
-#   - for species w/o IUCN spatial data, but with AM data, flag spatial_source with am;
-#   - otherwise NA.
-# * Filter out anything with spatial_source NA, and with status NA
-# 
 #
 # Create lookup: region_id <-> cell_id
 # * pretty much already done... include both LOICZID(?) and CsquareCode?
@@ -47,7 +33,28 @@
 # Aquamaps list (already created)
 #   AM_speciesID  CsquareCode  prob
 # 
-# 
+
+# Create Aquamaps master spp spatial chart: all species using AM data
+# sciname | am_sid | popn_status | popn_trend | loiczid | prob
+# * filter master species lookup by spatial_source == 'am'
+# * filter out any popn_status NAs
+# * check for subspecies (duplicated scinames)
+# * load AM species spatial data
+# * filter spatial data for loiczids in region lookup
+# * inner_join AquaMaps species spatial data to species lookup 
+
+# Create IUCN master spp spatial chart: all species using IUCN data
+# sciname | iucn_sid | popn_status | popn_trend | loiczid | prop_area
+# * filter master species lookup by spatial_source == 'iucn'
+# * filter out any popn_status NAs
+# * check for subspecies (duplicated scinames)
+# * filter spatial data for loiczids in region lookup
+# * inner_join each IUCN species spatial data to species lookup 
+
+# join full master spp spatial chart:
+# select out unneeded columns (prob and prop_area)
+# bind_rows by sciname, sid, popn_status, popn_trend, loiczid
+
 
 ##############################################################################=
 library(foreign)    # or: library(shapefiles) for read.dbf()
@@ -58,8 +65,9 @@ library(raster)
 library(readr)      # for read_csv()
 
 setwd('~/github/ohiprep') # if not already there!  Should this be a standard practice - always 
-  # just keep your working directory set to the base github repository location?
+# just keep your working directory set to the base github repository location?
 source('src/R/common.R')
+
 
 # Note the following masked package:raster functions
 #   intersect, select, union (masked by 'dplyr');  extract (masked by 'tidyr')
@@ -67,41 +75,12 @@ source('src/R/common.R')
 
 goal <- 'globalprep/SpeciesDiversity'
 dir_anx <- file.path(dir_neptune_data, 'git-annex', goal) 
-  # ??? use global variable for ease of passing file location into functions?
+# ??? use global variable for ease of passing file location into functions?
 dir_git <- file.path('~/github/ohiprep', goal)
 
 source(file.path(dir_git, 'R/spp_fxn.R'))
 # SPP-specific functions
 
-##############################################################################=
-### Generate lookup - region ID to spatial raster cell IDs ----
-### * generate half-degree LOICZID raster using Aquamaps cell information
-### * Get percent of each raster cell in the region polygons
-### * create .csv associating raster cell, LOICZID, and CsquareCode
-##############################################################################=
-if(!exists('am_cells_raw')) {
-  file_loc <- file.path(dir_anx, 'raw/aquamaps_2014/tables/hcaf.csv')
-  cat(sprintf('Loading AquaMaps cell data.  Less than 1 minute.\n  %s \n', file_loc))
-  am_cells_raw <- fread(file.path(dir_anx,    'raw/aquamaps_2014/tables/hcaf.csv'))
-  # fread: elapsed time 27 seconds vs  read.csv: elapsed time 100.5 seconds.
-}
-
-am_cells <- am_cells_raw %>%
-  select(csq = CsquareCode, LOICZID, CenterLat, CenterLong, CellArea, OceanArea)
-#rm(am_cells_raw) # remove it if memory is an issue.
-
-### Create raster of .5 degree cells, each identified by LOICZID.  
-###   LOICZID and CsquareCode match 1:1?
-create_loiczid_raster(am_cells, dir_anx, reload = TRUE)
-# saves raster to git-annex/globalprep/SpeciesDiversity/rgns/loiczid_raster.grd and .gri
-
-### Overlay region shapefile on LOICZID raster to create lookup table
-### of region IDs to LOICZID
-###   TO DO:  currently as sp_id; update to translate sp_id to rgn_id
-###   TO DO:  Also include csq, since the AM species lookup uses csq not LOICZID.
-ogr_location <- file.path(dir_neptune_data, 'git-annex/Global/NCEAS-Regions_v2014/data')
-rgn_cell_lookup <- extract_loiczid_per_region(dir_anx, ogr_location, reload = TRUE)
-# saves lookup table to git-annex/globalprep/SpeciesDiversity/rgns/region_prop_df.csv
 
 
 ##############################################################################=
@@ -113,163 +92,89 @@ rgn_cell_lookup <- extract_loiczid_per_region(dir_anx, ogr_location, reload = TR
 
 spp_all <- create_spp_master_lookup(dir_anx, scenario = 'v2015', reload = FALSE)
 ### Output is data frame with these fields:
-### am_sid | SpecCode | sciname | am_status | iucn_sid | iucn_status | popn_trend | popn_status | info_source | spp_group | id_no | objectid | spatial_source
-### ??? do we need SpecCode, objectid? id_no?
+### am_sid | sciname | am_status | iucn_sid | iucn_status | popn_trend | popn_status | info_source | spp_group | id_no | objectid | spatial_source
 ### Outputs saved to:
 ### * v201X/intermediate/spp_iucn_maps_all.csv 
 ###     (list of all species represented in the IUCN shape files)
 ### * v201X/intermediate/spp_all.csv (complete data frame)
-  
-
 
 ##############################################################################=
 ### Generate lookup - IUCN species ID to cell ID ----
 ###   iucn_sid <-> LOICZID
 ##############################################################################=
 
-# Determine intersections of IUCN maps with Aquamaps half-degree cells
+# Determine intersections of IUCN range maps with Aquamaps half-degree cells
 #
 # * from the spp_all.csv, extract the species with IUCN range maps.  Use this file
 #   rather than the spp_iucn_maps_all because it is truncated to just species on spp_iucn_marine_global.csv list.
-# * for each species group (spp_group), open parent shape file
-#   * select by attribute - binomial == sciname in the list
-#   * use union to join multiple polygons with identical sciname. ??? needed?
+# * for each species group (spp_group), load parent shape file
+#   * filter by attribute on binomial field, using sciname from range maps list
 #   * use raster::extract, lay the selected polygons over the LOICZID raster.
-#     * NOTE: this takes a long time - 15-20 minutes for the region shapefile, with ~200 polygons.
-#       We're looking at ~ 4000 polygons, on the order of five hours? probably much more?
-# * when finished with each spp_group, save a file of iucn_sid | LOICZID | proportionArea for that group.
+#     * NOTE: this takes a long time - multiple hours for some of the shape files
+# * when finished with each spp_group, save a file for that group, with fields:
+#       sciname | iucn_sid | LOICZID | prop_area
 
-extract_loiczid_per_spp(dir_anx, scenario = 'v2015', reload = FALSE)
+tmp <- c("AMPHANURA","SEAGRASSES","MANGROVES")
+extract_loiczid_per_spp(dir_anx, scenario = 'v2015', groups_override = tmp, reload = FALSE)
 # use groups_override argument to run function on partial list of species groups.
-
-  
 
 ##############################################################################=
 ### load Aquamaps data on cell IDs and species per cell
 ##############################################################################=
+# Create Aquamaps master spp spatial chart: all species using AM data
+# sciname | am_sid | popn_status | popn_trend | loiczid | prob
+# * filter master species lookup by spatial_source == 'am'
+# * filter out any popn_status NAs
+# * check for subspecies (duplicated scinames)
+# * load AM species spatial data
+# * filter spatial data for loiczids in region lookup
+# * inner_join AquaMaps species spatial data to species lookup 
 
-if(!exists('am_cells_spp_raw')) {
-  file_loc <- file.path(dir_anx, 'raw/aquamaps_2014/tables/ohi_hcaf_species_native.csv')
-  cat(sprintf('Loading AquaMaps cell-species data.  Come back in 10 minutes. \n  %s \n', file_loc))
-  am_cells_spp_raw <- read_csv(file_loc, n_max = 50000)
-  # fread: seems to choke in RStudio.
-  # read.csv: painfully slow, probably ~20 minutes; but RStudio hangs after 5ish.
-  # read_csv: 9 minutes to load 78,168,470 lines.  No problems reported.
-}
+##############################################################################=
+### Generate lookup - region ID to spatial raster cell IDs ----
+### * generate half-degree LOICZID raster using Aquamaps cell information
+### * Get percent of each raster cell in the region polygons
+### * create .csv associating raster cell, LOICZID, and CsquareCode
+##############################################################################=
 
-am_cells_spp <- am_cells_spp_raw %>%
-  select(sid = SpeciesID, csq = CsquareCode, probability)
+### Load or generate lookup table of region IDs to loiczid identifier
+###   TO DO:  currently as sp_id; update to translate sp_id to rgn_id
+###   TO DO:  Also include csq, since the AM species lookup uses csq not LOICZID.
+rgn_cell_lookup <- extract_loiczid_per_region(dir_anx, reload = FALSE)
+# ??? DOES THIS STILL INCLUDE ALL THE HS and AQ SHIT? get rid of those!
+# saves lookup table to git-annex/globalprep/SpeciesDiversity/rgns/region_prop_df.csv
 
-am_cells1 <- am_cells %>%
-  select(csq, LOICZID, CellArea) 
-### drop lat/long; use CellArea because the weights from extract() will allocate proportional area
+# load and format AquaMaps half-degree cell authority file
+file_loc <- file.path(dir_anx, 'raw/aquamaps_2014/tables/hcaf.csv')
+cat(sprintf('Loading AquaMaps half-degree cell authority file.  Less than 1 minute.\n  %s \n', file_loc))
+am_cells <- fread(file_loc, header = TRUE, stringsAsFactors = FALSE) %>%
+  as.data.frame() %>%
+  select(csq = CsquareCode, loiczid = LOICZID, cell_area = CellArea)
 stopifnot(sum(duplicated(am_cells$csq)) == 0)
 
-# * Merge the cells_spp and cells, by csq.  This associates LOICZID with
-#   each species identifier, instead of csq.  
-# * Drop csq.
-# * Attach species info (spp) via Aquamaps species ID code.  This brings with it
-#   IUCN category, and IUCN species ID (sid), for each species in each cell.
-# * This is going to be a massive data.frame.  Consider breaking into small ones
-#   * by species group? (maybe family or some such)?
-#   * The big file is just SpeciesID and csq.  Maybe divide by chunks of csq, first portion?
-#   * or chop off the first bit of speciesID and group that way?
-# * for each smaller data frame, process it down to summaries of total score per species.
-#   * this implies it would be best to group by family groups, not by cell ID.
-am_loiczid_spp <- data.table(am_cells, key = 'csq') %>%
-  merge(data.table(am_cells_spp, key = 'csq'))
+am_cells_rgn <- am_cells %>%
+  filter(loiczid %in% rgn_cell_lookup$loiczid)
 
-x <- am_cells_spp_raw %>% select(SpeciesID) %>% mutate(SpeciesID = str_split(SpeciesID, '-', n = 1))
- 
-# # ??? These are files from HS-AQ 2014 for testing ----
-# am_cells2013     <- fread(file.path(dir_anx,    'raw/aquamaps_2013/tables/cells.csv'))
-# # ??? compare this to Melanie's am_cellsx <- read.csv(file.path(dir_neptune_data, "/model/GL-NCEAS-SpeciesDiversity_v2013a/tmp/am_cells_data.csv")
-# am_cells_spp2013 <- read_csv(file.path(dir_anx, 'raw/aquamaps_2013/tables/cells_spp.csv'))
-# am_spp2013       <- fread(file.path(dir_anx,    'raw/aquamaps_2013/tables/spp.csv'))
+file_loc <- file.path(dir_anx, 'raw/aquamaps_2014/tables/ohi_hcaf_species_native.csv')
+cat(sprintf('Loading AquaMaps cell-species data.  Come back in 10 minutes. \n  %s \n', file_loc))
+am_cells_spp <- read_csv(file_loc, col_types = '_ccn__', n_max = 500000) %>%
+  rename(am_sid = SpeciesID, csq = CsquareCode, prob = probability)
 
-# am_cells2013: ??? This should be Aquamaps data but possibly modified by one of BB's scripts.  Which one?
-#   cid  csq  rgn_id  id_typ  rgn_nam  area_km2  category_linear_cnt  category_linear_avg  
-#   popn_trend_linear_cnt  popn_trend_linear_avg  category_linear_score  popn_trend_linear_avg_linear_2012chg_cnt
-#   category_linear_2012chg_cnt  category_linear_2012chg_avg  category_linear_2012chg_score  popn_trend_linear_2012chg_cnt  popn_trend_linear_2012chg_avg
-# many of these fields created in script below (or similar): 
-#   category_linear_cnt2  category_linear_avg2  popn_trend_linear_cnt2  popn_trend_linear_avg2  rgn_spp_score_2013
+### ??? START HERE ----
+# filter entire aquamaps table to just cells found in global regions. 
+am_cells_spp1 <- am_cells_spp %>% 
+  filter(csq %in% am_cells_rgn$csq) %>% # this filter quickly reduces by about 20%?
+  filter(prob >= .40)                   # this filter quickly reduces by about 50% with prev.
 
-# am_cells:
-#   V1 CsquareCode LOICZID NLimit Slimit WLimit ELimit CenterLat CenterLong CellArea OceanArea CellType PWater FAOAreaM FAOAreaIn CountryMain CountrySecond CountryThird
-#   CountrySubMain CountrySubSecond CountrySubThird EEZFirst EEZSecond EEZThird EEZFourth EEZFifth EEZSixth EEZAll EEZRemark LME LME_2010 LMEBorder OceanBasin Longhurst
-#   IslandsNo Area0_20 Area20_40 Area40_60 Area60_80 Area80_100 AreaBelow100 ElevationMin ElevationMax ElevationMean ElevationSD DepthMin DepthMax DepthMean DepthSD SSTMnMin
-#   SSTMnMax SSTAnMean SSTAnSD SSTMnRange SBTAnMean SalinityMin SalinityMax SalinityMean SalinitySD SalinityBMean PrimProdMean IceConAnn Shelf    Slope IceConSpr IceConSum
-#   IceConFal IceConWin LandDist WaveHeight TidalRange  Abyssal Coral Estuary Seamount MPA          SST1950          SBT1950     Salinity1950    SalinityB1950     PrimProd1950
-#   IceCon1950          SST1999          SBT1999     Salinity1999    SalinityB1999     PrimProd1999 IceCon1999          SST2050          SBT2050     Salinity2050
-#   SalinityB2050     PrimProd2050 IceCon2050          SST2100          SBT2100     Salinity2100    SalinityB2100     PrimProd2100 IceCon2100         SST1950c
-#   SBT1950c    Salinity1950c   SalinityB1950c PrimProd1950c IceCon1950c         SST2050c         SBT2050c    Salinity2050c   SalinityB2050c PrimProd2050c IceCon2050c
-#   SST2100c        SBT2100c    Salinity2100c   SalinityB2100c IceCon2100c PrimProd2100c
+# then join.
+ptm <- proc.time()
+am_cells_spp2 <- am_cells_spp %>%
+  inner_join(am_cells_rgn, by = 'csq') %>%
+  select(-csq)
+proc.time() - ptm # 6ish sec
 
-# am_cells_spp2013:
-#   cid  sid
-# am_cells_spp:
-#   [EMPTY]  SpeciesID	CsquareCode	probability	BoundBoxYN	FAOAreaYN
-
-# am_spp2013:
-#   sid  sciname  class  order  family  genus  species  authority  modified_year
-#   category  criteria  habitat  popn_trend  shp_dbf  shp_grp  shp_cnt  src_distn  
-#   am_speciesid  TEXT  category_2012chg  popn_trend_2012chg
-# am_spp:
-#   V1  SPECIESID  SpecCode  Genus  Species  FBname  Kingdom  Phylum  Class  
-#   Order  Family  iucn_code  expert_id
-
-
-# am_cells:     links  CsquareCode <-> LOICZID.
-# am_cells_spp: links  species ID  <-> CsquareCode
-# am_spp:       links  IUCN code   <-> species ID; but are we keeping this?
-
-
-####################################################################################
-# The following explores the extent of differences when the IUCN species are
-# assigned to one aquamap cell...
-# (result: basically produces same results) ----
-####################################################################################
-
-# for Aquamaps - ignore IUCN categories and trends!  bring those in from scraped IUCN .htm files
-# create lookup table of weights for extinction risk categories and population trends
-category   <- data.frame(category   = c('LC', 'NT', 'VU', 'EN', 'CR', 'EX'), 
-                         catScore   = c(  0,   0.2,  0.4,  0.6,  0.8,   1 ))
-popn_trend <- data.frame(popn_trend = c('Decreasing', 'Stable', 'Increasing'), 
-                         trendScore = c(   -0.5,          0,         0.5))
-
-# reformat our am_spp: sid, sciname
-am_spp <- am_spp %>%
-  unite(sciname, Genus, Species, sep = ' ')
-
-am_spp1 <- am_spp %>%
-  # ??? sid is number part of SPECIESID? or SpecCode?
-  # ??? This should be done in the extraction of am_spp?
-  # ??? category and popn_trend are not in am_spp file.
-  select(sid = SPECIESID, category, popn_trend) %>%
-  left_join(category) %>%
-  left_join(popn_trend) 
-
-# calculate average weight and count for category and trend scores
-scoreData <- cells_spp %>%
-  group_by(LOICZID) %>%
-  inner_join(spp) %>% 
-  summarise(category_linear_cnt2 = length(category[!(category %in% 'DD')]),
-            category_linear_avg2 = mean(catScore, na.rm=TRUE),
-            popn_trend_linear_cnt2 = sum(!is.na(trendScore)),
-            popn_trend_linear_avg2 = mean(trendScore, na.rm=TRUE))
-
-cells_2013 <- cells_2013 %>%
-  #left_join(am_cells, by='csq') %.%
-  left_join(scoreData, by='LOICZID')
-
-# rescale lower end of the biodiversity goal to be 0 when 75% species are extinct, a level comparable to the five documented mass extinctions
-cells_2013$category_linear_score2 <- ((1 - cells_2013$category_linear_avg2) - 0.25) / 0.75 * 100
-
-
-# calculate area-weighted regional scores ----
-regionScores <- cells_2013 %.%
-  group_by(rgn_id) %.%
-  summarise(rgn_spp_score_2013=sum(category_linear_score2*area_km2)/sum(area_km2),
-            rgn_spp_trend_2013=sum(popn_trend_linear_avg2*area_km2)/sum(area_km2))
-
-
+ptm <- proc.time()
+am_cells_spp3 <- am_cells_spp1 %>%
+  left_join(am_cells_rgn, by = 'csq') %>%
+  select(-csq)
+proc.time() - ptm # 5ish sec
