5adc412b682622c0fb394518fb3f6528f8f3dae9
Casey O'Hara <ohara@nceas.ucsb.edu>
2015-06-09 15:23:43 -0700

    subpop functions

--- globalprep/SpeciesDiversity/R/ingest_iucn.R
+++ globalprep/SpeciesDiversity/R/ingest_iucn.R
@@ -6,9 +6,9 @@
 
 # # load packages - note this should be sourced by data_prep.R, so no need to load libraries already loaded from there.
 # library(ohi)
-# library(RCurl)
-# library(XML)
-# library(parallel)
+library(RCurl)
+library(XML)
+library(parallel)
 # library(plyr)
 # 
 # library(foreign)
@@ -34,179 +34,247 @@
 # wd = '/var/data/ohi/git-annex/globalprep/SpeciesDiversity' # (Mac/Linux)
 # setwd(wd)
 
-#######################################################
-# get all species
-all_spp_file <- file.path(dir_anx, scenario, 'intermediate/spp_iucn_all.csv')
-all_spp_url  <- 'http://api.iucnredlist.org/index/all.csv'
-if (!file.exists(all_spp_file) | reload){
-  cat(sprintf('Reading full IUCN redlist species from: %s\n', all_spp_url))
-  spp_iucn_all <- read.csv(all_spp_url)                     # nrows = 122843
-  spp_iucn_all <- spp_iucn_all[!duplicated(spp_iucn_all), ] # remove duplicates # nrows = 120484
-  spp_iucn_all <- spp_iucn_all %>%
-    rename(sciname  = Scientific.Name,
-           iucn_sid = Red.List.Species.ID,
-           category = Category)  
-  cat(sprintf('Writing full IUCN redlist species list to: %s\n', all_spp_url))
-  write_csv(spp_iucn_all, all_spp_file)
-} else {
-  cat(sprintf('Reading full IUCN redlist species list from: %s\n', all_spp_url))
-  spp_iucn_all <- read.csv(all_spp_file)
+#############################################################################=
+generate_iucn_all_spp_list <- function(reload = FALSE) {
+### get full IUCN species list from git-annex or web.  If generating a new
+### dataframe, saves to git-annex and returns dataframe; otherwise reads
+### then returns the dataframe.
+#############################################################################=
+  
+  all_spp_file <- file.path(dir_anx, scenario, 'intermediate/spp_iucn_all.csv')
+  all_spp_url  <- 'http://api.iucnredlist.org/index/all.csv'
+  if (!file.exists(all_spp_file) | reload){
+    cat(sprintf('Reading full IUCN redlist species from: %s\n', all_spp_url))
+    spp_iucn_all <- read.csv(all_spp_url) 
+    spp_iucn_all <- spp_iucn_all[!duplicated(spp_iucn_all), ] 
+    spp_iucn_all <- spp_iucn_all %>%
+      rename(sciname  = Scientific.Name,
+             iucn_sid = Red.List.Species.ID) %>%
+      select(-Primary)
+    spp_iucn_all <- unique(spp_iucn_all)
+    names(spp_iucn_all) = tolower(names(spp_iucn_all))
+    
+    cat(sprintf('Writing full IUCN redlist species list to: %s\n', all_spp_file))
+    write_csv(spp_iucn_all, all_spp_file)
+  } else {
+    cat(sprintf('Reading full IUCN redlist species list from: %s\n', all_spp_file))
+    spp_iucn_all <- read.csv(all_spp_file)
+  }
+  return(invisible(spp_iucn_all))
 }
 
-# function to extract just habitat from the IUCN API given the iucn_sid (= Red.List.Species.ID)
+spp_iucn_all <- generate_iucn_all_spp_list()
+  
+
+#############################################################################=
 getHabitats <- function(sid, download_tries = 10) {
+### function to extract just habitat from the IUCN API given the iucn_sid 
+### (= Red.List.Species.ID).
+### support function for generate_iucn_habitat_list().
+  
   url <- sprintf('http://api.iucnredlist.org/details/%d/0', sid)
   htm <- file.path(dir_anx, sprintf('cache/iucn_details/%d.htm', sid))
 
   i <- 0
   if(!file.exists(htm)) cat(sprintf('No file found for %s.  Attempting to download from %s. ', htm, url))
-  else cat(sprintf('.htm file found for %s.  No need to download. \n', htm))
+  # else cat(sprintf('.htm file found for %s.  No need to download. \n', htm))
   while (!file.exists(htm) | (file.info(htm)$size == 0 & i < download_tries)) {
     download.file(url, htm, method = 'auto', quiet = TRUE, mode = 'wb')
     i=i+1
     cat(sprintf('%d... ', i))
   }
-  if (file.info(htm)$size == 0) stop(sprintf('Only getting empty file for: %s', url))
-  cat('\n')
-  
+  if (file.info(htm)$size == 0) stop(sprintf('Only getting empty file for: %s \n', url))
+
   h <- htmlParse(htm)
   habitats = xpathSApply(h, '//li[@class ="system"]', xmlValue)
   return(setNames(rep(sid, length(habitats)), habitats))
 }
 
-spp_hab_file <- file.path(dir_anx, scenario, 'intermediate/spp_iucn_habitats.csv')
-if (!file.exists(spp_hab_file) | reload) {  
-  print(system.time({    
-    #r <- lapply(spp_iucn_all$iucn_sid, getHabitats) # DEBUG
-    r <- mclapply(spp_iucn_all$iucn_sid, getHabitats, mc.cores = detectCores(), mc.preschedule = F) # took ~ 4 hrs on neptune
-  }))
-  r <- unlist(r)
-  spp_iucn_habitats <- data.frame(iucn_sid = r, habitat = names(r))
-  write_csv(spp_iucn_habitats, spp_hab_file)
-} else {
-  spp_iucn_habitats <- read.csv(spp_hab_file)
+
+#############################################################################=
+generate_iucn_habitat_list <- function(spp_list = spp_iucn_all, reload = FALSE) {
+### Applies the getHabitat() function to all lines within the spp_list
+### dataframe, and creates a master list of habitat per species ID.
+### If generating a new list, saves to git-annex then returns dataframe;
+### otherwise reads from git-annex and returns.
+  spp_hab_file <- file.path(dir_anx, scenario, 'intermediate/spp_iucn_habitats.csv')
+  if (!file.exists(spp_hab_file) | reload) {
+    if(!file.exists(spp_hab_file)) cat(sprintf('No species-habitat file found at %s. '))
+    cat(sprintf('Generating species habitat list.\n', spp_hab_file))
+    print(system.time({    
+      # r <- lapply(spp_list$iucn_sid, getHabitats) # DEBUG
+      r <- mclapply(spp_list$iucn_sid, getHabitats, mc.cores = detectCores(), mc.preschedule = FALSE) 
+            # runs faster across multiple cores, but no reporting out from within getHabitats()
+    }))
+    r <- unlist(r)
+    spp_iucn_habitats <- data.frame(iucn_sid = r, habitat = names(r))
+    cat(sprintf('Writing species-habitat list to:\n  %s\n', spp_hab_file))
+    write_csv(spp_iucn_habitats, spp_hab_file)
+  } else {
+    cat(sprintf('Reading species-habitat list from:\n  %s\n', spp_hab_file))
+    spp_iucn_habitats <- read.csv(spp_hab_file)
+  }
+  return(invisible(spp_iucn_habitats))
 }
 
-# check no remaining download errors. might need to manually run. should be 0:
-download_errors <- spp_iucn_habitats %>%
-  filter(is.na(habitat))
-cat(sprintf('Download errors (habitat == NA) - should be zero: %d\n', download_errors))
+spp_iucn_habitats <- generate_iucn_habitat_list(reload = TRUE)
 
-# species without habitats assigned (n=67) - mostly birds (n=58)
-sid_miss = setdiff(spp_iucn_all$iucn_sid, spp_iucn_habitats$iucn_sid)
-print(addmargins(table(filter(spp_iucn_all, iucn_sid %in% sid.miss)$Class, useNA='ifany')))
-#       AMPHIBIA           AVES CHONDRICHTHYES      CRUSTACEA     GASTROPODA  HOLOTHUROIDEA        INSECTA       MAMMALIA            Sum 
-#              1             58              1              1              2              2              1              1             67
 
-# get counts of species / subpopulations
-print(addmargins(table(spp_iucn_habitats$habitat, useNA='ifany')))
-#head(spp_iucn_habitats[is.na(spp_iucn_habitats$habitat),])
-#  Freshwater      Marine Terrestrial         Sum 
-#       25245        8380       50135       83760 
-
-#######################################################################
-
-# get distinct marine species
-options('stringsAsFactors'=T)
-spp_iucn_all = read.csv('tmp/spp_iucn_all.csv')
-spp_iucn_habitats = read.csv('tmp/spp_iucn_habitats.csv')
-d = merge(spp_iucn_all, subset(spp_iucn_habitats, habitat=='Marine'), by.x='Red.List.Species.ID', by.y='sid')
-nrow(d) # 8380
-length(unique(d$Scientific.Name)) # 8178
-print(addmargins(table(d$Class, useNA='ifany')))
-#     ACTINOPTERYGII           ANTHOZOA               AVES           BIVALVIA CEPHALASPIDOMORPHI        CEPHALOPODA      CHLOROPHYCEAE     CHONDRICHTHYES          CRUSTACEA         ECHINOIDEA             ENOPLA 
-#               3332                842                839                 34                  4                195                  1               1113                256                  1                  1 
-#    FLORIDEOPHYCEAE         GASTROPODA      HOLOTHUROIDEA           HYDROZOA            INSECTA         LILIOPSIDA      MAGNOLIOPSIDA           MAMMALIA        MEROSTOMATA             MYXINI       PHAEOPHYCEAE 
-#                 58                806                369                 16                  1                 78                 64                170                  4                 76                 15 
-#         POLYCHAETA     POLYPODIOPSIDA           REPTILIA      SARCOPTERYGII        ULVOPHYCEAE                Sum 
-#                  2                  3                 97                  2                  1               8380
-names(d) = tolower(names(d))
-d = rename(d, c('red.list.species.id'='sid','scientific.name'='sciname'))
-d = d[, !names(d) %in% c('primary')]
-
-# remove infraranks: 30 subspecies records ('ssp.')
-d = subset(d, infrarank.type!='ssp.', !names(d) %in% c('infrarank','infrarank.type','infrarank.authority'))
-# CCO: subset dataframe 'd', rows != ssp, and deselect infrarank columns.
-table(d$category)
-#    CR    DD    EN    EW    EX    LC LR/cd LR/lc LR/nt    NT    VU 
-#   147  2175   228     0    24  4557     6     5    12   518   678
-
-# convert IUCN category from IUCN Red List Categories & Criteria v2.3 (1994-2000) to v3.1 (since 2001)
-# http://en.wikipedia.org/wiki/Wikipedia:Conservation_status
-#   LR/cd: Least Risk / Conservation Dependent -> NT
-#   LR/nt: Least Risk / Near Threatened -> NT
-#   LR/lc: Least Risk / Least Concern -> LC
-x.cat = c('LR/cd'='NT','LR/nt'='NT','LR/lc'='LC')
-for (i in 1:length(x.cat)){ # g = c('LR/cd'='NT','LR/nt'='NT','LR/lc'='LC')[1]
-  idx = which(d$category==names(x.cat)[i])
-  print(sprintf('%s: %d', x.cat[[i]], length(idx)))
-  d$category[idx] = x.cat[[1]]  
+#############################################################################=
+ingest_iucn_errorcheck1 <- function() {
+  # check no remaining download errors. might need to manually run. should be 0:
+  cat(sprintf('Download errors (habitat == NA) - should be zero: %d\n', 
+              nrow(spp_iucn_habitats %>% filter(is.na(habitat)))))
+  
+  # species without habitats assigned (n=67) - mostly birds (n=58)
+  sid_miss <- setdiff(spp_iucn_all$iucn_sid, spp_iucn_habitats$iucn_sid)
+  print(addmargins(table(filter(spp_iucn_all, iucn_sid %in% sid_miss)$class, useNA = 'ifany')))
+  #   BIVALVIA    GASTROPODA MAGNOLIOPSIDA      REPTILIA           Sum 
+  #          2             2             3             1             8 
+  # get counts of species / subpopulations
+  print(addmargins(table(spp_iucn_habitats$habitat, useNA='ifany')))
+  #   Freshwater      Marine Terrestrial         Sum 
+  #        43501       19779       77225      140505 
 }
-d$category = factor(x=as.character(d$category), 
-                                  levels=c('DD','LC','NT','VU','EN','CR','EX'), ordered=T)
-table(d$category)
-#   DD   LC   NT   VU   EN   CR   EX 
-# 2175 4580  518  678  228  147   24
 
-# categories used:
-#   EX = Extinct (if not EX in 2012)           
-#   CR = Critically Endangered
-#   EN = Endangered
-#   VU = Vulnerable
-#   NT = Near Threatened
-#   LC = Least Concern
-# other:
-#   DD = Data deficient
-#   NE = Not evaluated
-# missing:
-#   EW = Extinct in the wild
-#   PE = Probably extinct (informal)
-#   PEW = Probably extinct in the wild (informal)
+ingest_iucn_errorcheck1()
 
-# # only include EXtinct species if previously showed up in 2012 not as EXtinct
-# # UPDATE DECISION 2013-08-13: including ALL EXtinct species for 2013 vs not doing so last year because of baseline reference issues.
-# spp_2012 = read.csv('tmp/spp_2012.csv')
-# sci.ex = as.character(subset(spp_2012, as.character(scientific) %in% subset(d, category=='EX', sciname, drop=T), scientific, drop=T))
-# sci.ex
-# # [1] "Oncorhynchus nerka" # only one species (sockeye salmon), and just for 5 local subpopulations
-# subset(d, category=='EX' & sciname %in% sci.ex)
-# d = subset(d, category !='EX' | sciname %in% sci.ex)
-# table(d$category)
-#   DD   LC   NT   VU   EN   CR   EX 
-# 2175 4580  518  678  228  147    5
+#####################
 
-table(d$category)
-#   DD   LC   NT   VU   EN   CR   EX 
-# 2175 4580  518  678  228  147   24
+get_mar_spp <- function(reload = FALSE) {
+  spp_iucn_marine_file <- file.path(dir_anx, scenario, 'intermediate/spp_iucn_marine.csv')
+  
+  if(!file.exists(spp_iucn_marine_file) | reload) {
+    # get distinct marine species
+    spp_iucn_all      <- read.csv(all_spp_file, stringsAsFactors = FALSE)
+    spp_iucn_habitats <- read.csv(spp_hab_file, stringsAsFactors = FALSE)
+    
+    df <- spp_iucn_all %>%
+      inner_join(spp_iucn_habitats %>% 
+                   filter(habitat == 'Marine'),
+                 by = 'iucn_sid')
+    df_dupes <- duplicated(df)
+    df <- df %>% unique() # why so many duplicates?
+    
+    # remove infraranks: 30 subspecies records ('ssp.')
+    #d = subset(d, infrarank.type!='ssp.', !names(d) %in% c('infrarank','infrarank.type','infrarank.authority'))
+    df <- df %>%
+      filter(infrarank.type != 'ssp.') %>%
+      select(-infrarank, -infrarank.type, -infrarank.authority)
+    
+    # convert IUCN category from IUCN Red List Categories & Criteria v2.3 (1994-2000) to v3.1 (since 2001)
+    df <- df %>%
+      mutate(category = ifelse(category %in% c('LR/cd', 'LR/nt', 'LR/lc'), 'NT',
+                               ifelse (category == 'LR/lc', 'LC',
+                                       category)))
+    
+    
+    spp_iucn_marine_file <- file.path(dir_anx, scenario, 'intermediate/spp_iucn_marine.csv')
+    cat(sprintf('Writing IUCN marine species file to: \n  %s\n', spp_iucn_marine_file))
+    write_csv(df, spp_iucn_marine_file)
+  } else {
+    # get list of unique species and their global extinction status
+    cat(sprintf('Reading IUCN marine species file from: \n  %s\n', spp_iucn_marine_file))
+    df <- read.csv(spp_iucn_marine_file, stringsAsFactors = FALSE)
+  }
+  return(df)
+}
 
-nrow(subset(d, category!='DD')) # 6175
-write.csv(d, 'tmp/spp_iucn_marine.csv', row.names=F, na='')
-
-# get list of unique species and their global extinction status
-d = read.csv('tmp/spp_iucn_marine.csv')
+spp_iucn_mar <- get_mar_spp()
 
 # function to extract just habitat from the IUCN API given the Red.List.Species.ID
-getDetails = function(sid, download.tries=10){
+getDetails = function(sid, download_tries = 10) {
   # example species Oncorhynchus nerka: sid=135301 # (parent) ## sid=135322 # (child)  # sid=4162
-  url = sprintf('http://api.iucnredlist.org/details/%d/0', sid)
-  htm = sprintf('cache/iucn_details/%d.htm', sid) # htm = '135322.htm'
-  i=0
-  while ( !file.exists(htm) | (file.info(htm)$size==0 & i<download.tries) ){
-    download.file(url, htm, method='auto', quiet=T, mode='wb')
-    i=i+1
+  url <- sprintf('http://api.iucnredlist.org/details/%d/0', sid)
+  htm <- sprintf('cache/iucn_details/%d.htm', sid) # htm = '135322.htm'
+  
+  i <- 0
+  while (!file.exists(htm) | (file.info(htm)$size == 0 & i < download_tries)){
+    download.file(url, htm, method = 'auto', quiet = TRUE, mode = 'wb')
+    i <- i + 1
   }
-  if (file.info(htm)$size==0) stop(sprintf('Only getting empty file for: %s', url))
-  h = htmlParse(htm)
-  subpopulations.href = xpathSApply(h, '//li[@class ="subspecie"]/a', xmlGetAttr, 'href')
-  subpopulations = as.integer(sub('/details/([0-9]+)/0', '\\1', subpopulations.href))  
-  countries = xpathSApply(h, '//ul[@class="country_distribution"]/li/ul/li', xmlValue)
-  popn_trend = xpathSApply(h, '//div[@id="population_trend"]', xmlValue)
+  
+  if (file.info(htm)$size == 0) stop(sprintf('Only getting empty file for: %s', url))
+  h <- htmlParse(htm)
+  subpops_href <- xpathSApply(h, '//li[@class ="subspecie"]/a', xmlGetAttr, 'href')
+  subpops      <- as.integer(sub('/details/([0-9]+)/0', '\\1', subpops_href))  
+  popn_trend   <- xpathSApply(h, '//div[@id="population_trend"]', xmlValue)
   
   # NOTE greater specificity 
   #   for http://www.iucnredlist.org/details/135322/0 -- United States (Alaska) 
   #   vs  http://api.iucnredlist.org/details/135322/0 -- United States
   #countries = xpathSApply(h, '//td[@class="label"][strong="Countries:"]/following-sibling::td/div/text()', xmlValue)    
   
-  return(list(subpopulations=subpopulations, countries=countries, popn_trend=popn_trend))
+  return(list(subpops = subpops, popn_trend = popn_trend))
 }
+
+get_trend_and_subpops <- function(df = spp_iucn_mar) {
+  # get list of all subpopulations and countries
+  spp_subpop     <- data.frame() # initialize
+  spp_popn_trend <- data.frame()
+  
+  for (sid in df$iucn_sid) { # sid = df$sid[1]
+    sp <-  getDetails(sid)
+    # subpop
+    if (length(sp$subpops) > 0) {
+      cat(sprintf('Found subpopulations for parent %d: %d\n', sid, sp_subpops))
+      sp_subpop = data.frame(parent_sid = sid, subpop_sid = sp$subpops)
+      spp_subpop = rbind(spp_subpop, sp_subpop)
+    }
+    # popn_trend
+    if (length(sp$popn_trend) > 0) {
+      sp_popn_trend <- data.frame(iucn_sid = sid, popn_trend = sp$popn_trend)
+      spp_popn_trend = rbind(spp_popn_trend, sp_popn_trend)
+    }  
+  }
+  cat(sprintf('Joining trends and subspecies data frames: %d and %d rows respectively.\n', nrow(spp_popn_trend), nrow(spp_subpop)))
+  spp_trend_subpops <- full_join(spp_popn_trend, 
+                                 spp_subpop, 
+                                 by = c('iucn_sid' = 'parent_sid'))
+  write_csv(spp_trend_subpops, file.path(dir_anx, 'tmp/trend_and_subpops.csv'))
+  return(spp_trend_subpops)
+}
+
+spp_trend_subpops <- get_trend_and_subpops(spp_iucn_mar)
+
+# assign popn_trend to d, before subsetting into subpopulations and global species and 
+spp_iucn_mar_trend <- spp_iucn_mar %>%
+  left_join(spp_trend_subpops, 
+            by = 'iucn_sid')
+
+summary(d$popn_trend)
+#    Unknown Decreasing     Stable Increasing       NA's 
+#       5559       1338       1002        193        258 
+
+
+# manual subpopulations discovered later, somehow not listed in the IUCN details page of parent 
+# (again difference b/n API and WWW versions), after defining spp_global with the following:
+#   subset(spp_global, sciname %in% spp_global$sciname[duplicated(spp_global$sciname)])
+spp_subpop.manual = c('Pristis pectinata'        = 18175,
+                      'Centrophorus moluccensis' = 42838,
+                      'Pristis pristis'          = 18584848)
+for (i in 1:length(spp_subpop.manual)) { # i=1
+  parent_sid = spp_subpop.manual[[i]]
+  sp_subpop = data.frame(parent_sid=parent_sid, 
+                         sid=subset(d, sciname %in% names(spp_subpop.manual)[i] & sid!=parent_sid, sid, drop=T))
+  spp_subpop = rbind(spp_subpop, sp_subpop)
+}
+#write.csv(spp_subpop   , 'tmp/spp_iucn_marine_subpopulations.csv', row.names=F, na='')
+#write.csv(spp_countries, 'tmp/spp_iucn_marine_countries.csv', row.names=F, na='')
+
+# TODO: next time remove data deficient (DD) first, before using to extract rangemaps, for expediency
+
+
+
+# get list of subpopulations and countries, for overriding global category with local one
+#   TODO: ISSUE: can have more than one subpopulation with different categories in the same country, eg 135301 sockey salmon
+# tmp/spp_iucn_marine_subpop.csv: sid, parent_sid, sciname, category
+# tmp/spp_iucn_marine_subpop_countries.csv: sid, country
+spp_subpop = merge(spp_subpop, d[, c('sid','sciname','category','popn_trend')], by='sid')
+spp_subpop_countries = subset(spp_countries, sid %in% spp_subpop$sid)
+cat(sprintf('\nFound %d marine subpopulations for %d species in %d countries.\n', nrow(spp_subpop), length(unique(spp_subpop$parent_sid)), nrow(spp_subpop_countries)))
+# Found 177 marine subpopulations for 51 species in 736 countries.
+write.csv(spp_subpop          , 'tmp/spp_iucn_marine_subpop.csv'          , row.names=F, na='')
+write.csv(spp_subpop_countries, 'tmp/spp_iucn_marine_subpop_countries.csv', row.names=F, na='')
+
 
