fee49fb1ff7b6662f3147c4a11ddcf039f13e315
Melsteroni <frazier@nceas.ucsb.edu>
2015-06-11 10:44:15 -0700

    start of b/bmsy explore for FIS

---
+++ b_bmsy_explore.R
@@ -0,0 +1,14 @@
+##########################################
+## B/bmsy calculations: testing phase
+## purpose is to develop a script to 
+## test b/bmsy values.  These are test values
+## because we are expecting the data to be
+## updated.
+##########################################
+
+library(devtools)
+
+## source b/bmsy script
+devtools::install('../datalimited/datalimited')
+library(datalimited)
+?cmsy
