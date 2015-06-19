fb018118dbe2a986b6b4487b41d252bfa631c63a
Jamie Afflerbach <afflerbach@neptune.nceas.ucsb.edu>
2015-06-19 12:43:16 -0700

    init commit

---
+++ globalprep/Pressures_SST/scripts/create_layer.R
@@ -0,0 +1,41 @@
+# Create SST layers
+
+library(raster)
+
+#paths
+
+dir_N = c('Windows' = '//neptune.nceas.ucsb.edu/data_edit',
+          'Darwin'  = '/Volumes/data_edit',
+          'Linux'   = '/var/data/ohi')[[ Sys.info()[['sysname']] ]]
+
+setwd(file.path(dir_N,'git-annex/globalprep/Pressures_SST'))
+
+# set tmp directory
+
+	tmpdir='~/big/R_raster_tmp'
+	dir.create(tmpdir, showWarnings=F)
+	rasterOptions(tmpdir=tmpdir)
+
+# data
+
+# Bring in SSTA
+
+ssta = stack('data/cortadv5_SSTA.nc',varname='SSTA')
+weekly_sst = stack('data/cortadv5_WeeklySST.nc',varname='WeeklySST')
+
+# Second Loop to calculate annual positive anomalies
+
+# Bring in 
+
+for (i in 1982:2012){
+  
+  s = stack()
+  
+  for (j in 1:53){
+    
+    sd = raster(paste0('tmp/sd_sst_week_',j,'.tif'))
+    
+    
+    
+  }
+}
