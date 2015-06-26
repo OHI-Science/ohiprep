e812a12cc2e66079525284accd0ab0d2473fc07f
jafflerbach <afflerbach@nceas.ucsb.edu>
2015-06-26 15:21:14 -0700

    updated npp aggregation across years and standardizing catch by npp

--- globalprep/Pressures_fishing/v2015/create_fish_pressure_layers.R
+++ globalprep/Pressures_fishing/v2015/create_fish_pressure_layers.R
@@ -23,6 +23,18 @@ dir_N = c('Windows' = '//neptune.nceas.ucsb.edu/data_edit',
 
 setwd(file.path(dir_N,'git-annex/globalprep/Pressures_fishing'))
 
+# set tmp directory
+
+# set tmp directory
+tmpdir=file.path(dir_N,'home_big/afflerbach/R_raster_tmp')
+dir.create(tmpdir, showWarnings=F)
+rasterOptions(tmpdir=tmpdir)
+
+# set mollweide projection
+moll_crs = CRS("+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs")
+
+
+ocean = raster(file.path(dir_N, 'model/GL-NCEAS-Halpern2008/tmp/ocean.tif'))
 #-----------------------------------------------------------------------------
 #READ IN DATA
 #-----------------------------------------------------------------------------
@@ -32,16 +44,42 @@ setwd(file.path(dir_N,'git-annex/globalprep/Pressures_fishing'))
 catch_06_10 = raster('v2015/catch_06_10_1km.tif')
 catch_05_09 = raster('v2015/catch_05_09_1km.tif')
 catch_04_08 = raster('v2015/catch_04_08_1km.tif')
+catch_03_07 = raster('v2015/catch_03_07_1km.tif')
 
 #Primary productivity at 1km - aggregate to time periods
 
+npp = list.files(file.path(dir_N,'git-annex/globalprep/VGPM_primary_productivity/v_2015/output'),pattern='annual_mean_npp_2',full.names=T)
 
+npp_06_10 = calc(stack(npp[substr(npp,113,116) %in% 2006:2010]),fun=function(x){mean(x,na.rm=T)},progress='text')%>%
+             projectRaster(.,crs=moll_crs,progress='text',over=T)%>%
+              resample(.,ocean,method='ngb',filename='v2015/npp/npp_06_10.tif')
+
+npp_05_09 = calc(stack(npp[substr(npp,113,116) %in% 2005:2009]),fun=function(x){mean(x,na.rm=T)},progress='text')%>%
+               projectRaster(.,crs=moll_crs,progress='text',over=T)%>%
+                resample(.,ocean,method='ngb',filename='v2015/npp/npp_05_09.tif')
+
+npp_04_08 = calc(stack(npp[substr(npp,113,116) %in% 2004:2008]),fun=function(x){mean(x,na.rm=T)},progress='text')%>%
+             projectRaster(.,crs=moll_crs,progress='text',over=T)%>%
+              resample(.,ocean,method='ngb',filename='v2015/npp/npp_04_08.tif')
+
+npp_03_07 = calc(stack(npp[substr(npp,113,116) %in% 2003:2007]),fun=function(x){mean(x,na.rm=T)},progress='text')%>%
+             projectRaster(.,crs=moll_crs,progress='text',over=T)%>%
+              resample(.,ocean,method='ngb',filename='v2015/npp/npp_03_07.tif')
+
+  
 # gear proportions at 1 km
 
 gear_hb = raster('v2015/gear_prop_hb_moll_1km_ocean.tif')
 gear_lb = raster('v2015/gear_prop_lb_moll_1km_ocean.tif')
 
+#---------------------------------------------------------------------------
 
+# Catch standardized by primary productivity
+
+catch_npp_06_10 = overlay(catch_06_10,npp_06_10,fun=function(x,y){x/y},progress='text',filename='v2015/catch_npp_06_10.tif')
+catch_npp_05_09 = overlay(catch_05_09,npp_05_09,fun=function(x,y){x/y},progress='text',filename='v2015/catch_npp_05_09.tif')
+catch_npp_04_08 = overlay(catch_04_08,npp_04_08,fun=function(x,y){x/y},progress='text',filename='v2015/catch_npp_04_08.tif')
+catch_npp_03_07 = overlay(catch_03_07,npp_03_07,fun=function(x,y){x/y},progress='text',filename='v2015/catch_npp_03_07.tif')
 #----------------------------------------------------------------------------
 # Divide catch by primary productivity then multiply by gear proportions
 
