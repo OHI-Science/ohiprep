0c152373c578a9dadb02cfd2fa3f9f9f0a59ba52
Casey O'Hara <ohara@nceas.ucsb.edu>
2015-08-31 17:56:21 -0700

    adding mollweide downrezed shapefiles and a mol land shapefile

--- globalprep/spatial/downres_polygons.R
+++ globalprep/spatial/downres_polygons.R
@@ -15,8 +15,11 @@ source('src/R/common.R')
 library(maptools)
 library(shapefiles)
 
+#proj_type = 'gcs'
+proj_type = 'mol'
+
 dir_rgn  <- file.path(dir_neptune_data, 'git-annex/globalprep/spatial/v2015/data')
-poly_layer <- file.path(dir_rgn, 'regions_gcs')
+poly_layer <- file.path(dir_rgn, sprintf('regions_%s', proj_type)) 
 dir_git  <- file.path(getwd(), 'globalprep/spatial/downres')
    # writeOGR dsn needs to be an absolute path? apparently ~ causes issues.
 
@@ -38,7 +41,7 @@ rgn_poly <- readShapePoly(poly_layer)
     # [96]     1 18222 18222    16    15  1577  1576    71    70     2     1  3404  3403     8     7    19    18    14    13
     
 ### Exclude land polygons
-rgn_mar <- rgn_poly[!(rgn_poly@data$rgn_typ %in% c('land', 'land-disputed', 'land-noeez')), ]
+rgn_mar <- rgn_poly[(rgn_poly@data$rgn_typ %in% c('land', 'land-disputed', 'land-noeez')), ]
 rgn_mar_orig <- rgn_mar
 
     sapply(rgn_mar@polygons, function(x) length(x@Polygons))
@@ -60,13 +63,20 @@ area <- lapply(rgn_mar@polygons, function(x) sapply(x@Polygons, function(y) y@ar
     #       0%          25%          50%          75%         100% 
     # 8.307850e-11 5.946673e-06 1.224704e-05 3.964384e-05 2.662491e+03 
 
-    # min(unlist(lapply(area, function(x) (max(x)))))
-    # for (i in 1:length(area)) {
-    #   cat(sprintf('rgn %s: largest polygon %.5f (%s)\n', rgn_mar@data$rgn_id[i], max(area[[i]]), rgn_mar@data$rgn_nam[i]))
-    # } 
+### Determine min-max area - the max area within each region is presumably the main component of the EEZ;
+### so find the smallest EEZ.
+min(unlist(lapply(area, function(x) (max(x)))))
+for (i in 1:length(area)) {
+  cat(sprintf('rgn %s: largest polygon %.5f (%s)\n', rgn_mar@data$rgn_id[i], max(area[[i]]), rgn_mar@data$rgn_nam[i]))
+} 
     
-### ditch the small polys;  smallest EEZ region is Bosnia/Herzegovina at 0.00141 so threshold below that
-mainPolys <- lapply(area, function(x) which(x > 0.001))
+### ditch the small polys;  smallest EEZ region is Bosnia/Herzegovina #232 at 0.00141 so threshold below that.
+### (in Mollweide units (meters), 12791438 m^2)
+### Generalizing to do either gcs or mol projections, since different units, need different area threshold.
+area_threshold <- switch(proj_type,
+                       'gcs' = 0.001,
+                       'mol' = 1e5)
+mainPolys <- lapply(area, function(x) which(x > area_threshold))
 
 for(i in 1:length(mainPolys)){
   if(length(mainPolys[[i]]) >= 1 && mainPolys[[i]][1] >= 1){
@@ -85,9 +95,17 @@ table(rgn_mar@data$ant_typ)
 # eez    eez-ccamlr  eez-disputed    eez-inland           fao          land   land-ccamlr land-disputed    land-noeez 
 # 220            19            15             5            15             0             0             0             0 
 
-res_list = c(ultralow = 0.5, verylow = 0.25, low = 0.1)
+
+### The threshold for resolution in the dp() function:
+### Appears to be basically in same units of shapefile - the minimum deviation of a point between two
+### other points, to be saved rather than deleted in simplifying.
+res_list <- switch(proj_type,
+                   'gcs' = c(low = 0.1),
+                   'mol' = c(low = 10000))
+
+
 num_poly <- length(rgn_mar@polygons)
-for(k in 1:length(res_list)) { # res = c(verylow = 0.5)
+for(k in 1:length(res_list)) { # k = 1
   # res is the resolution for the dp() call
   rgn_mar_temp <- rgn_mar
   # set working rgn_mar_temp to original rgn_mar
@@ -104,36 +122,36 @@ for(k in 1:length(res_list)) { # res = c(verylow = 0.5)
     }
   }
   
-  area <- lapply(rgn_mar@polygons, function(x) sapply(x@Polygons, function(y) y@area))
+  area <- lapply(rgn_mar_temp@polygons, function(x) sapply(x@Polygons, function(y) y@area))
   print(quantile(unlist(area)))
   
   rgn_mar_temp@plotOrder <- 1:length(rgn_mar@polygons)
 
   rgn_eez   <- rgn_mar_temp[str_detect(rgn_mar_temp@data$rgn_typ, 'eez') & !str_detect(rgn_mar_temp@data$ant_typ, 'ccamlr'), ]
   rgn_eez@plotOrder <- 1:length(rgn_eez@polygons)
-  
   # includes eez-disputed and eez-inland, but no FAO or CCAMLR regions
+
   rgn_ant   <- rgn_mar_temp[rgn_mar_temp@data$ant_typ == 'eez-ccamlr', ]
   rgn_ant@plotOrder <- 1:length(rgn_ant@polygons)
   rgn_fao   <- rgn_mar_temp[rgn_mar_temp@data$rgn_typ == 'fao', ]
   rgn_fao@plotOrder <- 1:length(rgn_fao@polygons)
   
-  output_all <- sprintf('rgn_all_gcs_%s_res', names(res_list[k]))
+  output_all <- sprintf('rgn_land_%s_%s_res', proj_type, names(res_list[k]))
   cat(sprintf('Writing %s to %s  ', output_all, dir_git))
   writeOGR(rgn_mar_temp, dsn = dir_git, layer = output_all, driver = 'ESRI Shapefile', overwrite_layer = TRUE, morphToESRI = TRUE)
   cat(sprintf('Shapefile size: %.3f MB\n', 1e-6*(file.size(file.path(dir_git, paste(output_all, '.shp', sep = ''))))))
 
-  output_eez <- sprintf('rgn_eez_gcs_%s_res', names(res_list[k]))
+  output_eez <- sprintf('rgn_eez_%s_%s_res', proj_type, names(res_list[k]))
   cat(sprintf('Writing %s to %s  ', output_eez, dir_git))
   writeOGR(rgn_eez, dsn = dir_git, layer = output_eez, driver = 'ESRI Shapefile', overwrite_layer = TRUE, morphToESRI = TRUE)
   cat(sprintf('Shapefile size: %.3f MB\n', 1e-6*(file.size(file.path(dir_git, paste(output_eez, '.shp', sep = ''))))))
   
-  output_ant <- sprintf('rgn_ant_gcs_%s_res', names(res_list[k]))
+  output_ant <- sprintf('rgn_ant_%s_%s_res', proj_type, names(res_list[k]))
   cat(sprintf('Writing %s to %s  ', output_ant, dir_git))
   writeOGR(rgn_ant, dsn = dir_git, layer = output_ant, driver = 'ESRI Shapefile', overwrite_layer = TRUE, morphToESRI = TRUE)
   cat(sprintf('Shapefile size: %.3f MB\n', 1e-6*(file.size(file.path(dir_git, paste(output_ant, '.shp', sep = ''))))))
   
-  output_fao <- sprintf('rgn_fao_gcs_%s_res', names(res_list[k]))
+  output_fao <- sprintf('rgn_fao_%s_%s_res', proj_type, names(res_list[k]))
   cat(sprintf('Writing %s to %s  ', output_fao, dir_git))
   writeOGR(rgn_fao, dsn = dir_git, layer = output_fao, driver = 'ESRI Shapefile', overwrite_layer = TRUE, morphToESRI = TRUE)
   cat(sprintf('Shapefile size: %.3f MB\n', 1e-6*(file.size(file.path(dir_git, paste(output_fao, '.shp', sep = ''))))))
@@ -159,7 +177,7 @@ res_list2 <- c('low_res', 'verylow_res', 'ultralow_res')
 type_list <- c('all', 'eez', 'fao', 'ant')
 for (i in res_list2) { # i = 'low_res'
   for (j in type_list) {
-    fn <- sprintf('rgn_%s_gcs_%s', j, i)
+    fn <- sprintf('rgn_%s_%s_%s', j, proj_type, i)
     poly_layer <- file.path(dir_git, fn)
     cat(sprintf('Reading %s file: \n  %s\n', fn, poly_layer))
     ptm <- proc.time()
