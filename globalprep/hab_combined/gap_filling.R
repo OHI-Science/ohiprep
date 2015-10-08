237f0890b5a8faa142e446a9153a4a6899d053d9
Melsteroni <frazier@nceas.ucsb.edu>
2015-10-08 12:58:48 -0700

    Habitat gapfilling

--- globalprep/hab_combined/gap_filling.R
+++ globalprep/hab_combined/gap_filling.R
@@ -6,8 +6,51 @@ library(tidyr)
 
 # mangrove gap-filling is done here: ohiprep/globalprep/hab_mangrove/v2015/MangroveFinalDataFormatting.R
 # not sure where rocky reef is used.....don't have gap-filling data
-
 ## Bottom of script combines the habitat-gapfilling so it corresponds to the habitat layer
+
+
+### General gap-fill function
+gap_fill_function <- function(var, no_gap_fill, data){
+  
+  if(var=="health"){
+    health <- data %>%
+      mutate(variable = var) %>%
+      mutate(condition = ifelse(condition %in% no_gap_fill, 0, condition)) %>%    # if actuals, it is a zero    
+      mutate(gap_fill = paste(condition, gap_fill_1, sep=' and ')) %>%
+      mutate(gap_fill = gsub(' and NA', '', gap_fill)) %>%         # clean up
+      mutate(gap_fill = ifelse(condition %in% 0 & gap_fill_1 %in% "disagg2012_gap_fill", gap_fill_1, gap_fill)) %>%    # original based on estimate but gap-filled during disagregation
+      mutate(gap_fill = ifelse(is.na(km2) | km2==0, NA, gap_fill)) %>%      # get rid of any values without extents
+      mutate(gap_fill = ifelse(!is.na(km2) & km2>0 & is.na(health), 'mean of other habitats', gap_fill)) %>%  # when there is an area, but no health, this ends up being dropped from the calculation
+      mutate(gap_fill = ifelse(is.na(condition) & !is.na(health), "assume gap-fill", gap_fill)) # when there is a health value, but no information on gap-filling
+      #select(rgn_id, habitat, variable, gap_fill)
+    return(health)
+  }
+  
+  if(var=="extent"){
+    extent <- tmp %>%
+      select(rgn_id, habitat, km2) %>%
+      mutate(variable = var) %>%
+      mutate(gap_fill = ifelse(km2>0, 0, NA)) %>%   # all actual data, so anything with a value is 0, others are NA
+      mutate(gap_fill = ifelse(rgn_id %in% d_regions$rgn_id & !is.na(km2), "disagg2012_gap_fill", gap_fill))
+      #select(rgn_id, habitat, variable, gap_fill)
+    return(extent)
+  }
+  
+  if(var=="trend")  {
+    trend <- tmp %>%
+      mutate(variable = var) %>%
+      mutate(trend_gaps = ifelse(trend_gaps == no_gap_fill, 0, trend_gaps)) %>%  # convert non-gapfills to zero
+      mutate(gap_fill = paste(trend_gaps, gap_fill_1, sep=' and ')) %>%
+      mutate(gap_fill = gsub(' and NA', '', gap_fill)) %>%         # clean up
+      mutate(gap_fill = ifelse(trend_gaps %in% 0 & gap_fill_1 %in% "disagg2012_gap_fill", gap_fill_1, gap_fill)) %>%    # original based on estimate but gap-filled during disagregation
+      mutate(gap_fill = ifelse(is.na(km2) | km2 %in% 0, NA, gap_fill)) %>%
+      mutate(gap_fill = ifelse(km2>0 & is.na(trend), 'mean of other habitats', gap_fill)) %>%    # na.rm in functions.R
+      mutate(gap_fill = ifelse(is.na(trend_gaps) & !is.na(trend) , "assume gap-fill", gap_fill))
+      #select(rgn_id, habitat, variable, gap_fill)
+    return(trend)
+  }
+}
+
 
 ## regions
 regions <- read.csv('../ohiprep/src/LookupTables/eez_rgn_2013master.csv') %>%
@@ -44,6 +87,12 @@ whence <- read.csv('../ohiprep/src/LookupTables/eez_rgn_2013master.csv') %>%
 ############################################################################
 table(whence$soft_bottom)
 
+sb_whence <- whence %>%
+  select(rgn_id, region_id_2012, metric, soft_bottom) %>%
+  spread(metric, soft_bottom) %>%
+  select(rgn_id, region_id_2012, extent, condition, trend_gaps=trend) %>%
+  left_join(d_regions)
+
 sb_extent <- read.csv('globalprep/hab_soft_bottom/v2012/data/habitat_extent_softbottom.csv')
 summary(sb_extent)
 sb_health <- read.csv('globalprep/hab_soft_bottom/v2012/data/habitat_health_softbottom.csv')
@@ -51,43 +100,32 @@ summary(sb_health)
 sb_trend <- read.csv('globalprep/hab_soft_bottom/v2012/data/habitat_trend_softbottom.csv')
 summary(sb_trend)
 
+
 # step 1: see if there was gap-filling due to disaggregation:
-left_join(d_regions, sb_extent) #I think there was due to disaggration, values aren't the same because it was scaled to eez area
-left_join(d_regions, sb_health) #yes
+left_join(d_regions, sb_extent) %>%
+  arrange(rgn_id) #I believe so, scaled by eez area
+left_join(d_regions, sb_health) %>%#yes
+  arrange(region_id_2012)
 left_join(d_regions, sb_trend) #yes
 
-tmp <- left_join(regions, sb_extent) %>%
+
+tmp <- left_join(sb_whence, sb_extent) %>%
   left_join(sb_health) %>%
   left_join(sb_trend) %>%
-  mutate(habitat="soft_bottom") %>%
-  ## for health/trend all values are 0, except those with values and in the disagg list 
-  ## also add contingency when no extent, but health/trend values, or
-  ## when there is extent but no health/trend values 
-  mutate(health = ifelse(!is.na(health), 0, health)) %>%
-  mutate(health = ifelse(rgn_id %in% d_regions$rgn_id & !is.na(health), "disagg2012_gap_fill", health)) %>%
-  mutate(health = ifelse(km2==0 | is.na(km2), NA, health)) %>% 
-  mutate(health = ifelse(km2>0 & is.na(health), "mean of other habitats", health)) %>% 
-  mutate(trend = ifelse(!is.na(trend), 0, trend)) %>%
-  mutate(trend = ifelse(rgn_id %in% d_regions$rgn_id & !is.na(trend), "disagg2012_gap_fill", trend)) %>%
-  mutate(trend = ifelse(km2==0 | is.na(km2), NA, trend)) %>% 
-  mutate(trend = ifelse(km2>0 & is.na(trend), "mean of other habitats", trend)) %>%
-  mutate(extent_gap_fill = ifelse(km2>0, 0, NA)) %>%
-  mutate(extent_gap_fill = ifelse(rgn_id %in% d_regions$rgn_id & !is.na(km2), "disagg2012_gap_fill", extent_gap_fill)) 
+  mutate(habitat="soft_bottom")
 
-extent <- tmp %>%
-  mutate(variable = "extent") %>%
-  select(rgn_id, habitat, variable, gap_fill = extent_gap_fill)
-write.csv(extent, 'globalprep/hab_soft_bottom/v2012/data/extent_gap_fill.csv', row.names=FALSE)
 
-health <- tmp %>%
-  mutate(variable = 'health') %>%
-  select(rgn_id, habitat, variable, gap_fill=health)
-write.csv(health, 'globalprep/hab_soft_bottom/v2012/data/health_gap_fill.csv', row.names=FALSE)
+sb_extent <- gap_fill_function(var="extent", no_gap_fill = "actuals", data=tmp) %>%
+  select(rgn_id, habitat, variable, gap_fill)
+write.csv(sb_extent, 'globalprep/hab_soft_bottom/v2012/data/extent_gap_fill.csv', row.names=FALSE)
 
-trend <- tmp %>%
-  mutate(variable = 'trend') %>%
-  select(rgn_id, habitat, variable, gap_fill=trend)
-write.csv(health, 'globalprep/hab_soft_bottom/v2012/data/trend_gap_fill.csv', row.names=FALSE)
+sb_health <- gap_fill_function(var="health", no_gap_fill = "actuals", data=tmp) %>%
+  select(rgn_id, habitat, variable, gap_fill)
+write.csv(sb_health, 'globalprep/hab_soft_bottom/v2012/data/health_gap_fill.csv', row.names=FALSE)
+
+sb_trend <- gap_fill_function(var="trend", no_gap_fill = "actuals", data=tmp) %>%
+  select(rgn_id, habitat, variable, gap_fill)
+write.csv(sb_health, 'globalprep/hab_soft_bottom/v2012/data/trend_gap_fill.csv', row.names=FALSE)
 
 ############################################################################
 ##corals ----
@@ -123,39 +161,17 @@ tmp <- left_join(c_whence, coral_extent) %>%
   left_join(coral_trend) %>%
   mutate(habitat="coral")
 
-extent <- tmp %>%
-  select(rgn_id, habitat, km2) %>%
-  mutate(variable = "extent") %>%
-  mutate(gap_fill = ifelse(km2>0, 0, NA)) %>%   # all actual data, so anything with a value is 0, others are NA
-  mutate(gap_fill = ifelse(rgn_id %in% d_regions$rgn_id & !is.na(km2), "disagg2012_gap_fill", gap_fill)) %>%
+c_extent <- gap_fill_function(var="extent", no_gap_fill = "actuals", data=tmp) %>%
   select(rgn_id, habitat, variable, gap_fill)
-write.csv(extent, 'globalprep/hab_coral/v2012/data/extent_gap_fill_coral.csv', row.names=FALSE)
+write.csv(c_extent, 'globalprep/hab_coral/v2012/data/extent_gap_fill_coral.csv', row.names=FALSE)
 
-### NOTE: I think prediction might be considered as non-gapfilled, otherwise everything is gap-filled!
-health <- tmp %>%
-  mutate(variable = "health") %>%
-  mutate(condition = ifelse(condition %in% c('actuals', 'prediction'), 0, condition)) %>%    # if actuals, it is a zero    
-  mutate(gap_fill = paste(condition, gap_fill_1, sep=' and ')) %>%
-  mutate(gap_fill = gsub(' and NA', '', gap_fill)) %>%         # clean up
-  mutate(gap_fill = ifelse(condition %in% 0 & gap_fill_1 %in% "disagg2012_gap_fill", gap_fill_1, gap_fill)) %>%    # original based on estimate but gap-filled during disagregation
-  mutate(gap_fill = ifelse(is.na(km2) | km2==0, NA, gap_fill)) %>%      # get rid of any values without extents
-  mutate(gap_fill = ifelse(!is.na(km2) & km2>0 & is.na(health), 'mean of other habitats', gap_fill)) %>%  # when there is an area, but no health, this ends up being dropped from the calculation
-  mutate(gap_fill = ifelse(is.na(condition) & !is.na(health), "assume gap-fill", gap_fill)) %>% # when there is a health value, but no information on gap-filling
+c_health <- gap_fill_function(var="health", no_gap_fill = c("actuals", "prediction"), data=tmp) %>%
   select(rgn_id, habitat, variable, gap_fill)
-write.csv(health, 'globalprep/hab_coral/v2012/data/health_gap_fill_coral.csv', row.names=FALSE)
+write.csv(c_health, 'globalprep/hab_coral/v2012/data/health_gap_fill_coral.csv', row.names=FALSE)
 
-table(tmp$trend_gaps)
-trend <- tmp %>%
-  mutate(variable = "trend") %>%
-  mutate(trend_gaps = ifelse(trend_gaps == 'actuals', 0, trend_gaps)) %>%  # convert non-gapfills to zero
-  mutate(gap_fill = paste(trend_gaps, gap_fill_1, sep=' and ')) %>%
-  mutate(gap_fill = gsub(' and NA', '', gap_fill)) %>%         # clean up
-  mutate(gap_fill = ifelse(trend_gaps %in% 0 & gap_fill_1 %in% "disagg2012_gap_fill", gap_fill_1, gap_fill)) %>%    # original based on estimate but gap-filled during disagregation
-  mutate(gap_fill = ifelse(is.na(km2) | km2 %in% 0, NA, gap_fill)) %>%
-  mutate(gap_fill = ifelse(km2>0 & is.na(trend), 'mean of other habitats', gap_fill)) %>%    # na.rm in functions.R
-  mutate(gap_fill = ifelse(is.na(trend_gaps) & !is.na(trend) , "assume gap-fill", gap_fill)) %>%
+c_trend <- gap_fill_function(var="trend", no_gap_fill = c("actuals"), data=tmp) %>%
   select(rgn_id, habitat, variable, gap_fill)
-write.csv(trend, 'globalprep/hab_coral/v2012/data/trend_gap_fill_coral.csv', row.names=FALSE)
+write.csv(c_trend, 'globalprep/hab_coral/v2012/data/trend_gap_fill_coral.csv', row.names=FALSE)
 
 
 ############################################################################
@@ -191,42 +207,18 @@ tmp <- left_join(sm_whence, sm_extent) %>%
   left_join(sm_trend) %>%
   mutate(habitat="salt_marsh")
 
-extent <- tmp %>%
-  mutate(variable = "extent") %>%
-  mutate(extent = ifelse(extent %in% 'actuals', 0, extent)) %>%    # if actuals, it is a zero    
-  mutate(gap_fill = paste(extent, gap_fill_1, sep=' and ')) %>%
-  mutate(gap_fill = gsub(' and NA', '', gap_fill)) %>%         # clean up
-  mutate(gap_fill = ifelse(extent ==0 & gap_fill_1 %in% "disagg2012_gap_fill", gap_fill_1, gap_fill)) %>%    # original based on estimate but gap-filled during disagregation
-  mutate(gap_fill = ifelse(is.na(km2) | km2==0, NA, gap_fill)) %>%      # get rid of any values without extents
+
+sm_extent <- gap_fill_function(var="extent", no_gap_fill = "actuals", data=tmp) %>%
   select(rgn_id, habitat, variable, gap_fill)
-write.csv(extent, 'globalprep/hab_saltmarsh/v2012/data/extent_gap_fill_saltmarsh.csv', row.names=FALSE)
+write.csv(sm_extent, 'globalprep/hab_saltmarsh/v2012/data/extent_gap_fill_saltmarsh.csv', row.names=FALSE)
 
-
-health <- tmp %>%
-  mutate(variable = "health") %>%
-  mutate(condition = ifelse(condition %in% 'actuals', 0, condition)) %>%    # if actuals, it is a zero for gap-filling    
-  mutate(gap_fill = paste(condition, gap_fill_1, sep=' and ')) %>%
-  mutate(gap_fill = gsub(' and NA', '', gap_fill)) %>%         # clean up
-  mutate(gap_fill = ifelse(condition %in% 0 & gap_fill_1 %in% "disagg2012_gap_fill", gap_fill_1, gap_fill)) %>%    # original based on estimate but gap-filled during disagregation
-  mutate(gap_fill = ifelse(is.na(km2) | km2==0, NA, gap_fill)) %>%      # get rid of any values without extents
-  mutate(gap_fill = ifelse(!is.na(km2) & km2>0 & is.na(health), 'mean of other habitats', gap_fill)) %>%
-  mutate(gap_fill = ifelse(is.na(condition) & !is.na(health), "assume gap-fill", gap_fill)) %>%
+sm_health <- gap_fill_function(var="health", no_gap_fill = "actuals", data=tmp) %>%
   select(rgn_id, habitat, variable, gap_fill)
-write.csv(health, 'globalprep/hab_saltmarsh/v2012/data/health_gap_fill_saltmarsh.csv', row.names=FALSE)
+write.csv(sm_health, 'globalprep/hab_saltmarsh/v2012/data/health_gap_fill_saltmarsh.csv', row.names=FALSE)
 
-table(tmp$trend_gaps)
-trend <- tmp %>%
-  mutate(variable = "trend") %>%
-  mutate(trend_gaps = ifelse(trend_gaps == 'actuals', 0, trend_gaps)) %>%  # convert non-gapfills to zero
-  mutate(gap_fill = paste(trend_gaps, gap_fill_1, sep=' and ')) %>%
-  mutate(gap_fill = gsub(' and NA', '', gap_fill)) %>%         # clean up
-  mutate(gap_fill = ifelse(trend_gaps %in% 0 & gap_fill_1 %in% "disagg2012_gap_fill", gap_fill_1, gap_fill)) %>%    # original based on estimate but gap-filled during disagregation
-  mutate(gap_fill = ifelse(is.na(km2) | km2 %in% 0, NA, gap_fill)) %>%
-  mutate(gap_fill = ifelse(km2>0 & is.na(gap_fill), 'mean of other habitats', gap_fill)) %>%    # na.rm in functions.R
-  mutate(gap_fill = ifelse(is.na(trend_gaps) & !is.na(trend) , "assume gap-fill", gap_fill)) %>%
+sm_trend <- gap_fill_function(var="trend", no_gap_fill = c("actuals"), data=tmp) %>%
   select(rgn_id, habitat, variable, gap_fill)
-write.csv(trend, 'globalprep/hab_saltmarsh/v2012/data/trend_gap_fill_saltmarsh.csv', row.names=FALSE)
-
+write.csv(sm_trend, 'globalprep/hab_saltmarsh/v2012/data/trend_gap_fill_saltmarsh.csv', row.names=FALSE)
 
 ############################################################################
 ##sea grass ----
@@ -261,40 +253,19 @@ tmp <- left_join(sg_whence, sg_extent) %>%
   left_join(sg_trend) %>%
   mutate(habitat="seagrasses")
 
-extent <- tmp %>%
-  mutate(variable = "extent") %>%
-  mutate(extent = ifelse(extent %in% 'actuals', 0, extent)) %>%    # if actuals, it is a zero    
-  mutate(gap_fill = paste(extent, gap_fill_1, sep=' and ')) %>%
-  mutate(gap_fill = gsub(' and NA', '', gap_fill)) %>%         # clean up
-  mutate(gap_fill = ifelse(extent ==0 & gap_fill_1 %in% "disagg2012_gap_fill", gap_fill_1, gap_fill)) %>%    # original based on estimate but gap-filled during disagregation
-  mutate(gap_fill = ifelse(is.na(km2) | km2==0, NA, gap_fill)) %>%      # get rid of any values without extents
-  select(rgn_id, habitat, variable, gap_fill)
-write.csv(extent, 'globalprep/hab_seagrass/v2012/data/extent_gap_fill_seagrass.csv', row.names=FALSE)
 
-# counting prediction and actuals-mixed as non-gapfilled...should check on this.
-health <- tmp %>%
-  mutate(variable = "health") %>%
-  mutate(condition = ifelse(condition %in% c('actuals-mixed', 'prediction'), 0, condition)) %>%    # if actuals, it is a zero    
-  mutate(gap_fill = paste(condition, gap_fill_1, sep=' and ')) %>%
-  mutate(gap_fill = gsub(' and NA', '', gap_fill)) %>%         # clean up
-  mutate(gap_fill = ifelse(condition %in% 0 & gap_fill_1 %in% "disagg2012_gap_fill", gap_fill_1, gap_fill)) %>%    # original based on estimate but gap-filled during disagregation
-  mutate(gap_fill = ifelse(is.na(km2) | km2==0, NA, gap_fill)) %>%      # get rid of any values without extents
-  mutate(gap_fill = ifelse(!is.na(km2) & km2>0 & is.na(health), 'mean of other habitats', gap_fill)) %>%
-  mutate(gap_fill = ifelse(is.na(condition) & !is.na(health), "assume gap-fill", gap_fill)) %>%
+sg_extent <- gap_fill_function(var="extent", no_gap_fill = "actuals", data=tmp) %>%
   select(rgn_id, habitat, variable, gap_fill)
-write.csv(health, 'globalprep/hab_seagrass/v2012/data/health_gap_fill_seagrass.csv', row.names=FALSE)
+write.csv(sg_extent, 'globalprep/hab_seagrass/v2012/data/extent_gap_fill_seagrass.csv', row.names=FALSE)
 
-table(tmp$trend_gaps)
-trend <- tmp %>%
-  mutate(variable = "trend") %>%
-  mutate(trend_gaps = ifelse(trend_gaps == 'actuals', 0, trend_gaps)) %>%  # convert non-gapfills to zero
-  mutate(gap_fill = paste(trend_gaps, gap_fill_1, sep=' and ')) %>%
-  mutate(gap_fill = ifelse(trend_gaps %in% 0 & gap_fill_1 %in% "disagg2012_gap_fill", gap_fill_1, gap_fill)) %>%    # original based on estimate but gap-filled during disagregation
-  mutate(gap_fill = ifelse(is.na(km2) | km2 %in% 0, NA, gap_fill)) %>%
-  mutate(gap_fill = ifelse(km2>0 & is.na(gap_fill), 'mean of other habitats', gap_fill)) %>%    # na.rm in functions.R
-  mutate(gap_fill = ifelse(is.na(trend_gaps) & !is.na(trend) , "assume gap-fill", gap_fill)) %>%
+sg_health <- gap_fill_function(var="health", no_gap_fill = c('actuals-mixed', 'prediction'), data=tmp) %>%
   select(rgn_id, habitat, variable, gap_fill)
-write.csv(trend, 'globalprep/hab_seagrass/v2012/data/trend_gap_fill_seagrass.csv', row.names=FALSE)
+write.csv(sg_health, 'globalprep/hab_seagrass/v2012/data/health_gap_fill_seagrass.csv', row.names=FALSE)
+
+sg_trend <- gap_fill_function(var="trend", no_gap_fill = c("actuals"), data=tmp) %>%
+  select(rgn_id, habitat, variable, gap_fill)
+write.csv(sg_trend, 'globalprep/hab_seagrass/v2012/data/trend_gap_fill_seagrass.csv', row.names=FALSE)
+
 
 ############################################################################
 ## sea ice ----
