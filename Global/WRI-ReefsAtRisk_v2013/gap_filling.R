25f301002a98c5f38c5c17249e1a336e036d8ce2
Melsteroni <frazier@nceas.ucsb.edu>
2015-11-12 16:22:00 -0800

    gapfilling: fixing a mistake in blast/poison NP data

--- Global/WRI-ReefsAtRisk_v2013/gap_filling.R
+++ Global/WRI-ReefsAtRisk_v2013/gap_filling.R
@@ -1,6 +1,8 @@
 ## create gap-filling dataset for cyanide and blast data
 ## no gap-filling for these data (so should be easy!)
 
+library(dplyr)
+
 cyanide <- read.csv("Global/WRI-ReefsAtRisk_v2013/data/gl_thr_poison_3nm_rgn2013.csv") %>%
   mutate(gap_fill = ifelse(!is.na(score) & score > 0, 0, NA)) %>%
   select(rgn_id, gap_fill)
@@ -12,4 +14,4 @@ blast <- read.csv("Global/WRI-ReefsAtRisk_v2013/data/gl_thr_blast_3nm_rgn2013.cs
   mutate(gap_fill = ifelse(!is.na(score) & score > 0, 0, NA)) %>%
   select(rgn_id, gap_fill)
 
-write.csv(cyanide, "Global/WRI-ReefsAtRisk_v2013/data/gl_thr_blast_3nm_rgn2013_gapfill.csv", row.names=FALSE)
+write.csv(blast, "Global/WRI-ReefsAtRisk_v2013/data/gl_thr_blast_3nm_rgn2013_gapfill.csv", row.names=FALSE)
