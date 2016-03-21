5a795414659426b91ce2ce50548584d8a19e8670
Melsteroni <frazier@nceas.ucsb.edu>
2016-03-21 14:48:29 -0700

    Adding MPA data for global OHI2016 resilience

--- globalprep/WDPA_MPA/v2015/mpa_resilience.R
+++ globalprep/WDPA_MPA/v2015/mpa_resilience.R
@@ -10,7 +10,7 @@ library(dplyr)
 # reference points = 30% of area
 ref_pct_cmpa=30 
 ref_pct_cp=30
-
+scenario_year=2015
 
 ##########################
 #### 3nm region
@@ -20,7 +20,7 @@ lsp_3nm <- read.csv('globalprep/WDPA_MPA/v2015/data/lsp_protarea_offshore3nm.csv
 area_3nm <- read.csv('../ohi-global/eez2013/layers/rgn_area_offshore3nm.csv') %>%
   select(rgn_id, area_km2_3nm = area_km2)
 
-status_year = 2014
+status_years = max(lsp_3nm$year)
 
 # fill in time series for all regions and generate cumulative sum
 r.yrs <- expand.grid(rgn_id = unique(lsp_3nm$rgn_id),
@@ -30,8 +30,7 @@ r.yrs <- expand.grid(rgn_id = unique(lsp_3nm$rgn_id),
   mutate(area_km2 = ifelse(is.na(area_km2), 0, area_km2)) %>%
   group_by(rgn_id) %>%
   mutate(cmpa_cumsum  = cumsum(area_km2)) %>%
-  filter(year == status_year) %>%
-  select(rgn_id, cmpa_cumsum) %>%
+  select(rgn_id, year, cmpa_cumsum) %>%
   ungroup() 
 
 # get percent of total area that is protected 
@@ -40,9 +39,21 @@ r.yrs = r.yrs %>%
   mutate(pct_cmpa  = pmin(cmpa_cumsum / area_km2_3nm * 100, 100),
          prop_protected    = ( pmin(pct_cmpa / ref_pct_cmpa, 1))) %>%
   filter(!is.na(prop_protected)) %>%
-  select(rgn_id, pressure.score = prop_protected)
+  select(rgn_id, year, pressure.score = prop_protected)
 
-write.csv(r.yrs, 'globalprep/WDPA_MPA/v2015/data/resilience_lsp_score_offshore3nm.csv')
+for(time in 0:3){  #time = 0
+scen_year <- scenario_year - time
+stat_year <- status_year - time 
+  
+tmp <- r.yrs %>%
+  filter(year == stat_year) %>%
+  select(rgn_id, pressure.score) %>%
+  data.frame()
+
+write.csv(tmp, sprintf('globalprep/WDPA_MPA/v2015/data/resilience_lsp_score_offshore3nm_%s.csv',
+          scen_year), row.names=FALSE)
+}  
+  
 
 
 ##########################
@@ -53,7 +64,7 @@ lsp_eez <- read.csv('globalprep/WDPA_MPA/v2014_archive/data/lsp_protarea_eez.csv
 area_eez <- read.csv('../ohi-global/eez2013/layers/rgn_area.csv') %>%
   select(rgn_id, area_km2_eez = area_km2)
 
-status_year = 2013
+status_years = max(lsp_eez$year)
 
 # fill in time series for all regions and generate cumulative sum
 r.yrs <- expand.grid(rgn_id = unique(lsp_eez$rgn_id),
@@ -63,8 +74,7 @@ r.yrs <- expand.grid(rgn_id = unique(lsp_eez$rgn_id),
   mutate(area_km2 = ifelse(is.na(area_km2), 0, area_km2)) %>%
   group_by(rgn_id) %>%
   mutate(cmpa_cumsum  = cumsum(area_km2)) %>%
-  filter(year == status_year) %>%
-  select(rgn_id, cmpa_cumsum) %>%
+  select(rgn_id, year, cmpa_cumsum) %>%
   ungroup() 
 
 # get percent of total area that is protected 
@@ -73,6 +83,18 @@ r.yrs = r.yrs %>%
   mutate(pct_cmpa  = pmin(cmpa_cumsum / area_km2_eez * 100, 100),
          prop_protected    = ( pmin(pct_cmpa / ref_pct_cmpa, 1))) %>%
   filter(!is.na(prop_protected)) %>%
-  select(rgn_id, pressure.score = prop_protected)
+  select(rgn_id, year, pressure.score = prop_protected)
 
-write.csv(r.yrs, 'globalprep/WDPA_MPA/v2015/data/resilience_lsp_score_eez.csv')
+for(time in c(1,2,3)){  #time = 0
+  scen_year <- scenario_year - time
+  stat_year <- status_year - time 
+  
+  tmp <- r.yrs %>%
+    filter(year == stat_year) %>%
+    select(rgn_id, pressure.score) %>%
+    data.frame()
+  
+  write.csv(tmp, sprintf('globalprep/WDPA_MPA/v2015/data/resilience_lsp_score_eez_%s.csv',
+                         scen_year), row.names=FALSE)
+}  
+
