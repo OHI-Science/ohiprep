ff0bdf49761418f69c0a0108924918c969bc7ec8
= <=>
2016-04-15 20:32:49 +0000

    updated common.R for Mazu directories

--- src/R/common.R
+++ src/R/common.R
@@ -1,22 +1,33 @@
 
-# set the neptune data_edit share based on operating system
-dir_neptune_data = c('Windows' = '//neptune.nceas.ucsb.edu/data_edit',
-                     'Darwin'  = '/Volumes/data_edit',
-                     'Linux'   = '/var/data/ohi')[[ Sys.info()[['sysname']] ]]
+# set the mazu and neptune data_edit share based on operating system
+dir_M             <- c('Windows' = '//mazu.nceas.ucsb.edu/ohi', ### someone on Windows - fix this?
+                       'Darwin'  = '/Volumes/ohi',    ### connect (cmd-K) to smb://mazu/ohi
+                       'Linux'   = '/home/shares/ohi')[[ Sys.info()[['sysname']] ]]
 
-dir_neptune_local = c('Windows' = '//neptune.nceas.ucsb.edu/local_edit',
-                      'Darwin'  = '/Volumes/local_edit',
-                      'Linux'   = '/usr/local/ohi')[[ Sys.info()[['sysname']] ]]
+dir_neptune_data  <- c('Windows' = '//neptune.nceas.ucsb.edu/data_edit',
+                       'Darwin'  = '/Volumes/data_edit',
+                       'Linux'   = '/var/data/ohi')[[ Sys.info()[['sysname']] ]]
 
-dir_halpern2008 = c('Windows' = '//neptune.nceas.ucsb.edu/halpern2008_edit',
-                    'Darwin'  = '/Volumes/halpern2008_edit',
-                    'Linux'   = '/var/cache/halpern-et-al')[[ Sys.info()[['sysname']] ]]
+dir_neptune_local <- c('Windows' = '//neptune.nceas.ucsb.edu/local_edit',
+                       'Darwin'  = '/Volumes/local_edit',
+                       'Linux'   = '/usr/local/ohi')[[ Sys.info()[['sysname']] ]]
 
-# stop if directory doesn't exist
-if (!file.exists(sprintf('%s/',dir_neptune_data))){
-  stop(sprintf("The directory for variable dir_neptune_data set in src/R/common.R does not exist. Do you need to mount %s?", dir_neptune_data))
-  
+dir_halpern2008   <- c('Windows' = '//neptune.nceas.ucsb.edu/halpern2008_edit',
+                       'Darwin'  = '/Volumes/halpern2008_edit',
+                       'Linux'   = '/var/cache/halpern-et-al')[[ Sys.info()[['sysname']] ]]
+
+# warning if Neptune or Mazu directory doesn't exist
+if (Sys.info()[['sysname']] != 'Linux' & !file.exists(sprintf('%s/', dir_neptune_data))){
+  warning(sprintf("The Neptune directory dir_neptune_data set in src/R/common.R does not exist. Do you need to mount Neptune: %s?", dir_neptune_data))
 }
+if (Sys.info()[['sysname']] != 'Linux' & !file.exists(dir_M)){
+  warning(sprintf("The Mazu directory dir_M set in src/R/common.R does not exist. Do you need to mount Mazu: %s?", dir_M))
+}
+if (Sys.info()[['sysname']] == 'Linux' & (!file.exists(dir_M) & !file.exists(dir_neptune_data))){
+  stop(sprintf("Neither Mazu dir_M nor Neptune dir_neptune_data variables (set in src/R/common.R) exist. \n  Do you need to mount Mazu: %s or Neptune: %s?", dir_M, dir_neptune_data))
+}
+
+
 
 # install (if necessary) and load commonly used libraries
 packages <- c("dplyr", "tidyr", "stringr")
