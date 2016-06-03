4034ab3063066edf4033fd71ad95c101c3fb1e24
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2016-06-03 08:44:14 -0700

    fixed high seas issue to get catch from high seas areas with SAUP R package

--- globalprep/fis/v2016/getSAUPdata.R
+++ globalprep/fis/v2016/getSAUPdata.R
@@ -19,7 +19,11 @@ ctrys <- list(rgns$num)
 
 hs <- listregions("highseas")%>%
         rename(FAOarea = title)%>%
-          mutate(num = rownames(.))
+          mutate(num = rownames(.))%>%
+          filter(num>18) #take out Arctic FAO area since SAUP reports no catch there
+
+h <- list(h$num)
+
 
 #Since you can only get catch data from one country at a time, I've written a function to collate them all into the format we want
 
@@ -31,12 +35,20 @@ saup_catchdata <- function(rgn, ctry){
  #works fine.... you can try this by setting ctry = 16 (American Samoa) and you'll find 2 Snapper columns... which is
  tons$year = rownames(tons)
  
+ if(rgn == 'eez'){
+ 
  out <- tons%>%
             melt(variable.name = 'species',
                  value.name = 'tons')%>%
             mutate(rgn_num = ctry,
                    rgn_name = rgns$EEZ[match(ctry,rgns$num)])
-        
+ }else{
+   out<-tons%>%
+          melt(variable.name = 'species',
+               value.name = 'tons')%>%
+            mutate(rgn_num = ctry+1000,
+                   rgn_name = hs$FAOarea[match(ctry,hs$num)])
+ }
  return(out)  
  
 }
@@ -48,6 +60,12 @@ d <- do.call(rbind.data.frame, all)
 write.csv(d,file=file.path(dir_M,'git-annex/globalprep/fis/raw/SAUP_catch_taxon_tons_eezs.csv'))
 
 #High Seas is not working...
-hs <- mclapply(list(hs$num)[[1]],saup_catchdata,rgn='highseas')
+
+#don't include area 18 since there are no catches
+hs_catch <- lapply(c(48,34,27,21,47,41,31,58,57,51,88,77,67,61,87,81,71),saup_catchdata,rgn='highseas')
+
+f <- do.call(rbind.data.frame,hs_catch)
+
+write.csv(f,file=file.path(dir_M,'git-annex/globalprep/fis/raw/SAUP_catch_taxon_tons_highseas.csv'))
 
 
