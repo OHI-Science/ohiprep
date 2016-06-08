0f3f0705d977b0812433f6f2619b943d2f8c6488
Casey O'Hara <ohara@nceas.ucsb.edu>
2016-06-07 17:16:00 -0700

    why do these things not delete already?

--- globalprep/WDPA_MPA/data_prep.r
+++
@@ -1,6 +0,0 @@
-# sequence:
-#   use model.py to rasterize WDPA polygons and output .dbfs
-#     use python just to generate rasters, and move raster analysis over to R script?
-#     call python script from top-level R script using rPython?
-#       if so, can we pass variables to it, e.g. file locations for a new analysis?
-#   use digest.R to crunch .dbfs into total areas per region per year
