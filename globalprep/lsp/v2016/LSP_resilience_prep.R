4de252da313d5ea8c458a62b2cf917aa27724cbf
Melsteroni <frazier@nceas.ucsb.edu>
2016-07-13 12:37:24 -0700

    OHI 2016: LSP resilience

--- globalprep/lsp/v2016/LSP_resilience_prep.R
+++ globalprep/lsp/v2016/LSP_resilience_prep.R
@@ -36,3 +36,38 @@ area_offshore <- read.csv("globalprep/lsp/v2016/output/lsp_a_total_offshore3nm.c
 
 write.csv(area_offshore, "globalprep/lsp/v2016/output/rgn_area_offshore3nm.csv", row.names=FALSE)
   
+
+### eez resilience 
+
+## first need to calculate status
+
+# select data ----
+r = read.csv("globalprep/lsp/v2016/output/lsp_a_total_eez.csv") %>% #total eez area
+  dplyr::select(rgn_id, a_tot_eez) %>%
+  unique() %>%
+  filter(rgn_id <= 250)
+
+
+ry = read.csv("globalprep/lsp/v2016/output/lsp_protected_eez.csv") %>% #total cumulative protected areas                      
+ dplyr::select(rgn_id, year, mpa=a_prot_eez) %>%
+  filter(rgn_id <= 250)
+
+r.yrs <- expand.grid(rgn_id = unique(ry$rgn_id),
+                     year = unique(ry$year)) %>%
+  left_join(ry, by=c('rgn_id', 'year')) %>%
+  arrange(rgn_id, year) %>%
+  mutate(mpa = ifelse(is.na(mpa), 0, mpa)) 
+
+# get percent of total area that is protected for inland1km (cp) and offshore3nm (cmpa) per year
+# and calculate status score
+r.yrs = r.yrs %>%
+  full_join(r, by="rgn_id") %>%
+  mutate(pct_mpa    = pmin(mpa   / a_tot_eez   * 100, 100),
+         prop_protected    = ( pmin(pct_mpa / 30, 1))) %>%
+  filter(!is.na(prop_protected))
+
+for(year in 2011:2015){ #year=2011
+  tmp <- r.yrs[r.yrs$year == year, ] %>%
+    dplyr::select(rgn_id, resilience.score = prop_protected)   
+  write.csv(tmp, sprintf("globalprep/lsp/v2016/output/mpa_eez_%s_resilience.csv", year), row.names=FALSE)
+}
