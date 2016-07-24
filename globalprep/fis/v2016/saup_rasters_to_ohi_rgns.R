87169fdafd79a315b1e3d789944bb2a8914ebe38
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2016-07-24 14:34:48 -0700

    code formatting - no significant changes

--- globalprep/fis/v2016/saup_rasters_to_ohi_rgns.R
+++ globalprep/fis/v2016/saup_rasters_to_ohi_rgns.R
@@ -4,7 +4,7 @@
 ## MRF: June 6 2016
 ###############################################
 install.packages("devtools")
-devtools::install_github("SeaAroundUs/rseaaroundus",force=TRUE)
+devtools::install_github("SeaAroundUs/rseaaroundus")
 
 library(reshape2)
 library(dplyr)
@@ -15,19 +15,19 @@ library(raster)
 source('src/R/common.R')
 
 ohi_regions <- readOGR(dsn = file.path(dir_M, "git-annex/globalprep/spatial/d2014/data"), layer="regions_gcs")  
-inland <- ohi_regions[ohi_regions@data$rgn_typ == "eez-inland",]
+inland      <- ohi_regions[ohi_regions@data$rgn_typ == "eez-inland",]
 plot(inland)
 
-saup_cells <- getcells("POLYGON ((-180 90,-180 -90, 180 -90, 180 90, -180 90))")
-saup_rast <- raster(ncol=720, nrow=360)
+saup_cells  <- getcells("POLYGON ((-180 90,-180 -90, 180 -90, 180 90, -180 90))")
+saup_rast   <- raster(ncol=720, nrow=360)
 saup_rast[] <- saup_cells
 
 plot(saup_rast)
 plot(ohi_regions, add=TRUE)
 
-ohi_to_saup_raster <- raster::extract(saup_rast, ohi_regions, weights = TRUE, normalizeWeights = FALSE, progress = 'text') 
+ohi_to_saup_raster        <- raster::extract(saup_rast, ohi_regions, weights = TRUE, normalizeWeights = FALSE, progress = 'text') 
 names(ohi_to_saup_raster) <- paste(ohi_regions@data$ant_typ, ohi_regions@data$ant_id, sep="_") 
-region_prop_df     <- plyr::ldply(ohi_to_saup_raster, rbind)
+region_prop_df            <- plyr::ldply(ohi_to_saup_raster, rbind)
 
 region_prop_df <- region_prop_df %>%
   separate(.id, c('rgn_typ', 'rgn_id'), sep = '_') %>%
@@ -37,6 +37,7 @@ region_prop_df <- region_prop_df %>%
 ## save all the cell associations
 write.csv(region_prop_df, file.path(dir_M, 
                 "git-annex/globalprep/fis/v2015/raw/saup_rasters_to_ohi_rgns.csv"), row.names=FALSE)
+
 
 ## save only the water cells (this will probably be the file we actually need)
 region_prop_df <- region_prop_df %>%
