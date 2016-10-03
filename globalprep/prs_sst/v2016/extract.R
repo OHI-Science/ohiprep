aa6260c2413175b9427151f318b40c9b9fa212e0
Melsteroni <frazier@nceas.ucsb.edu>
2016-10-03 11:04:50 -0700

    Extracting updated sst data and organizing files

--- globalprep/prs_sst/v2016/extract.R
+++ globalprep/prs_sst/v2016/extract.R
@@ -1,11 +1,8 @@
 ######################################
-## SLR extract for regions
+## SST extract for regions
 ##
-## MRF: 7/12/2016
+## MRF: 9/29/2016
 ######################################
-
-## this is only relevant to eez regions (not high seas and not Antarctica)
-
 
 source('../ohiprep/src/R/common.R')
 
@@ -15,21 +12,45 @@ library(sp)
 library(dplyr)
 
 # raster/zonal data
-slr_loc <- file.path(dir_M, "git-annex/globalprep/prs_slr/v2016/output")
+sst_loc <- file.path(dir_M, "git-annex/globalprep/prs_sst/v2016/output")
 
 zones <- raster(file.path(dir_M, "git-annex/globalprep/spatial/d2014/data/rgn_mol_raster_1km/sp_mol_raster_1km.tif"))  # raster data
 
 rgn_data <- read.csv(file.path(dir_M, "git-annex/globalprep/spatial/d2014/data/rgn_mol_raster_1km/regionData.csv"))    # data for sp_id's used in raster
 
 # save location
-save_loc <- "globalprep/prs_slr/v2016"
+save_loc <- "globalprep/prs_sst/v2016"
+
+
+###########################################
+# apply ice mask
+###########################################
+# read in raster files
+rasts <- list.files(sst_loc)
+rasts <- grep("sst", rasts, value=TRUE)
+
+ice_mask <- raster(file.path(dir_M, "git-annex/Global/NCEAS-Pressures-Summaries_frazier2013/ice_mask_resampled"))
+
+for(sst in rasts){ # sst = "sst_1986-1990_1985-1989.tif"
+  rast <- raster(file.path(sst_loc, sst))
+  overlay(rast, ice_mask, fun=function(x,y) x*y, progress='text',
+          filename=file.path(dir_M, 
+                             sprintf('git-annex/globalprep/prs_sst/v2016/output/sea_ice_mask/sea_ice_mask_%s', sst)),
+          overwrite=TRUE)
+}
+
+
+
+######################
+## Extract data for each region using sst data (with sea ice regions cropped out)
 
 # read in raster files
-rasts <- list.files(slr_loc)
+rasts <- list.files(file.path(sst_loc, "sea_ice_mask"))
 
 pressure_stack <- stack()
-for(raster in rasts){ #raster="slr_1993.tif"
-  tmp <- raster(file.path(slr_loc, raster))
+
+for(raster in rasts){ #raster="sea_ice_mask_sst_1986-1990_1985-1989.tif"
+  tmp <- raster(file.path(sst_loc, "sea_ice_mask", raster))
   pressure_stack <- stack(pressure_stack, tmp)
 }
 
@@ -44,42 +65,44 @@ setdiff(regions_stats2$zone, rgn_data$ant_id) # antarctica regions are in there,
 setdiff(rgn_data$ant_id, regions_stats2$zone) # 213 is in there, that makes sense (Antarctica)
 
 data <- merge(rgn_data, regions_stats, all.y=TRUE, by.x="rgn_id", by.y="zone") %>%
-  gather("year", "pressure_score", starts_with("sea")) 
+  gather("year", "pressure_score", starts_with("slr")) 
+
+write.csv(data, "tmp_data.csv", row.names=FALSE)
 
 sst_data <- data %>%
-  mutate(year=substr(year, 23,26)) %>%
+  mutate(year=gsub('slr_', '', year)) %>%
   mutate(year = as.numeric(year)) %>%
   filter(rgn_typ == "eez") %>%
   dplyr::select(rgn_id, rgn_nam, year, pressure_score)
 
-write.csv(slr_data, file.path(save_loc, "int/sst.csv"), row.names=FALSE)
+write.csv(slr_data, file.path(save_loc, "int/slr.csv"), row.names=FALSE)
 
 ## save toolbox data for different years/regions
 
 # function to extract data more easily
 saveData <- function(newYear){
   
-  assessYear <- newYear + 4
+  assessYear <- newYear + 1
   criteria_year <- ~year == newYear
-
-    sst  <- sst_data %>%
-      filter_(criteria_year) %>%
-      dplyr::select(rgn_id, pressure_score) %>%
-      arrange(rgn_id)
   
-  write.csv(sst, file.path(save_loc, sprintf('output/sst_%s.csv', assessYear)), row.names=FALSE)
+  slr  <- slr_data %>%
+    filter_(criteria_year) %>%
+    dplyr::select(rgn_id, pressure_score) %>%
+    arrange(rgn_id)
+  
+  write.csv(slr, file.path(save_loc, sprintf('output/slr_%s.csv', assessYear)), row.names=FALSE)
 }
 
 
 ### extract data 
-for(newYear in (max(sst_data$year) - 4):(max(sst_data$year))){
+for(newYear in (max(slr_data$year) - 4):(max(slr_data$year))){
   saveData(newYear)
 }
 
 
 ### try visualizing the data using googleVis plot
 library(googleVis)
-plotData <- sst_data %>%
+plotData <- slr_data %>%
   dplyr::select(rgn_nam, year, pressure_score)
 
 Motion=gvisMotionChart(plotData, 
@@ -87,6 +110,6 @@ Motion=gvisMotionChart(plotData,
                        timevar="year")
 plot(Motion)
 
-print(Motion, file=file.path(save_loc, 'sst.html'))
+print(Motion, file=file.path(save_loc, 'slr.html'))
 
 
