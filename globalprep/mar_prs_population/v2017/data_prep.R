773501c5f3dccb96cc01c4617aa5711b47cd61e6
Melsteroni <melanie.frazier@gmail.com>
2017-08-24 14:27:25 -0700

    UN population data

---
+++ globalprep/mar_prs_population/v2017/data_prep.R
@@ -0,0 +1,35 @@
+library(ohicore)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+source("src/R/common.R")
+
+pop <- read.csv(file.path(dir_M,
+        "git-annex/globalprep/_raw_data/UnitedNations_population/v2017/UN_pop_clean.csv"), 
+        strip.white = TRUE, stringsAsFactors = FALSE)
+
+pop_gather <- pop %>%
+  gather("year", "population", starts_with("X")) %>%
+  mutate(population = gsub(" ", "", population)) %>%
+  mutate(year = gsub("X", "", year)) %>%
+  mutate(population = as.numeric(as.character(population)) * 1000)
+
+# Channel Islands: Jersey and Guernsey (ignore for now)
+
+pop_gather_rename <- pop_gather %>%
+  mutate(country = ifelse(country=="Côte d'Ivoire", "Cote d'Ivoire", country)) %>%  # this one is hard to 
+  mutate(country = ifelse(country=="Réunion", "Reunion", country)) %>%
+  mutate(country = ifelse(country=="Curaçao", "Curacao", country)) %>%
+  mutate(country = ifelse(country=="China, Taiwan Province of China", "Taiwan", country)) %>%
+  mutate(country = ifelse(country=="Dem. People's Republic of Korea", "North Korea", country))
+  
+pop_rgn <- name_2_rgn(df_in = pop_gather_rename, 
+                      fld_name='country', 
+                      flds_unique=c('year'))
+pop_rgn <- pop_rgn %>%
+  group_by(rgn_id, year) %>%
+  summarize(population = sum(population)) %>%
+  data.frame()
+  
+write.csv(pop_rgn, "globalprep/mar_prs_population/v2017/output/UN_population.csv", row.names=FALSE)
