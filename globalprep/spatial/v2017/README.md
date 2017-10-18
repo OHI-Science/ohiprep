a36ca7bbf627613c208d6541efcb32d58a7b235c
Melsteroni <melanie.frazier@gmail.com>
2017-10-18 11:36:52 -0700

    Organizing spatial data

--- globalprep/spatial/v2017/README.md
+++ globalprep/spatial/v2017/README.md
@@ -1,7 +1,6 @@
 These changes in the spatial data correct an issue that raster::rasterize has dealing with polygon holes.
 
-Instead of using the raster package we used fasterize to create the raster which has fewer issues
-according to our tests.
+Instead of using the raster package we used fasterize to create the raster which has fewer issues according to our tests.
 
 I also cleaned the polygon file so it is more useful.
 
@@ -11,8 +10,14 @@ regions_2017_update: all the land/eez/fao/antarctica regions with improved label
 
 The following raster files were made with the regions_2017_update shapefile:
 
-ocean.tif: includes eez/fao/antarctica ocean regions (1 or NA values).  This is typically used as a mask for the pressure layers
-
 regions_eez_with_fao_ant.tif: This includes all the ocean regions (eez/fao/antarctica), but the raster cell values correspond to the rgn_ant_id in regions_2017_update.  This file is most often used to extract pressure values for each region.
 
 regions_land_ocean.tif: This includes the land and ocean regions, with the raster cell values corresponding to the rgn_ant_id in regions_2017_update.  This file isn't frequently used.  But it has been used to extract region populations.  
+
+regionData.csv: Uses same variable names as in the regions_2017_update.shp data
+
+Other data files were moved from "ohiprep/src/LookupTables": 
+georegion_labels.csv
+georegions.csv
+rgn_eez_v2013a_synonyms.csv
+rgn_uninhabited_islands.csv
--- src/R/spatial_common.R
+++ src/R/spatial_common.R
@@ -5,22 +5,49 @@ library(sp)
 library(rgdal)
 library(sf)
 library(raster)
-
+devtools::install_github("krlmlr/rprojroot")
+library(rprojroot)
 
 ## set the mazu and neptune data_edit share based on operating system
 dir_M             <- c('Windows' = '//mazu.nceas.ucsb.edu/ohi',
                        'Darwin'  = '/Volumes/ohi',    ### connect (cmd-K) to smb://mazu/ohi
                        'Linux'   = '/home/shares/ohi')[[ Sys.info()[['sysname']] ]]
 
-## Shapefiles
+ohiprep_root <- rprojroot::find_rstudio_root_file()
 
-## OHI regions in vector format
+## OHI region files
+
+### Shapefile:
+## OHI eez, antarctica, and high seas regions
+## This is the most up-to-date file with some topology issues corrected.
+## rgn_type and type_w_ant indicate whether the region is "eez", "land", 
 regions <- st_read(dsn = file.path(dir_M, "git-annex/globalprep/spatial/v2017"), layer = "regions_2017_update")
 
-## Rasters
+### Raster:
+## rasterized OHI regions for zonal statistics
+## This was created using the git-annex/globalprpe/spatial/v2017/regions_2017_update file and
+## the fasterize package (which corrected some small errors in the previous raster that was created 
+## using the rasterize package)
+zones <- raster(file.path(dir_M, "git-annex/globalprep/spatial/v2017/regions_eez_with_fao_ant.tif"))
+
+### Dataframes
+## csv file with region names and ID variables that match the OHI region file
+ohiprep_root <- rprojroot::find_rstudio_root_file()
+rgns_global <- read.csv(file.path(ohiprep_root, "globalprep/spatial/v2017/output/regionData.csv"))
+UNgeorgn_id <- read.csv(file.path(ohiprep_root, "globalprep/spatial/v2017/output/georegions.csv")) 
+UNgeorgn_nm <- read.csv(file.path(ohiprep_root, "globalprep/spatial/v2017/output/georegion_labels.csv")) 
+rgn_syns <- read.csv(file.path(ohiprep_root, "globalprep/spatial/v2017/output/rgn_eez_v2013a_synonyms.csv")) 
+low_pop <- read.csv(file.path(ohiprep_root, "globalprep/spatial/v2017/output/rgn_uninhabited_islands.csv"))
+
+
+ohi_global_root <- sub("/[^/]*$","", ohiprep_root) 
+territory <- read.csv(file.path(ohi_global_root, "ohi-global/eez/spatial/rgns_list.csv"))
+
+### Ocean raster
 
 ## load ocean raster for masking spatial raster data
+## this was the ocean layer created for the original cumulative human impacts
+## This should be used when layers are going to be used for cumulative human impacts
 ocean <- raster(file.path(dir_M, 'model/GL-NCEAS-Halpern2008/tmp/ocean.tif'))
 
-## rasterized OHI regions for zonal statistics
-zones <- raster(file.path(dir_M, "git-annex/globalprep/spatial/v2017/regions_eez_with_fao_ant.tif"))
+
