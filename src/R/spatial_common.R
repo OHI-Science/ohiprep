b797b1eb71898188a26e11dd62509138540cd7b9
Melsteroni <melanie.frazier@gmail.com>
2017-10-19 09:22:16 -0700

    spatial_common updates

--- src/R/spatial_common.R
+++ src/R/spatial_common.R
@@ -1,10 +1,10 @@
 
 ## spatial libraries
 
-library(sp)
-library(rgdal)
-library(sf)
-library(raster)
+#library(sp)
+#library(rgdal)
+#library(sf)
+#library(raster)
 devtools::install_github("krlmlr/rprojroot")
 library(rprojroot)
 
@@ -21,14 +21,14 @@ ohiprep_root <- rprojroot::find_rstudio_root_file()
 ## OHI eez, antarctica, and high seas regions
 ## This is the most up-to-date file with some topology issues corrected.
 ## rgn_type and type_w_ant indicate whether the region is "eez", "land", 
-regions <- st_read(dsn = file.path(dir_M, "git-annex/globalprep/spatial/v2017"), layer = "regions_2017_update")
+regions <- sf::st_read(dsn = file.path(dir_M, "git-annex/globalprep/spatial/v2017"), layer = "regions_2017_update")
 
 ### Raster:
 ## rasterized OHI regions for zonal statistics
 ## This was created using the git-annex/globalprpe/spatial/v2017/regions_2017_update file and
 ## the fasterize package (which corrected some small errors in the previous raster that was created 
 ## using the rasterize package)
-zones <- raster(file.path(dir_M, "git-annex/globalprep/spatial/v2017/regions_eez_with_fao_ant.tif"))
+zones <- raster::raster(file.path(dir_M, "git-annex/globalprep/spatial/v2017/regions_eez_with_fao_ant.tif"))
 
 ### Dataframes
 ## csv file with region names and ID variables that match the OHI region file
@@ -48,6 +48,6 @@ territory <- read.csv(file.path(ohi_global_root, "ohi-global/eez/spatial/rgns_li
 ## load ocean raster for masking spatial raster data
 ## this was the ocean layer created for the original cumulative human impacts
 ## This should be used when layers are going to be used for cumulative human impacts
-ocean <- raster(file.path(dir_M, 'model/GL-NCEAS-Halpern2008/tmp/ocean.tif'))
+ocean <- raster::raster(file.path(dir_M, 'model/GL-NCEAS-Halpern2008/tmp/ocean.tif'))
 
 
